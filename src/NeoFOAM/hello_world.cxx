3a0adf82311511743541851d7582c6933ced6938
Gregor Olenik <gregor.olenik@web.de>
2023-10-24 14:34:19 +0200

    add SPDX headers test reuse linter

--- src/NeoFOAM/hello_world.cxx
+++ src/NeoFOAM/hello_world.cxx
@@ -1,4 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-FileCopyrightText: 2023 NeoFOAM authors
+
 export module NeoFOAM;
 
 namespace NeoFOAM {
--- src/test/test_hello_world.cpp
+++ src/test/test_hello_world.cpp
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-FileCopyrightText: 2023 NeoFOAM authors
+
 #include "gtest/gtest.h"
 
 import NeoFOAM;
