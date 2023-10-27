222a43ad1ab69dd1b1e10e30fda694435d8d02b2
Scheufler, Henning <henning.scheufler@dlr.de>
2023-10-27 22:18:00 +0200

    increased the number of meshes for the computation

--- benchmark/matrixAssembly/plotData.py
+++ benchmark/matrixAssembly/plotData.py
@@ -5,8 +5,6 @@ import pandas as pd
 #%%
 bench_data = pd.read_csv("results/benchmark.csv")
 #%%
-bench_data
-#%%
 bench_data = bench_data[~bench_data["description"].str.contains("application::main")]
 bench_data['nProcs'] = pd.to_numeric(bench_data['nProcs'])
 bench_data['mesh'] = bench_data['mesh'].str.replace('hex_', '')
@@ -15,12 +13,13 @@ bench_data['totalTime'] = pd.to_numeric(bench_data['totalTime'])
 bench_data
 #%%
 bench_data
+bench_data['nCells'] = bench_data['mesh']*bench_data['mesh']
 filtered = bench_data[bench_data["proc"] == "processor0"]
 filtered = filtered[filtered["description"].str.contains("Energy")]
 filtered
 # %%
 # Draw a nested barplot by species and sex
-sns.scatterplot(
-    data=filtered, x="mesh", y="totalTime", hue="description",style="nProcs"
+sns.lineplot(
+    data=filtered, x="nCells", y="totalTime", hue="description",style="nProcs"
 )
 plt.show()
---
+++ setEnv.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+export PATH=$PATH:$PWD/NeoFOAM/bin/ # not really robust
