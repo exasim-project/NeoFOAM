6a075ab2aeefd82fa86eb40c75f39fcc703ec49c
bevan <bevanwsjones@hotmail.com>
2023-12-13 09:22:21 +0200

    - Added some constant iterator support for NeoFOAM::vector. - moved equality operator out of vector. - re-orgnaised includes in test_blas_main.cpp

--- include/NeoFOAM/blas/primitives/vector.hpp
+++ include/NeoFOAM/blas/primitives/vector.hpp
@@ -19,6 +19,18 @@ namespace NeoFOAM
         vector(scalar x, scalar y, scalar z) : cmpts_({x, y, z}) {}
 
         KOKKOS_INLINE_FUNCTION
+        std::array<scalar, 3>::const_iterator begin() const noexcept
+        {
+            return cmpts_.begin();
+        }
+
+        KOKKOS_INLINE_FUNCTION
+        std::array<scalar, 3>::const_iterator end() const noexcept
+        {
+            return cmpts_.end();
+        }
+
+        KOKKOS_INLINE_FUNCTION
         scalar& operator()(const int i)
         {
             return cmpts_[i];
@@ -31,27 +43,21 @@ namespace NeoFOAM
         }
 
         KOKKOS_INLINE_FUNCTION
-        bool operator==(const vector &rhs) const
-        {
-            return cmpts_[0] == rhs(0) && cmpts_[1] == rhs(1) && cmpts_[2] == rhs(2);
-        }
-
-        KOKKOS_INLINE_FUNCTION
-        vector& operator+=(const vector &other)
+        vector& operator+=(const vector& other)
         {
             for(auto i = 0; i < cmpts_.size(); ++i) cmpts_[i] += other.cmpts_[i]; 
             return *this;
         }
 
         KOKKOS_INLINE_FUNCTION
-        vector operator-=(const vector &other)
+        vector& operator-=(const vector& other)
         {
             for(auto i = 0; i < cmpts_.size(); ++i) cmpts_[i] -= other.cmpts_[i]; 
             return *this;
         }
 
         KOKKOS_INLINE_FUNCTION
-        vector operator*=(const scalar &sclr)
+        vector& operator*=(const scalar &sclr)
         {
             for(auto& comp : cmpts_) comp *= sclr; 
             return *this;
@@ -61,6 +67,12 @@ namespace NeoFOAM
         std::array<scalar, 3> cmpts_ = {0.0, 0.0, 0.0};
     };
 
+
+KOKKOS_INLINE_FUNCTION
+bool operator==(const vector& lhs, const vector& rhs)
+{
+    return std::equal(lhs.begin(), lhs.end(), rhs.begin());
+}
 
 KOKKOS_INLINE_FUNCTION
 vector operator+(vector lhs, const vector& rhs)
