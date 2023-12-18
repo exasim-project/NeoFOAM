19b4f8f4d3c25e7a6d6f3ee5cb367c49fc484ea4
bevan <bevanwsjones@hotmail.com>
2023-12-18 08:35:06 +0200

    - renamed fields to field. - renamed test_blas to test_field.

--- src/test/test_field.hpp
+++ src/test/test_field.hpp
@@ -230,10 +230,10 @@ TEST(DeviceAdjacency, InsertPentagonGraph_Undirected) {
 
     // Insert the edges of the 'pentagon' graph
     EXPECT_TRUE(graph.insert({0, 1}));
-    EXPECT_TRUE(graph.insert({0, 4}));
+    EXPECT_TRUE(graph.insert({1, 2}));
     EXPECT_TRUE(graph.insert({2, 3}));
     EXPECT_TRUE(graph.insert({3, 4}));
-    EXPECT_TRUE(graph.insert({1, 2}));
+    EXPECT_TRUE(graph.insert({0, 4}));
 
     // Check the size of the graph
     EXPECT_EQ(graph.size(), 5);
@@ -256,6 +256,58 @@ TEST(DeviceAdjacency, InsertPentagonGraph_Undirected) {
     EXPECT_EQ(graph(3)(1), 4);
     EXPECT_EQ(graph(4)(0), 0);
     EXPECT_EQ(graph(4)(1), 3);
+}
+
+TEST(DeviceAdjacency, ResizeGraph) {
+    NeoFOAM::localAdjacency<true> graph;
+
+    // Insert the edges of the 'pentagon' graph
+    EXPECT_TRUE(graph.insert({0, 1}));
+    EXPECT_TRUE(graph.insert({1, 2}));
+    EXPECT_TRUE(graph.insert({2, 3}));
+    EXPECT_TRUE(graph.insert({3, 4}));
+    EXPECT_TRUE(graph.insert({0, 4}));
+
+    // Resize the graph to a larger size
+    graph.resize(10);
+
+    // Check the size of the graph
+    EXPECT_EQ(graph.size(), 10);
+
+    // Check the adjacency of each vertex
+    EXPECT_EQ(graph(0).size(), 1);
+    EXPECT_EQ(graph(1).size(), 1);
+    EXPECT_EQ(graph(2).size(), 1);
+    EXPECT_EQ(graph(3).size(), 1);
+    EXPECT_EQ(graph(4).size(), 1);
+    EXPECT_EQ(graph(5).size(), 0);
+    EXPECT_EQ(graph(6).size(), 0);
+    EXPECT_EQ(graph(7).size(), 0);
+    EXPECT_EQ(graph(8).size(), 0);
+    EXPECT_EQ(graph(9).size(), 0);
+
+    // Check the connections of each vertex
+    EXPECT_EQ(graph(0)(0), 1);
+    EXPECT_EQ(graph(1)(0), 2);
+    EXPECT_EQ(graph(2)(0), 3);
+    EXPECT_EQ(graph(3)(0), 4);
+    EXPECT_EQ(graph(4)(0), 0);
+
+    // Resize the graph to a smaller size
+    graph.resize(3);
+
+    // Check the size of the graph
+    EXPECT_EQ(graph.size(), 3);
+
+    // Check the adjacency of each vertex
+    EXPECT_EQ(graph(0).size(), 1);
+    EXPECT_EQ(graph(1).size(), 1);
+    EXPECT_EQ(graph(2).size(), 1);
+
+    // Check the connections of each vertex
+    EXPECT_EQ(graph(0)(0), 1);
+    EXPECT_EQ(graph(1)(0), 2);
+    EXPECT_EQ(graph(2)(0), 0);
 }
 
 TEST(DeviceAdjacency, insert) {
