fe0e48d0943bac7502e6919e0ef7bfe0a328286b
Gregor Olenik <gregor.olenik@web.de>
2023-12-31 16:42:03 +0100

    format files

--- src/test/test_time.cpp
+++ src/test/test_time.cpp
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: MPL-2.0
 // SPDX-FileCopyrightText: 2023 NeoFOAM authors
 
-#include "gtest/gtest.h"
 #include "NeoFOAM/NeoFOAM.hpp"
+#include "gtest/gtest.h"
 
-TEST(HelloWord, check_return_value)
-{
-    NeoFOAM::Time time {};
-    EXPECT_EQ(time.timeName(), "0.000000");
+TEST(HelloWord, check_return_value) {
+  NeoFOAM::Time time{};
+  EXPECT_EQ(time.timeName(), "0.000000");
 }
