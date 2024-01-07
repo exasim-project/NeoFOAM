174955309b6b60a3dac81183643fe08e80056199
Gregor Olenik <gregor.olenik@web.de>
2024-01-07 20:35:29 +0100

    Add review comments
    Co-authored-by: Henning Scheufler <henning.scheufler@web.de>

--- cmake/kokkos.cmake
+++ cmake/kokkos.cmake
@@ -8,16 +8,15 @@ set(KOKKOS_CHECKOUT_VERSION
 find_package(Kokkos ${KOKKOS_CHECKOUT_VERSION} QUIET)
 
 if (NOT ${Kokkos_FOUND} OR ${FORCE_NEOFOAM_BUILD_KOKKOS})
-	include(FetchContent)
+    include(FetchContent)
 
-	FetchContent_Declare(
-	  kokkos
-	  QUITE
-	  GIT_SHALLOW ON
-	  GIT_REPOSITORY "https://github.com/kokkos/kokkos.git"
-	  GIT_TAG ${KOKKOS_CHECKOUT_VERSION}
-	  FIND_PACKAGE_ARGS NAMES Kokkos
-	)
+    FetchContent_Declare(
+      kokkos
+      QUITE
+      GIT_SHALLOW ON
+      GIT_REPOSITORY "https://github.com/kokkos/kokkos.git"
+      GIT_TAG ${KOKKOS_CHECKOUT_VERSION}
+    )
 
-	FetchContent_MakeAvailable(Kokkos)
+    FetchContent_MakeAvailable(Kokkos)
 endif()
