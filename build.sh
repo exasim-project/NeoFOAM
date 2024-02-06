c7833b60b03e7ee0a20aa13c7600c81082870842
Gregor Olenik <gregor.olenik@web.de>
2024-02-06 20:08:49 +0100

    remove scripts, lets use cmake for builds and installing 3rd party stuff

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-# Run CMake to generate the build files
-# cmake -G "Ninja" -S . -B build  -DKokkos_DIR="$PWD/Kokkos/lib/cmake/Kokkos" -DCMAKE_BUILD_TYPE=Debug
-cmake -S . -B build  -DKokkos_DIR="$PWD/Kokkos/lib/cmake/Kokkos" -DCMAKE_BUILD_TYPE=Release
-
-# Build the project using make
-cmake --build build
