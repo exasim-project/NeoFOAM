f877d3f2922ed60d42c13dd79f38d0a6fc4e2898
Gregor Olenik <gregor.olenik@web.de>
2024-02-18 21:16:54 +0100

    add license

--- script/runBenchmarks.sh
+++ script/runBenchmarks.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# SPDX-License-Identifier: Unlicense
+# SPDX-FileCopyrightText: 2023 NeoFOAM authors
 
 ./build/src/benchmarks/blas/bench_blas -r XML > blas.xml
 python plotBenchmarks.py
