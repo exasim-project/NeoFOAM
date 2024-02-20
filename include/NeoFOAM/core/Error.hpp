6b852be71df7b9f07898f975ffb8244d34fa9505
bevan <jnsbev005@myuct.ac.za>
2024-02-20 22:00:02 +0100

    - added extra info to the exit method in Error.hpp.

--- include/NeoFOAM/core/Error.hpp
+++ include/NeoFOAM/core/Error.hpp
@@ -2,13 +2,28 @@
 // SPDX-FileCopyrightText: 2023 NeoFOAM authors
 #pragma once
 
+#include <source_location>
+#include <iostream>
 #include <string>
 
 namespace NeoFOAM {
 
 	class error {
-		public:
-		        void exit(const int errNo = 1) {};
+		public:	
+
+		/**
+		 * @brief Exit the program with an error message.
+		 * 
+		 * @param errNo The error number to exit with.
+		 * @param location Default argument for the location of the error.
+		 */
+		void exit(const int errNo = 1, const std::source_location location = std::source_location::current()) {
+			std::cout << "Error: " << errNo << '\n'
+		        		<< "At: " << location.file_name() << '\n'
+						    << "Line: " << location.line() << '\n'
+						    << "Function: " << location.function_name() << '\n';
+				std::exit(errNo);
+			};
 
 			error(std::string){};
 	};
