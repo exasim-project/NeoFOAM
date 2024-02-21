555fe6644926232b995aedabad7db1c1b9a662dc
Gregor Olenik <go@hpsim.de>
2024-02-21 19:24:04 +0000

    remove source_location for now, because of nvcc issues

--- include/NeoFOAM/core/Error.hpp
+++ include/NeoFOAM/core/Error.hpp
@@ -17,11 +17,8 @@ namespace NeoFOAM {
 		 * @param errNo The error number to exit with.
 		 * @param location Default argument for the location of the error.
 		 */
-		void exit(const int errNo = 1, const std::source_location location = std::source_location::current()) {
-			std::cout << "Error: " << errNo << '\n'
-		        		<< "At: " << location.file_name() << '\n'
-						    << "Line: " << location.line() << '\n'
-						    << "Function: " << location.function_name() << '\n';
+		void exit(const int errNo = 1) {
+			std::cout << "Error: " << errNo << '\n';
 				std::exit(errNo);
 			};
 
