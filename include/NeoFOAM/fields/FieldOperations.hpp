c879e772e75be00d3839a6469cc03e0562385af3
Gregor Olenik <gregor.olenik@web.de>
2024-03-09 15:06:27 +0100

    Revert "Use templated functor"
    This reverts commit 3e2bcd4631d4fe541fe01fa88da713a0ce2fafb9.

--- include/NeoFOAM/fields/FieldOperations.hpp
+++ include/NeoFOAM/fields/FieldOperations.hpp
@@ -9,77 +9,45 @@ namespace NeoFOAM
 {
 
 
-template<typename T>
-struct UnaryKernelFunctor
-{
-    template<typename Executor, typename KernelImplementation>
-    void
-    operator()(const Executor& exec, Field<T>& a, const KernelImplementation f)
-    {
-        using executor = typename Executor::exec;
-        auto a_f = a.field();
-        Kokkos::parallel_for(
-            Kokkos::RangePolicy<executor>(0, a_f.size()),
-            KOKKOS_CLASS_LAMBDA(const int i) { a_f[i] = f(i); }
-        );
+// NOTE DONT MERGE do we need the CPU version of this here, should we call this a reference
+// implementation?
+#define UNARY_FIELD_OP(Name, Kernel_Impl)                                     \
+    template<typename T, typename Inner>                                      \
+    struct Name##Op                                                           \
+    {                                                                         \
+        template<typename Executor>                                           \
+        void                                                                  \
+        operator()(const Executor& exec, Field<T>& a, const Inner in)         \
+        {                                                                     \
+            using executor = typename Executor::exec;                         \
+            auto a_f = a.field();                                             \
+            Kokkos::parallel_for(                                             \
+                Kokkos::RangePolicy<executor>(0, a_f.size()),                 \
+                KOKKOS_CLASS_LAMBDA(const int i) { a_f[i] = Kernel_Impl; }    \
+            );                                                                \
+        }                                                                     \
+                                                                              \
+        template<typename Executor>                                           \
+        void operator()(const CPUExecutor& exec, Field<T>& a, const Inner in) \
+        {                                                                     \
+            auto a_f = a.field();                                             \
+            for (int i = 0; i < a_f.size(); i++)                              \
+            {                                                                 \
+                a_f[i] = Kernel_Impl;                                         \
+            }                                                                 \
+        }                                                                     \
+    };                                                                        \
+    template<typename T, typename Inner>                                      \
+    void Name(Field<T>& a, const Inner inner)                                 \
+    {                                                                         \
+        Name##Op<T, Inner> op_;                                               \
+        std::visit([&](const auto& exec) { op_(exec, a, inner); }, a.exec()); \
     }
 
-    template<typename Executor, typename KernelImplementation>
-    void operator()(const CPUExecutor& exec, Field<T>& a, const Inner in)
-    {
-        auto a_f = a.field();
-        for (int i = 0; i < a_f.size(); i++)
-        {
-            a_f[i] = KernelImplementation(i);
-        }
-    }
-};
-
-template<typename T, typename Inner>
-void map(Field<T>& a, const Inner inner)
-{
-    UnaryKernelFunctor<T, Inner> op;
-    std::visit([&](const auto& exec)
-               { op(exec, a, [](const int i)
-                    { return inner(i); }); },
-               a.exec());
-}
-
-template<typename T, typename Inner>
-void fill(Field<T>& a, const Inner inner)
-{
-    UnaryKernelFunctor<T, Inner> op;
-    std::visit([&](const auto& exec)
-               { op(exec, a, [](const int i)
-                    { return inner; }); },
-               a.exec());
-}
-
-template<typename T, typename Inner>
-void setField(Field<T>& a, const Inner inner)
-{
-    UnaryKernelFunctor<T, Inner> op;
-    std::visit([&](const auto& exec)
-               { op(exec, a, [](const int i)
-                    { return inner[i]; }); },
-               a.exec());
-}
-
-template<typename T, typename Inner>
-void scalar_mul(Field<T>& a, const Inner inner)
-{
-    // FIXME
-    UnaryKernelFunctor<T, Inner> op;
-    std::visit([&](const auto& exec)
-               { op(exec, a, [](const int i)
-                    { return inner[i]; }); },
-               a.exec());
-}
-
-// UNARY_FIELD_OP(map, in(i));
-// UNARY_FIELD_OP(fill, in);
-// UNARY_FIELD_OP(setField, in[i]);
-// UNARY_FIELD_OP(scalar_mul, a_f[i] *= in);
+UNARY_FIELD_OP(map, in(i));
+UNARY_FIELD_OP(fill, in);
+UNARY_FIELD_OP(setField, in[i]);
+UNARY_FIELD_OP(scalar_mul, a_f[i] *= in);
 
 #undef UNARY_FIELD_OP
 
@@ -95,7 +63,7 @@ void scalar_mul(Field<T>& a, const Inner inner)
         {                                                                        \
             using executor = typename Executor::exec;                            \
             auto a_f = a.field();                                                \
-            const auto b_f = b.field();                                          \
+            auto b_f = b.field();                                                \
             Kokkos::parallel_for(                                                \
                 Kokkos::RangePolicy<executor>(0, a_f.size()),                    \
                 KOKKOS_CLASS_LAMBDA(const int i) { Kernel_Impl; }                \
