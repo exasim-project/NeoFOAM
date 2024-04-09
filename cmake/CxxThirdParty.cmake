482b479670fbf752470a59dc6392154b32f730c0
Gregor Olenik <gregor.olenik@web.de>
2024-04-09 10:23:56 +0200

    Apply review comments
    Co-authored-by: Marcel Koch <marcel.koch@kit.edu>

--- cmake/CxxThirdParty.cmake
+++ cmake/CxxThirdParty.cmake
@@ -3,20 +3,35 @@
 
 include(cmake/CPM.cmake)
 
-CPMAddPackage("gh:kokkos/kokkos#4.3.00")
-
-CPMAddPackage("gh:nlohmann/json@3.11.3")
-CPMAddPackage("gh:gabime/spdlog@1.13.0")
+CPMAddPackage(
+  NAME Kokkos
+  GITHUB_REPOSITORY kokkos/kokkos
+  VERSION 4.3.00
+  GIT_TAG 4.3.00
+)
 
 CPMAddPackage(
+  NAME nlohmann_json
+  GITHUB_REPOSITORY nlohmann/json
+  VERSION 3.11.3
+)
+
+CPMAddPackage(
+  NAME spdlog
+  GITHUB_REPOSITORY gabime/spdlog
+  VERSION 1.13.0
+)
+
+CPMAddPackage(
+  NAME cxxopts
   GITHUB_REPOSITORY jarro2783/cxxopts
   VERSION 3.2.0
-  OPTIONS
-  "CXXOPTS_BUILD_EXAMPLES NO"
-  "CXXOPTS_BUILD_TESTS NO"
-  "CXXOPTS_ENABLE_INSTALL YES"
-  )
+)
 
-if(NEOFOAM_BUILD_TESTS)
-    CPMAddPackage("gh:catchorg/Catch2@3.4.0")
+if(NEOFOAM_BUILD_TESTS OR NEOFOAM_BUILD_BENCHMARKS)
+  CPMAddPackage(
+    NAME Catch2
+    GITHUB_REPOSITORY catchorg/Catch2
+    VERSION 3.4.0
+  )
 endif()
