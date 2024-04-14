08b9493781ffa07c4bea7539969c2b86d119e2e2
Gregor Olenik <gregor.olenik@web.de>
2024-04-14 20:17:24 +0200

    add basic pybind11 framework

--- cmake/CxxThirdParty.cmake
+++ cmake/CxxThirdParty.cmake
@@ -3,35 +3,26 @@
 
 include(cmake/CPM.cmake)
 
-CPMAddPackage(
-  NAME Kokkos
-  GITHUB_REPOSITORY kokkos/kokkos
-  VERSION 4.3.00
-  GIT_TAG 4.3.00
-)
+cpmaddpackage(
+  NAME
+  Kokkos
+  GITHUB_REPOSITORY
+  kokkos/kokkos
+  VERSION
+  4.3.00
+  GIT_TAG
+  4.3.00)
 
-CPMAddPackage(
-  NAME nlohmann_json
-  GITHUB_REPOSITORY nlohmann/json
-  VERSION 3.11.3
-)
+cpmaddpackage(NAME nlohmann_json GITHUB_REPOSITORY nlohmann/json VERSION 3.11.3)
 
-CPMAddPackage(
-  NAME spdlog
-  GITHUB_REPOSITORY gabime/spdlog
-  VERSION 1.13.0
-)
+cpmaddpackage(NAME spdlog GITHUB_REPOSITORY gabime/spdlog VERSION 1.13.0)
 
-CPMAddPackage(
-  NAME cxxopts
-  GITHUB_REPOSITORY jarro2783/cxxopts
-  VERSION 3.2.0
-)
+cpmaddpackage(NAME cxxopts GITHUB_REPOSITORY jarro2783/cxxopts VERSION 3.2.0)
 
 if(NEOFOAM_BUILD_TESTS OR NEOFOAM_BUILD_BENCHMARKS)
-  CPMAddPackage(
-    NAME Catch2
-    GITHUB_REPOSITORY catchorg/Catch2
-    VERSION 3.4.0
-  )
+  cpmaddpackage(NAME Catch2 GITHUB_REPOSITORY catchorg/Catch2 VERSION 3.4.0)
+endif()
+
+if(NEOFOAM_BUILD_PYTHON_BINDINGS)
+  cpmaddpackage(NAME pybind11 GITHUB_REPOSITORY pybind/pybind11 VERSION 2.12.0)
 endif()
