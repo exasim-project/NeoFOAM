3405e5897f2abae048d09075e67acbfa0447f23b
Henning Scheufler <henning.scheufler@web.de>
2024-04-14 12:45:34 +0200

    added boundarysection for surface fields and the ability to create fvccSurfaceField with calculated boundary conditions

--- include/NeoFOAM/cellCentredFiniteVolume/bcFields/fvccBoundaryFieldSelector.hpp
+++ include/NeoFOAM/cellCentredFiniteVolume/bcFields/fvccBoundaryFieldSelector.hpp
@@ -17,23 +17,52 @@
 #include "NeoFOAM/cellCentredFiniteVolume/bcFields/vol/vector/fvccVectorZeroGradientBoundaryField.hpp"
 #include "NeoFOAM/cellCentredFiniteVolume/bcFields/vol/vector/fvccVectorEmptyBoundaryField.hpp"
 
+#include "NeoFOAM/cellCentredFiniteVolume/bcFields/fvccSurfaceBoundaryField.hpp"
+#include "NeoFOAM/cellCentredFiniteVolume/bcFields/surface/scalar/fvccSurfaceScalarCalculatedBoundaryField.hpp"
+#include "NeoFOAM/cellCentredFiniteVolume/bcFields/surface/scalar/fvccSurfaceScalarEmptyBoundaryField.hpp"
+
 #include <functional>
 #include <string>
 
 namespace NeoFOAM
 {
 
-template <typename T>
+template<typename T>
 std::unique_ptr<fvccBoundaryField<T>> getBC(const unstructuredMesh& mesh, int patchID, const Dictionary& patchDict)
 {
-
 }
 
-template <>
+template<>
 std::unique_ptr<fvccBoundaryField<scalar>> getBC(const unstructuredMesh& mesh, int patchID, const Dictionary& patchDict);
 
-template <>
+template<>
 std::unique_ptr<fvccBoundaryField<Vector>> getBC(const unstructuredMesh& mesh, int patchID, const Dictionary& patchDict);
 
+
+
+
+template<typename T>
+std::unique_ptr<fvccSurfaceBoundaryField<T>> getSurfaceBC(const unstructuredMesh& mesh, int patchID, const Dictionary& patchDict)
+{
+}
+
+template<>
+std::unique_ptr<fvccSurfaceBoundaryField<scalar>> getSurfaceBC(const unstructuredMesh& mesh, int patchID, const Dictionary& patchDict);
+
+template<>
+std::unique_ptr<fvccSurfaceBoundaryField<Vector>> getSurfaceBC(const unstructuredMesh& mesh, int patchID, const Dictionary& patchDict);
+
+
+
+template<typename T>
+std::vector<std::unique_ptr<fvccSurfaceBoundaryField<T>>> createCalculatedBCs(const unstructuredMesh& mesh)
+{
+}
+
+template<>
+std::vector<std::unique_ptr<fvccSurfaceBoundaryField<scalar>>>  createCalculatedBCs(const unstructuredMesh& mesh);
+
+template<>
+std::vector<std::unique_ptr<fvccSurfaceBoundaryField<Vector>>>  createCalculatedBCs(const unstructuredMesh& mesh);
 
 }; // namespace NeoFOAM
