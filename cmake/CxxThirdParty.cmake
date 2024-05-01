b1277f928b72d55cb8e5a07def085b86772f6e73
Henning Scheufler <henning.scheufler@web.de>
2024-05-01 12:07:33 +0200

    fix: neofoam compiles with cuda support and gcc

--- cmake/CxxThirdParty.cmake
+++ cmake/CxxThirdParty.cmake
@@ -3,35 +3,15 @@
 
 include(cmake/CPM.cmake)
 
-CPMAddPackage(
-  NAME Kokkos
-  GITHUB_REPOSITORY kokkos/kokkos
-  VERSION 4.3.00
-  GIT_TAG 4.3.00
-)
+# CPMAddPackage( NAME Kokkos GITHUB_REPOSITORY kokkos/kokkos VERSION 4.3.00
+# GIT_TAG 4.3.00 )
 
-CPMAddPackage(
-  NAME nlohmann_json
-  GITHUB_REPOSITORY nlohmann/json
-  VERSION 3.11.3
-)
+cpmaddpackage(NAME nlohmann_json GITHUB_REPOSITORY nlohmann/json VERSION 3.11.3)
 
-CPMAddPackage(
-  NAME spdlog
-  GITHUB_REPOSITORY gabime/spdlog
-  VERSION 1.13.0
-)
+cpmaddpackage(NAME spdlog GITHUB_REPOSITORY gabime/spdlog VERSION 1.13.0)
 
-CPMAddPackage(
-  NAME cxxopts
-  GITHUB_REPOSITORY jarro2783/cxxopts
-  VERSION 3.2.0
-)
+cpmaddpackage(NAME cxxopts GITHUB_REPOSITORY jarro2783/cxxopts VERSION 3.2.0)
 
 if(NEOFOAM_BUILD_TESTS OR NEOFOAM_BUILD_BENCHMARKS)
-  CPMAddPackage(
-    NAME Catch2
-    GITHUB_REPOSITORY catchorg/Catch2
-    VERSION 3.4.0
-  )
+  cpmaddpackage(NAME Catch2 GITHUB_REPOSITORY catchorg/Catch2 VERSION 3.4.0)
 endif()
