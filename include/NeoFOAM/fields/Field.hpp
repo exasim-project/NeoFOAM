1adc3c95485055d05f3172182e126583195edc82
Henning Scheufler <henning.scheufler@web.de>
2024-05-03 20:01:23 +0200

    ENH: added domain and boundaryField implementation

--- include/NeoFOAM/fields/Field.hpp
+++ include/NeoFOAM/fields/Field.hpp
@@ -41,13 +41,14 @@ public:
         data_ = static_cast<T*>(ptr);
     };
 
-    Field(const Field<T>& rhs) : size_(rhs.size_), data_(nullptr), exec_(rhs.exec_)
+    Field(const Field<T>& rhs)
+        : size_(rhs.size_), data_(nullptr), exec_(rhs.exec_)
     {
         void* ptr = nullptr;
         auto size = rhs.size_;
-        std::visit(
-            [this, &ptr, size](const auto& exec) { ptr = exec.alloc(size * sizeof(T)); }, exec_
-        );
+        std::visit([this, &ptr, size](const auto& exec)
+                   { ptr = exec.alloc(size * sizeof(T)); },
+                   exec_);
         data_ = static_cast<T*>(ptr);
         setField(*this, rhs.field());
     };
@@ -79,6 +80,36 @@ public:
     [[nodiscard]] Field<T> copyToHost() const
     {
         Field<T> result(CPUExecutor {}, size_);
+        if (!std::holds_alternative<GPUExecutor>(exec_))
+        {
+            result = *this;
+        }
+        else
+        {
+            if (result.size() != size_)
+            {
+                exit(1);
+            }
+
+            Kokkos::View<T*, Kokkos::DefaultExecutionSpace, Kokkos::MemoryUnmanaged>
+                GPU_view(data_, size_);
+            Kokkos::View<T*, Kokkos::HostSpace, Kokkos::MemoryUnmanaged> result_view(
+                result.data(), size_
+            );
+            Kokkos::deep_copy(result_view, GPU_view);
+        }
+        return result;
+    }
+
+    /**
+     * @brief Copies the data (from anywhere) to a parsed host field.
+     * @param result The field into which the data must be copied. Must be sized.
+     *
+     * @warning exits if the size of the result field is not the same as the
+     * source field.
+     */
+    void copyToHost(Field<T>& result)
+    {
         if (!std::holds_alternative<GPUExecutor>(exec_))
         {
             result = *this;
@@ -297,7 +328,7 @@ public:
      * @brief Gets the field as a span.
      * @return Span of the field.
      */
-    [[nodiscard]] const std::span<T> field() const { return std::span<T>(data_, size_); }
+    [[nodiscard]] const std::span<const T> field() const { return std::span<const T>(data_, size_); }
 
 private:
 
---
+++ test/test_Field.cpp
@@ -0,0 +1,185 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: 2023 NeoFOAM authors
+
+#define CATCH_CONFIG_RUNNER // Define this before including catch.hpp to create
+                            // a custom main
+#include <catch2/catch_session.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators_all.hpp>
+
+#include "NeoFOAM/fields/FieldTypeDefs.hpp"
+#include "NeoFOAM/fields/FieldOperations.hpp"
+
+#include "NeoFOAM/fields/boundaryFields.hpp"
+#include "NeoFOAM/fields/domainField.hpp"
+
+#include "NeoFOAM/fields/comparisions/fieldComparision.hpp"
+
+TEST_CASE("Field Operations")
+{
+    NeoFOAM::executor exec = GENERATE(
+        NeoFOAM::executor(NeoFOAM::CPUExecutor {}),
+        NeoFOAM::executor(NeoFOAM::OMPExecutor {}),
+        NeoFOAM::executor(NeoFOAM::GPUExecutor {})
+    );
+    std::string exec_name = std::visit([](auto e)
+                                       { return e.print(); },
+                                       exec);
+
+    SECTION("Field_" + exec_name)
+    {
+        int N = 10;
+        NeoFOAM::Field<NeoFOAM::scalar> a(exec, N);
+        auto s_a = a.field();
+        NeoFOAM::fill(a, 5.0);
+
+        REQUIRE(compare(a, 5.0));
+
+        NeoFOAM::Field<NeoFOAM::scalar> b(exec, N + 2);
+        NeoFOAM::fill(b, 10.0);
+
+        a = b;
+        REQUIRE(a.field().size() == N + 2);
+        REQUIRE(compare(a, b));
+
+        add(a, b);
+        REQUIRE(a.field().size() == N + 2);
+        REQUIRE(compare(a, 20.0));
+
+        a = a + b;
+        REQUIRE(compare(a, 30.0));
+
+        a = a - b;
+        REQUIRE(compare(a, 20.0));
+
+        a = a * 0.1;
+        REQUIRE(compare(a, 2.0));
+
+        a = a * b;
+        REQUIRE(compare(a, 20.0));
+
+        auto s_b = b.field();
+        a.apply(KOKKOS_LAMBDA(int i) { return 2 * s_b[i]; });
+        REQUIRE(compare(a, 20.0));
+    };
+}
+
+TEST_CASE("Primitives")
+{
+    SECTION("Vector")
+    {
+        SECTION("CPU")
+        {
+            NeoFOAM::Vector a(1.0, 2.0, 3.0);
+            REQUIRE(a(0) == 1.0);
+            REQUIRE(a(1) == 2.0);
+            REQUIRE(a(2) == 3.0);
+
+            NeoFOAM::Vector b(1.0, 2.0, 3.0);
+            REQUIRE(a == b);
+
+            NeoFOAM::Vector c(2.0, 4.0, 6.0);
+
+            REQUIRE(a + b == c);
+
+            REQUIRE((a - b) == NeoFOAM::Vector(0.0, 0.0, 0.0));
+
+            a += b;
+            REQUIRE(a == c);
+
+            a -= b;
+            REQUIRE(a == b);
+            a *= 2;
+            REQUIRE(a == c);
+            a = b;
+
+            REQUIRE(a == b);
+
+            NeoFOAM::Vector d(4.0, 8.0, 12.0);
+            REQUIRE((a + a + a + a) == d);
+            REQUIRE((4 * a) == d);
+            REQUIRE((a * 4) == d);
+            REQUIRE((a + 3 * a) == d);
+            REQUIRE((a + 2 * a + a) == d);
+        };
+    };
+};
+
+TEST_CASE("Boundaries")
+{
+
+    NeoFOAM::executor exec = GENERATE(
+        NeoFOAM::executor(NeoFOAM::CPUExecutor {}),
+        NeoFOAM::executor(NeoFOAM::OMPExecutor {}),
+        NeoFOAM::executor(NeoFOAM::GPUExecutor {})
+    );
+
+    std::string exec_name = std::visit([](auto e)
+                                       { return e.print(); },
+                                       exec);
+    SECTION("domainField_" + exec_name)
+    {
+
+        NeoFOAM::domainField<double> a(exec, 1000, 100, 10);
+
+        NeoFOAM::fill(a.internalField(), 2.0);
+        REQUIRE(compare(a.internalField(), 2.0));
+    }
+
+    SECTION("boundaryFields_" + exec_name)
+    {
+
+        NeoFOAM::boundaryFields<double> BCs(exec, 100, 10);
+
+        NeoFOAM::fill(BCs.value(), 2.0);
+        REQUIRE(compare(BCs.value(), 2.0));
+
+        NeoFOAM::fill(BCs.refValue(), 2.0);
+        REQUIRE(compare(BCs.refValue(), 2.0));
+
+        NeoFOAM::fill(BCs.refGrad(), 2.0);
+        REQUIRE(compare(BCs.refGrad(), 2.0));
+
+        NeoFOAM::fill(BCs.valueFraction(), 2.0);
+        REQUIRE(compare(BCs.valueFraction(), 2.0));
+    }
+
+    // SECTION("fvccBoundaryField")
+    // {
+
+    //     std::vector<std::unique_ptr<NeoFOAM::fvccBoundaryField<double>>> bcs;
+    //     bcs.push_back(std::make_unique<NeoFOAM::fvccScalarFixedValueBoundaryField>(0, 10, 1.0));
+    //     bcs.push_back(std::make_unique<NeoFOAM::fvccScalarFixedValueBoundaryField>(10, 20, 2.0));
+
+    //     NeoFOAM::fvccVolField<NeoFOAM::scalar> volField(
+    //         1000,
+    //         20,
+    //         2,
+    //         std::move(bcs),
+    //         exec
+    //     );
+
+    //     NeoFOAM::boundaryFields<NeoFOAM::scalar>& bField = volField.boundaryField();
+
+    //     auto& volBCs = volField.boundaryConditions();
+
+    //     REQUIRE(volBCs.size() == 2.0);
+
+    //     volField.correctBoundaryConditions();
+
+    //     auto& bIn = bField.value();
+    //     auto& bRefIn = bField.refValue();
+
+    //     for (int i = 0; i < 10; i++)
+    //     {
+    //         REQUIRE(bIn.field()[i] == 1.0);
+    //         REQUIRE(bRefIn.field()[i] == 1.0);
+    //     }
+
+    //     for (int i = 10; i < 20; i++)
+    //     {
+    //         REQUIRE(bIn.field()[i] == 2.0);
+    //         REQUIRE(bRefIn.field()[i] == 2.0);
+    //     }
+    // }
+}
