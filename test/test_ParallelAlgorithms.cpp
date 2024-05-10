b31b0486a506f4944350a5c1a6545086082e9da5
Henning Scheufler <henning.scheufler@web.de>
2024-05-10 14:17:08 +0200

    added parallelFor and parallelReduce

---
+++ test/test_ParallelAlgorithms.cpp
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: 2023 NeoFOAM authors
+
+#define CATCH_CONFIG_RUNNER // Define this before including catch.hpp to create
+                            // a custom main
+#include <catch2/catch_session.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators_all.hpp>
+
+#include "NeoFOAM/fields/Field.hpp"
+#include "NeoFOAM/core/executor/executor.hpp"
+#include "NeoFOAM/core/parallelAlgorithms.hpp"
+
+
+TEST_CASE("parallelFor")
+{
+    NeoFOAM::Executor exec = GENERATE(
+        NeoFOAM::Executor(NeoFOAM::CPUExecutor {}),
+        NeoFOAM::Executor(NeoFOAM::OMPExecutor {}),
+        NeoFOAM::Executor(NeoFOAM::GPUExecutor {})
+    );
+    std::string exec_name = std::visit([](auto e) { return e.print(); }, exec);
+
+    SECTION("parallelFor_" + exec_name)
+    {
+        NeoFOAM::Field<NeoFOAM::scalar> fieldA(exec, 5);
+        NeoFOAM::fill(fieldA, 0.0);
+        NeoFOAM::Field<NeoFOAM::scalar> fieldB(exec, 5);
+        auto spanA = fieldA.field();
+        auto spanB = fieldB.field();
+        NeoFOAM::fill(fieldB, 1.0);
+        NeoFOAM::parallelFor(
+            exec,
+            0,
+            5,
+            KOKKOS_LAMBDA(const int i) { spanA[i] = spanB[i] + 2.0; }
+        );
+        auto hostSpanA = fieldA.copyToHost().field();
+        for (int i = 0; i < 5; i++)
+        {
+            REQUIRE(hostSpanA[i] == 3.0);
+        }
+    }
+};
+
+
+TEST_CASE("parallelReduce")
+{
+    NeoFOAM::Executor exec = GENERATE(
+        NeoFOAM::Executor(NeoFOAM::CPUExecutor {}),
+        NeoFOAM::Executor(NeoFOAM::OMPExecutor {}),
+        NeoFOAM::Executor(NeoFOAM::GPUExecutor {})
+    );
+    std::string exec_name = std::visit([](auto e) { return e.print(); }, exec);
+
+    SECTION("parallelReduce_" + exec_name)
+    {
+        NeoFOAM::Field<NeoFOAM::scalar> fieldA(exec, 5);
+        NeoFOAM::fill(fieldA, 0.0);
+        NeoFOAM::Field<NeoFOAM::scalar> fieldB(exec, 5);
+        auto spanA = fieldA.field();
+        auto spanB = fieldB.field();
+        NeoFOAM::fill(fieldB, 1.0);
+        NeoFOAM::scalar sum = 0.0;
+        NeoFOAM::parallelReduce(
+            exec,
+            0,
+            5,
+            KOKKOS_LAMBDA(const int i, double& lsum) { lsum += spanB[i]; },
+            sum
+        );
+
+        REQUIRE(sum == 5.0);
+    }
+};
