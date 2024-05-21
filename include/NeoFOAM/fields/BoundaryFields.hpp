5ffae2f68ce9755fe6eb27eb590a49389333d1f5
Gregor Olenik <gregor.olenik@web.de>
2024-05-21 10:01:35 +0200

    add copydocs

--- include/NeoFOAM/fields/BoundaryFields.hpp
+++ include/NeoFOAM/fields/BoundaryFields.hpp
@@ -51,37 +51,42 @@ public:
           nBoundaries_(nBoundaries), nBoundaryFaces_(nBoundaryFaces)
     {}
 
+
+    /** @copydoc BoundaryFields::value()*/
+    const NeoFOAM::Field<T>& value() const { return value_; }
+
     /**
      * @brief Get the view storing the computed values from the boundary
      * condition.
      * @return The view storing the computed values.
      */
-    const NeoFOAM::Field<T>& value() const { return value_; }
-
     NeoFOAM::Field<T>& value() { return value_; }
+
+    /** @copydoc BoundaryFields::refValue()*/
+    const NeoFOAM::Field<T>& refValue() const { return refValue_; }
 
     /**
      * @brief Get the view storing the Dirichlet boundary values.
      * @return The view storing the Dirichlet boundary values.
      */
-    const NeoFOAM::Field<T>& refValue() const { return refValue_; }
-
     NeoFOAM::Field<T>& refValue() { return refValue_; }
+
+    /** @copydoc BoundaryFields::valueFraction()*/
+    const NeoFOAM::Field<scalar>& valueFraction() const { return valueFraction_; }
 
     /**
      * @brief Get the view storing the fraction of the boundary value.
      * @return The view storing the fraction of the boundary value.
      */
-    const NeoFOAM::Field<scalar>& valueFraction() const { return valueFraction_; }
-
     NeoFOAM::Field<scalar>& valueFraction() { return refValue_; }
+
+    /** @copydoc BoundaryFields::refGrad()*/
+    const NeoFOAM::Field<T>& refGrad() const { return refGrad_; }
 
     /**
      * @brief Get the view storing the Neumann boundary values.
      * @return The view storing the Neumann boundary values.
      */
-    const NeoFOAM::Field<T>& refGrad() const { return refGrad_; }
-
     NeoFOAM::Field<T>& refGrad() { return refGrad_; }
 
     /**
@@ -100,13 +105,13 @@ public:
      * @brief Get the number of boundaries.
      * @return The number of boundaries.
      */
-    int nBoundaries() { return nBoundaries_; }
+    int nBoundaries() const { return nBoundaries_; }
 
     /**
      * @brief Get the number of boundary faces.
      * @return The number of boundary faces.
      */
-    int nBoundaryFaces() { return nBoundaryFaces_; }
+    int nBoundaryFaces() const { return nBoundaryFaces_; }
 
     const Executor& exec() { return exec_; }
 
