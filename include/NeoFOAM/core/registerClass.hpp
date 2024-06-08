02f6e7799fd014c328bbe7df7ee6279c41cbd3a7
Henning Scheufler <henning.scheufler@web.de>
2024-06-08 16:21:12 +0200

    ENH: added runtime derived class discovery

---
+++ include/NeoFOAM/core/registerClass.hpp
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: 2024 NeoFOAM authors
+
+#pragma once
+
+#include <functional>
+#include <iostream>
+
+namespace NeoFOAM
+{
+
+
+template<typename baseClass, typename... Args>
+struct registerClassManager
+{
+    using createFunction = std::function<std::unique_ptr<baseClass>(Args... args)>;
+
+    static bool registerClass(const std::string name, createFunction funcCreate)
+    {
+        auto result = s_methods.insert({name, funcCreate});
+        if (!result.second)
+        {
+            throw std::runtime_error("Insertion failed: Key already exists.");
+        }
+        return result.second;
+    }
+
+    // static std::unique_ptr<baseClass> Create()
+    static std::unique_ptr<baseClass> Create(const std::string& name, Args... args)
+    {
+        try
+        {
+            auto func = s_methods.at(name);
+            return func(args...);
+        }
+        catch (const std::out_of_range& e)
+        {
+            std::cerr << "Error: " << e.what() << std::endl;
+            return nullptr;
+        }
+    }
+
+    static inline std::unordered_map<std::string, createFunction> s_methods;
+
+protected:
+};
+
+
+// template <typename baseClass, typename... Args>
+// bool registerClass<baseClass, Args...>::reg = registerClassManager<baseClass, Args...>::init();
+
+// template< typename derivedClass, typename baseClass>
+template<typename derivedClass, typename baseClass, typename... Args>
+struct registerClass
+{
+
+    using createFunction = std::function<std::unique_ptr<baseClass>(Args... args)>;
+    registerClass() { reg; };
+
+    // virtual std::string name() = 0;
+
+    static bool reg;
+    static bool init()
+    {
+        // derivedClass t;
+        // registerClassManager<baseClass, Args..>::registerClass(derivedClass::name(), [](Args...
+        // args) { return std::make_unique<derivedClass>(Args... args); });
+        // decltype(registerClassManager<baseClass, Args...>::Create) funcCreate = [](Args... args)
+        // { return std::make_unique<derivedClass>(args...); };
+        // std::function<std::unique_ptr<baseClass>(Args... args)> funcCreate2 = [](Args... args) {
+        // return std::make_unique<derivedClass>(Args... args); }; registerClassManager<baseClass,
+        // Args...>::createFunction funcCreate;
+        //  = [](Args... args) { return std::make_unique<derivedClass>(Args... args); };
+        registerClassManager<baseClass, Args...>::registerClass(
+            derivedClass::name(), derivedClass::create
+        ); //, [](Args... args) { return std::make_unique<derivedClass>(args...); });
+        // registerClassManager<baseClass>::registerClass(derivedClass::name());
+        return true;
+    }
+};
+
+template<typename derivedClass, typename baseClass, typename... Args>
+bool NeoFOAM::registerClass<derivedClass, baseClass, Args...>::reg =
+    NeoFOAM::registerClass<derivedClass, baseClass, Args...>::init();
+
+} // namespace NeoFOAM
