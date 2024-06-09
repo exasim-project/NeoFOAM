bf3fe887aeff9842f141bfb6904d9dda01d510ca
Henning Scheufler <16381681+HenningScheufler@users.noreply.github.com>
2024-06-09 08:52:47 +0200

    Apply review comments
    Co-authored-by: Gregor Olenik <gregor.olenik@web.de>

--- include/NeoFOAM/core/registerClass.hpp
+++ include/NeoFOAM/core/registerClass.hpp
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 // SPDX-FileCopyrightText: 2024 NeoFOAM authors
 
+// additional information see:
+// https://stackoverflow.com/questions/52354538/derived-class-discovery-at-compile-time
+// https://stackoverflow.com/questions/10332725/how-to-automatically-register-a-class-on-creation
+
 #pragma once
 
 #include <functional>
@@ -9,15 +13,48 @@
 namespace NeoFOAM
 {
 
-
-template<typename baseClass, typename... Args>
-struct RegisterClassManager
+// Type trait to check if a type is a std::function
+template<typename T>
+struct is_std_function : std::false_type
 {
-    using createFunction = std::function<std::unique_ptr<baseClass>(Args... args)>;
+};
 
-    static bool registerClass(const std::string name, createFunction funcCreate)
+template<typename Ret, typename... Args>
+struct is_std_function<std::function<Ret(Args...)>> : std::true_type
+{
+};
+
+// Concept that uses the type trait
+template<typename T>
+concept StdFunction = is_std_function<T>::value;
+
+/**
+ * @brief Template struct for managing class registration.
+ *
+ * This struct provides a mechanism for registering classes with a given base class and a create
+ * function. It maintains a map of class names to create functions, allowing for dynamic class
+ * instantiation.
+ *
+ * @tparam baseClass The base class type.
+ * @tparam CreateFunction The type of the create function.
+ */
+template<typename baseClass, StdFunction CreateFunction>
+class RegisterClassManager // needs to be a class otherwise breathe will not document it
+{
+public:
+
+    /**
+     * @brief Registers a class with the given name and create function.
+     *
+     * @param name The name of the class.
+     * @param createFunc The create function for the class.
+     * @return true if the class was successfully registered, false if the class name already exists
+     * in the map.
+     * @throws std::runtime_error if the class name already exists in the map.
+     */
+    static bool registerClass(const std::string name, CreateFunction createFunc)
     {
-        auto result = classMap.insert({name, funcCreate});
+        auto result = classMap.insert({name, createFunc});
         if (!result.second)
         {
             throw std::runtime_error("Insertion failed: Key already exists.");
@@ -25,46 +62,84 @@ struct RegisterClassManager
         return result.second;
     }
 
-    static std::unique_ptr<baseClass> create(const std::string& name, Args... args)
-    {
-        try
-        {
-            auto func = classMap.at(name);
-            return func(args...);
-        }
-        catch (const std::out_of_range& e)
-        {
-            std::cerr << "Error: " << e.what() << std::endl;
-            return nullptr;
-        }
-    }
+    /**
+     * @brief Gets the number of registered classes.
+     *
+     * @return The number of registered classes.
+     */
+    static size_t size() const { return classMap.size(); }
 
-    static inline std::unordered_map<std::string, createFunction> classMap;
-
-protected:
+    /**
+     * @brief A container that maps strings to create functions.
+     *
+     * This unordered map is used to store a mapping between strings and create functions.
+     * The keys are strings, and the values are functions that create objects
+     *
+     * @tparam Key The type of the keys in the map (std::string).
+     * @tparam T The type of the values in the map (CreateFunction)
+     */
+    static inline std::unordered_map<std::string, CreateFunction> classMap;
 };
 
 
-template<typename derivedClass, typename baseClass, typename... Args>
-struct RegisterClass
+/**
+ * @brief Template struct for registering a derived class with a base class.
+ *
+ * This struct provides a mechanism for registering a derived class with a base class.
+ *
+ * @tparam derivedClass The derived class to be registered.
+ * @tparam baseClass The base class that the derived class inherits from.
+ * @tparam CreateFunction The function pointer type for creating an instance of the derived class.
+ */
+template<typename derivedClass, typename baseClass, StdFunction CreateFunction>
+class RegisterClass
 {
+public:
 
-    using createFunction = std::function<std::unique_ptr<baseClass>(Args... args)>;
-    RegisterClass() { reg; };
+    /**
+     * @brief Constructor for the RegisterClass struct.
+     *
+     * reg needs to be called in the constructor to ensure that the class is registered.
+     */
+    RegisterClass() { reg; }
 
+    /**
+     * @brief Static flag indicating if the class has been registered.
+     */
     static bool reg;
 
+    /**
+     * @brief Initializes the registration of the derived class with the base class.
+     *
+     * This function registers the derived class with the base class by calling the
+     * RegisterClassManager::registerClass() function with the derived class's name and
+     * create function.
+     *
+     * @return True if the registration is successful, false otherwise.
+     */
     static bool init()
     {
-        RegisterClassManager<baseClass, Args...>::registerClass(
+        RegisterClassManager<baseClass, CreateFunction>::registerClass(
             derivedClass::name(), derivedClass::create
         );
         return true;
     }
 };
 
-template<typename derivedClass, typename baseClass, typename... Args>
-bool NeoFOAM::RegisterClass<derivedClass, baseClass, Args...>::reg =
-    NeoFOAM::RegisterClass<derivedClass, baseClass, Args...>::init();
+
+/**
+ * @brief Template specialization for registering a derived class with a base class in NeoFOAM.
+ *
+ * This template class is used to register a derived class with a base class in NeoFOAM.
+ * It provides a static member variable `reg` which is initialized to `true` when the class is
+ * instantiated
+ *
+ * @tparam derivedClass The derived class to be registered.
+ * @tparam baseClass The base class with which the derived class is registered.
+ * @tparam CreateFunction The function pointer type for creating an instance of the derived class.
+ */
+template<typename derivedClass, typename baseClass, StdFunction CreateFunction>
+bool NeoFOAM::RegisterClass<derivedClass, baseClass, CreateFunction>::reg =
+    NeoFOAM::RegisterClass<derivedClass, baseClass, CreateFunction>::init();
 
 } // namespace NeoFOAM
--- test/core/test_RegisterClass.cpp
+++ test/core/test_RegisterClass.cpp
@@ -10,37 +10,52 @@
 #include "NeoFOAM/core/dictionary.hpp"
 #include <iostream>
 
+// forward declaration so we can use it to define the create function and the class manager
+class TestBaseClass;
 
-class TestBaseClass
+// define the create function use to instantiate the derived classes
+using createFunc = std::function<std::unique_ptr<TestBaseClass>(std::string, double)>;
+
+// define the class manager to register the classes
+using TestBaseClassManager = NeoFOAM::RegisterClassManager<TestBaseClass, createFunc>;
+
+
+class TestBaseClass : public TestBaseClassManager
 {
 public:
 
-    // entries required runtime discovery
-    using regTestBaseClass = NeoFOAM::RegisterClassManager<TestBaseClass, std::string, double>;
 
     template<typename derivedClass>
-    using TestBaseClassReg =
-        NeoFOAM::RegisterClass<derivedClass, TestBaseClass, std::string, double>;
+    using TestBaseClassReg = NeoFOAM::RegisterClass<derivedClass, TestBaseClass, createFunc>;
 
-    template<typename derivedClass>
-    void registerClass()
+    static std::unique_ptr<TestBaseClass>
+    create(const std::string& name, std::string testString, double testValue)
     {
-        TestBaseClassReg<derivedClass> reg;
+        try
+        {
+            auto func = classMap.at(name);
+            return func(testString, testValue);
+        }
+        catch (const std::out_of_range& e)
+        {
+            std::cerr << "Error: " << e.what() << std::endl;
+            return nullptr;
+        }
     }
 
-    static int size() { return regTestBaseClass::classMap.size(); }
 
-    // standard base class entries
+    template<typename derivedClass>
+    bool registerClass()
+    {
+        return TestBaseClassReg<derivedClass>::reg;
+    }
+
     virtual ~TestBaseClass() = default;
 
     virtual std::string testString() = 0;
 
     virtual double testValue() = 0;
-
-
-private:
 };
-
 
 class TestDerivedClass : public TestBaseClass
 {
@@ -105,17 +120,20 @@ TEST_CASE("Register Class")
     REQUIRE(TestBaseClass::size() == 2);
 
     std::unique_ptr<TestBaseClass> testDerived =
-        TestBaseClass::regTestBaseClass::create("TestDerivedClass", "FirstDerived", 1.0);
+        TestBaseClass::create("TestDerivedClass", "FirstDerived", 1.0);
+
     std::cout << "TestBaseClass testValue: " << testDerived->testValue() << std::endl;
     std::cout << "TestBaseClass testString: " << testDerived->testString() << std::endl;
+
     REQUIRE(testDerived->testString() == "FirstDerived");
     REQUIRE(testDerived->testValue() == 1.0);
 
     std::unique_ptr<TestBaseClass> testDerived2 =
-        TestBaseClass::regTestBaseClass::create("TestDerivedClass2", "SecondDerived", 1.0);
+        TestBaseClass::create("TestDerivedClass2", "SecondDerived", 1.0);
 
     std::cout << "TestBaseClass testValue: " << testDerived2->testValue() << std::endl;
     std::cout << "TestBaseClass testString: " << testDerived2->testString() << std::endl;
+
     REQUIRE(testDerived2->testString() == "SecondDerived");
     REQUIRE(
         testDerived2->testValue() == 2.0
