0631d5aafc882e6248fe44e3d22e3fa77ca25acb
Gregor Olenik <gregor.olenik@web.de>
2024-06-09 20:40:19 +0200

    make surfaceBoundaryField a BoundaryField

--- include/NeoFOAM/finiteVolume/cellCentred/fields/boundary/surfaceBoundaryField.hpp
+++ include/NeoFOAM/finiteVolume/cellCentred/fields/boundary/surfaceBoundaryField.hpp
@@ -9,30 +9,15 @@
 namespace NeoFOAM::finiteVolume::cellCentred
 {
 
+/**
+ * @brief Represents a surface boundary field for a cell-centered finite volume method.
+ *
+ * @tparam ValueType The data type of the field.
+ */
 template<typename ValueType>
-class SurfaceBoundaryField
+class SurfaceBoundaryField : public BoundaryField<ValueType>
 {
-
-public:
-
-    SurfaceBoundaryField(const UnstructuredMesh& mesh, int patchID)
-        : mesh_(mesh), patchID_(patchID), start_(mesh.boundaryMesh().offset()[patchID_]),
-          end_(mesh.boundaryMesh().offset()[patchID_ + 1]), size_(end_ - start_)
-    {}
-
-    virtual void
-    correctBoundaryConditions(BoundaryFields<scalar>& bfield, Field<scalar>& internalField)
-    {}
-
-    int size() const { return size_; }
-
-protected:
-
-    const UnstructuredMesh& mesh_;
-    int patchID_;
-    int start_;
-    int end_;
-    int size_;
+    using BoundaryField<ValueType>::BoundaryField;
 };
 
-};
+}
