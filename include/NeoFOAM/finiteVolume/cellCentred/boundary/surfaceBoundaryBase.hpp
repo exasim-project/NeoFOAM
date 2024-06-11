b2fadf7c43f9b7a30cd0e871e70cf7378ec93396
Gregor Olenik <gregor.olenik@web.de>
2024-06-11 12:38:33 +0200

    add noop implementation of correctBoundaryConditions

--- include/NeoFOAM/finiteVolume/cellCentred/boundary/surfaceBoundaryBase.hpp
+++ include/NeoFOAM/finiteVolume/cellCentred/boundary/surfaceBoundaryBase.hpp
@@ -18,6 +18,11 @@ template<typename ValueType>
 class SurfaceBoundaryBase : public BoundaryBase<ValueType>
 {
     using BoundaryBase<ValueType>::BoundaryBase;
+
+    virtual void correctBoundaryConditions(
+        const Field<ValueType>& internalField, BoundaryFields<ValueType>& bfield
+    )
+    {}
 };
 
 }
