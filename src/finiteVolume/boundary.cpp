7a3739b7a4f881ae9a6a5305c1b94b2fcd9a3f27
Henning Scheufler <henning.scheufler@web.de>
2024-06-15 11:53:29 +0200

    volumeBoundary uses the classical strategy pattern with compile time discovery

---
+++ src/finiteVolume/boundary.cpp
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: 2024 NeoFOAM authors
+
+#include "NeoFOAM/finiteVolume/cellCentred/boundary/volumeBoundaryBase.hpp"
+#include "NeoFOAM/finiteVolume/cellCentred/boundary/volumeBoundaryBase.hpp"
+#include "NeoFOAM/finiteVolume/cellCentred/boundary/surfaceBoundaryBase.hpp"
+
+namespace fvcc = NeoFOAM::finiteVolume::cellCentred;
+
+template class fvcc::VolumeBoundaryModel<NeoFOAM::scalar>;
+template class fvcc::VolumeBoundaryModel<NeoFOAM::Vector>;
---
+++ test/finiteVolume/cellCentred/fields/boundary/test_boundaryField.cpp
@@ -0,0 +1,154 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: 2024 NeoFOAM authors
+
+#define CATCH_CONFIG_RUNNER // Define this before including catch.hpp to create
+                            // a custom main
+#include <catch2/catch_session.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators_all.hpp>
+
+#include "NeoFOAM/core.hpp"
+#include "NeoFOAM/finiteVolume/cellCentred.hpp"
+#include "NeoFOAM/mesh/unstructured/unstructuredMesh.hpp"
+#include "NeoFOAM/core/dictionary.hpp"
+
+namespace fvcc = NeoFOAM::finiteVolume::cellCentred;
+using fvccScalarBC = fvcc::VolumeBoundaryModel<NeoFOAM::scalar>;
+using fvccVectorBC = fvcc::VolumeBoundaryModel<NeoFOAM::Vector>;
+class TestDerivedClass : public fvccScalarBC
+{
+
+public:
+
+    TestDerivedClass(std::string name, double test)
+        : fvccScalarBC(), testString_(name), testValue_(test)
+    {
+        registerClass<TestDerivedClass>();
+    }
+
+    static std::unique_ptr<fvccScalarBC>
+    create(const NeoFOAM::UnstructuredMesh& mesh, const NeoFOAM::Dictionary& dict, int patchID)
+    {
+        std::string name = dict.get<std::string>("name");
+        double test = dict.get<double>("test");
+        return std::make_unique<TestDerivedClass>(name, test);
+    }
+
+    virtual void correctBoundaryConditions(NeoFOAM::DomainField<NeoFOAM::scalar>& domainField
+    ) override
+    {
+        std::cout << "Correcting boundary conditions" << std::endl;
+    }
+
+    static std::string name() { return "TestDerivedClass"; }
+
+private:
+
+    std::string testString_;
+    double testValue_;
+};
+
+template<typename ValueType>
+class fixedValue : public fvcc::VolumeBoundaryModel<ValueType>
+{
+
+public:
+
+    using FixedValueType = fixedValue<ValueType>;
+
+    template<typename executor>
+    void setFixedValue(const executor& exec, std::span<ValueType> field, ValueType value)
+    {
+        if constexpr (std::is_same<std::remove_reference_t<executor>, NeoFOAM::CPUExecutor>::value)
+        {
+            for (std::size_t i = start_; i < end_; i++)
+            {
+                field[i] = value;
+            }
+        }
+        else
+        {
+            using runOn = typename executor::exec;
+            Kokkos::parallel_for(
+                "parallelForImpl",
+                Kokkos::RangePolicy<runOn>(start_, end_),
+                KOKKOS_LAMBDA(std::size_t i) { field[i] = value; }
+            );
+        }
+    }
+
+    fixedValue(std::size_t start, std::size_t end, std::size_t patchID, ValueType uniformValue)
+        : fvcc::VolumeBoundaryModel<ValueType>(), start_(start), end_(end), patchID_(patchID),
+          uniformValue_(uniformValue)
+    {
+        fvcc::VolumeBoundaryModel<ValueType>::template registerClass<FixedValueType>();
+    }
+
+    static std::unique_ptr<fvcc::VolumeBoundaryModel<ValueType>>
+    create(const NeoFOAM::UnstructuredMesh& mesh, const NeoFOAM::Dictionary& dict, int patchID)
+    {
+
+        ValueType uniformValue = dict.get<ValueType>("uniformValue");
+        std::size_t start = dict.get<std::size_t>("start");
+        std::size_t end = dict.get<std::size_t>("end");
+        return std::make_unique<FixedValueType>(start, end, patchID, uniformValue);
+    }
+
+    virtual void correctBoundaryConditions(NeoFOAM::DomainField<ValueType>& domainField) override
+    {
+        std::visit(
+            [&](auto exec)
+            { setFixedValue(exec, domainField.boundaryField().refValue().field(), uniformValue_); },
+            domainField.exec()
+        );
+    }
+
+    static std::string name() { return "fixedValue"; }
+
+private:
+
+    ValueType uniformValue_;
+    std::size_t start_;
+    std::size_t end_;
+    std::size_t patchID_;
+};
+
+template class fixedValue<NeoFOAM::scalar>;
+template class fixedValue<NeoFOAM::Vector>;
+
+TEST_CASE("boundaryField")
+{
+    std::cout << "Number of registered classes: " << fvccScalarBC::nRegistered() << std::endl;
+    REQUIRE(fvccScalarBC::classMap().size() == 2);
+    REQUIRE(fvccVectorBC::classMap().size() == 1);
+
+    NeoFOAM::Executor exec = GENERATE(
+        NeoFOAM::Executor(NeoFOAM::CPUExecutor {}),
+        NeoFOAM::Executor(NeoFOAM::OMPExecutor {}),
+        NeoFOAM::Executor(NeoFOAM::GPUExecutor {})
+    );
+
+    std::string execName = std::visit([](auto e) { return e.print(); }, exec);
+
+    SECTION("TestDerivedClass" + execName)
+    {
+        NeoFOAM::DomainField<NeoFOAM::scalar> domainField(exec, 10, 10, 1);
+
+        TestDerivedClass testDerived("TestDerivedClass", 1.0);
+        testDerived.correctBoundaryConditions(domainField);
+        REQUIRE(fvccScalarBC::nRegistered() == 2);
+    }
+
+    SECTION("fixedValue" + execName)
+    {
+        NeoFOAM::DomainField<NeoFOAM::scalar> domainField(exec, 10, 10, 1);
+
+        fixedValue<NeoFOAM::scalar> fixedValue(0, 10, 1, 1.0);
+        fixedValue.correctBoundaryConditions(domainField);
+        auto refValueHost = domainField.boundaryField().refValue().copyToHost().field();
+        for (std::size_t i = 0; i < 10; i++)
+        {
+            REQUIRE(refValueHost[i] == 1.0);
+        }
+    }
+}
