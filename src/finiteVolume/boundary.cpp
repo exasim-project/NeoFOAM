ce3b0823aee5e3b9ff8e1aa76d92a34082af307a
Gregor Olenik <gregor.olenik@web.de>
2024-06-27 14:35:11 +0200

    fix includes, keep tests out for now

--- src/finiteVolume/boundary.cpp
+++ src/finiteVolume/boundary.cpp
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
 // SPDX-FileCopyrightText: 2024 NeoFOAM authors
 
-#include "NeoFOAM/finiteVolume/cellCentred/boundary/volumeBoundaryBase.hpp"
-#include "NeoFOAM/finiteVolume/cellCentred/boundary/surfaceBoundaryBase.hpp"
+#include "NeoFOAM/finiteVolume/cellCentred/boundary/volumeBoundaryFactory.hpp"
 
 namespace fvcc = NeoFOAM::finiteVolume::cellCentred;
 
