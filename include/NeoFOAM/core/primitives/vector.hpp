c778338ca6fbf02f8485d16673f633d8dec79e70
bevanwsjones <bevanwsjones@gmail.com>
2024-07-14 22:32:14 +0200

    - Corrected omission of return type for arithmetic assignment operator in Vector. - removed duplicated [] operator in Vector.

--- include/NeoFOAM/core/primitives/vector.hpp
+++ include/NeoFOAM/core/primitives/vector.hpp
@@ -70,12 +70,6 @@ public:
     scalar operator()(const int i) const { return cmpts_[i]; }
 
     KOKKOS_INLINE_FUNCTION
-    scalar& operator[](const int i) { return cmpts_[i]; }
-
-    KOKKOS_INLINE_FUNCTION
-    scalar operator[](const int i) const { return cmpts_[i]; }
-
-    KOKKOS_INLINE_FUNCTION
     bool operator==(const Vector& rhs) const
     {
         return cmpts_[0] == rhs(0) && cmpts_[1] == rhs(1) && cmpts_[2] == rhs(2);
@@ -88,11 +82,12 @@ public:
     }
 
     KOKKOS_INLINE_FUNCTION
-    void operator+=(const Vector& rhs)
+    Vector& operator+=(const Vector& rhs)
     {
         cmpts_[0] += rhs(0);
         cmpts_[1] += rhs(1);
         cmpts_[2] += rhs(2);
+        return *this;
     }
 
     KOKKOS_INLINE_FUNCTION
@@ -102,11 +97,12 @@ public:
     }
 
     KOKKOS_INLINE_FUNCTION
-    void operator-=(const Vector& rhs)
+    Vector& operator-=(const Vector& rhs)
     {
         cmpts_[0] -= rhs(0);
         cmpts_[1] -= rhs(1);
         cmpts_[2] -= rhs(2);
+        return *this;
     }
 
     KOKKOS_INLINE_FUNCTION
@@ -124,11 +120,12 @@ public:
 
 
     KOKKOS_INLINE_FUNCTION
-    void operator*=(const scalar& rhs)
+    Vector& operator*=(const scalar& rhs)
     {
         cmpts_[0] *= rhs;
         cmpts_[1] *= rhs;
         cmpts_[2] *= rhs;
+        return *this;
     }
 
 private:
