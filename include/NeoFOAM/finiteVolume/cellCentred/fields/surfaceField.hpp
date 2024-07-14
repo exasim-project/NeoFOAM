1c075e984e200ad3515bae30eddf38910b0da7b6
Henning Scheufler <henning.scheufler@web.de>
2024-07-14 14:57:04 +0200

    FIX: surface and VolumeField domain internalField size was incorrect

--- include/NeoFOAM/finiteVolume/cellCentred/fields/surfaceField.hpp
+++ include/NeoFOAM/finiteVolume/cellCentred/fields/surfaceField.hpp
@@ -12,6 +12,69 @@ namespace NeoFOAM::finiteVolume::cellCentred
 {
 
 template<typename ValueType>
-using SurfaceField = GeometricField<ValueType, SurfaceBoundaryPatchMixin<ValueType>>;
+class SurfaceField : public GeometricFieldMixin<ValueType>
+{
+
+public:
+
+    SurfaceField(
+        const Executor& exec,
+        const UnstructuredMesh& mesh,
+        std::vector<std::unique_ptr<SurfaceBoundary<ValueType>>>&& boundaryConditions
+    )
+        : GeometricFieldMixin<ValueType>(
+            exec,
+            mesh,
+            DomainField<ValueType>(
+                exec,
+                mesh.nInternalFaces() + mesh.nBoundaryFaces(),
+                mesh.nBoundaryFaces(),
+                mesh.nBoundaries()
+            )
+        ),
+          boundaryConditions_(std::move(boundaryConditions))
+    {}
+
+    SurfaceField(
+        const Executor& exec,
+        const UnstructuredMesh& mesh,
+        const DomainField<ValueType>& field,
+        std::vector<std::unique_ptr<SurfaceBoundary<ValueType>>>&& boundaryConditions
+    )
+        : GeometricFieldMixin<ValueType>(exec, mesh, field),
+          boundaryConditions_(std::move(boundaryConditions))
+    {}
+
+    SurfaceField(const SurfaceField& other)
+        : GeometricFieldMixin<ValueType>(other),
+          boundaryConditions_(other.boundaryConditions_.size())
+    {
+        // for (size_t i = 0; i < other.boundaryConditions_.size(); ++i)
+        // {
+        //     boundaryConditions_[i] =
+        //     std::make_unique<SurfaceBoundary<ValueType>>(*other.boundaryConditions_[i]);
+        // }
+    }
+
+    /**
+     * @brief Corrects the boundary conditions of the surface field.
+     *
+     * This function applies the correctBoundaryConditions() method to each boundary condition in
+     * the field.
+     */
+    void correctBoundaryConditions()
+    {
+        for (auto& boundaryCondition : boundaryConditions_)
+        {
+            boundaryCondition->correctBoundaryCondition(this->field_);
+        }
+    }
+
+private:
+
+    std::vector<std::unique_ptr<SurfaceBoundary<ValueType>>>
+        boundaryConditions_; // The vector of boundary conditions
+};
+
 
 } // namespace NeoFOAM
