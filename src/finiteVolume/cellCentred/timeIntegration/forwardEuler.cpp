3d895f13c712bd6c44512fc4a6f7dec562374d1e
Henning Scheufler <henning.scheufler@web.de>
2024-07-26 12:31:39 +0200

    WIP: forwardEuler

--- src/finiteVolume/cellCentred/timeIntegration/forwardEuler.cpp
+++ src/finiteVolume/cellCentred/timeIntegration/forwardEuler.cpp
@@ -19,7 +19,26 @@ ForwardEuler::ForwardEuler(const dsl::EqnSystem& eqnSystem, const Dictionary& di
 
 void ForwardEuler::solve()
 {
-    // Solve function
+    std::cout << "Solving using Forward Euler" << std::endl;
+    scalar dt = 0.001; // Time step
+    fvcc::VolumeField<scalar>* refField = eqnSystem_.volumeField();
+    // Field<scalar> Phi(eqnSystem_.exec(), eqnSystem_.nCells());
+    // NeoFOAM::fill(Phi, 0.0);
+    Field<scalar> source = eqnSystem_.explicitOperation();
+
+    // for (auto& eqnTerm : eqnSystem_.temporalTerms())
+    // {
+    //     eqnTerm.temporalOperation(Phi);
+    // }
+    // Phi += source*dt;
+    refField->internalField() -= source*dt;
+    refField->correctBoundaryConditions();
+
+    // check if execturo is GPU
+    if (std::holds_alternative<NeoFOAM::GPUExecutor>(eqnSystem_.exec()))
+    {
+       Kokkos::fence();
+    }
 }
 
 std::unique_ptr<TimeIntegrationFactory> ForwardEuler::clone() const
