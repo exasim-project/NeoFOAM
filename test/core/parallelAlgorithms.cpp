6343174b8c9792027a49948fb2c739d920b167c7
Henning Scheufler <henning.scheufler@web.de>
2024-08-19 20:08:37 +0200

    ENH: added ValueOrSpan that returns a Value of no span is present

--- test/core/parallelAlgorithms.cpp
+++ test/core/parallelAlgorithms.cpp
@@ -6,6 +6,7 @@
 #include <catch2/catch_session.hpp>
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/generators/catch_generators_all.hpp>
+#include <catch2/benchmark/catch_benchmark.hpp>
 
 #include "NeoFOAM/fields/field.hpp"
 #include "NeoFOAM/core/executor/executor.hpp"
@@ -76,7 +77,80 @@ TEST_CASE("parallelFor")
             REQUIRE(value == 3.0);
         }
     }
+
+    SECTION("parallelFor_Field_Visit" + execName)
+    {
+        NeoFOAM::Field<NeoFOAM::scalar> fieldA(exec, 5, 0.0);
+        NeoFOAM::Field<NeoFOAM::scalar> fieldB(exec, 5, 1.0);
+        auto [spanA, spanB] = NeoFOAM::spans(fieldA, fieldB);
+        NeoFOAM::ValueOrSpan<NeoFOAM::scalar> cOrS = spanB;
+
+        NeoFOAM::parallelFor(
+            fieldA, KOKKOS_LAMBDA(const size_t i) { return cOrS[i] + 2.0; }
+        );
+
+        auto hostSpanA = fieldA.copyToHost();
+        for (auto value : hostSpanA.span())
+        {
+            REQUIRE(value == 3.0);
+        }
+
+        cOrS = 1.0;
+
+        NeoFOAM::parallelFor(
+            fieldA, KOKKOS_LAMBDA(const size_t i) { return cOrS[i] + 2.0; }
+        );
+
+        hostSpanA = fieldA.copyToHost();
+        for (auto value : hostSpanA.span())
+        {
+            REQUIRE(value == 3.0);
+        }
+    }
 };
+
+TEST_CASE("ConstantOrSpan Benchmark")
+{
+    size_t size = GENERATE(1e2, 1e3, 1e4, 1e5, 1e6, 1e7);
+
+    NeoFOAM::Executor exec = GENERATE(
+        NeoFOAM::Executor(NeoFOAM::SerialExecutor {}),
+        NeoFOAM::Executor(NeoFOAM::CPUExecutor {}),
+        NeoFOAM::Executor(NeoFOAM::GPUExecutor {})
+    );
+
+    std::string execName = std::visit([](auto e) { return e.print(); }, exec);
+
+    NeoFOAM::Field<NeoFOAM::scalar> fieldA(exec, size, 0.0);
+    NeoFOAM::Field<NeoFOAM::scalar> fieldB(exec, size, 1.0);
+    auto [spanA, spanB] = NeoFOAM::spans(fieldA, fieldB);
+    NeoFOAM::ValueOrSpan<NeoFOAM::scalar> cOrS = spanB;
+    REQUIRE(cOrS.useSpan == true);
+
+    BENCHMARK("ValueOrSpan<NeoFOAM::scalar> Span " + execName + " " + std::to_string(size))
+    {
+        NeoFOAM::parallelFor(
+            fieldA, KOKKOS_LAMBDA(const size_t i) { return cOrS[i] + 2.0; }
+        );
+    };
+
+    cOrS = 1.0;
+    REQUIRE(cOrS.useSpan == false);
+
+    BENCHMARK("ValueOrSpan<NeoFOAM::scalar> Constant " + execName + " " + std::to_string(size))
+    {
+        NeoFOAM::parallelFor(
+            fieldA, KOKKOS_LAMBDA(const size_t i) { return cOrS[i] + 2.0; }
+        );
+    };
+
+    BENCHMARK("ValueOrSpan<NeoFOAM::scalar> Double " + execName + " " + std::to_string(size))
+    {
+        NeoFOAM::parallelFor(
+            fieldA, KOKKOS_LAMBDA(const size_t i) { return 1.0 + 2.0; }
+        );
+    };
+}
 
 
 TEST_CASE("parallelReduce")
