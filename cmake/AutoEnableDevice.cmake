98a9ea6636cb01fef0836caebb7e7be00b776e2f
Henning Scheufler <henning.scheufler@web.de>
2024-09-05 21:47:29 +0200

    FIX: link clang to gomp to get structured binding support

--- cmake/AutoEnableDevice.cmake
+++ cmake/AutoEnableDevice.cmake
@@ -7,6 +7,13 @@ if(NOT DEFINED Kokkos_ENABLE_OPENMP AND NOT DEFINED Kokkos_ENABLE_THREADS)
   find_package(OpenMP QUIET)
 
   if(OpenMP_FOUND)
+    # Check if the compiler is Clang
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+      message(STATUS "Using Clang compiler")
+      set(OpenMP_CXX_FLAGS "-fopenmp=libgomp")
+      set(OpenMP_CXX_LIB_NAMES "gomp")
+      set(OpenMP_gomp_LIBRARY gomp)
+    endif()
     set(Kokkos_ENABLE_OPENMP
         ON
         CACHE INTERNAL "")
