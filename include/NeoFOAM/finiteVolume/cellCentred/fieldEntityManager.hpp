7186f954370ac0f36f7b11c065f3152aea2754a6
Henning Scheufler <henning.scheufler@web.de>
2024-09-08 19:44:04 +0200

    moved fieldDataBase and EntityManager into fvcc

---
+++ include/NeoFOAM/finiteVolume/cellCentred/fieldEntityManager.hpp
@@ -0,0 +1,105 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: 2024 NeoFOAM authors
+#pragma once
+
+#include <unordered_map>
+#include <any>
+#include <string>
+#include <memory>
+#include <vector>
+
+#include "NeoFOAM/core/demangle.hpp"
+#include "NeoFOAM/core/error.hpp"
+
+#include "NeoFOAM/finiteVolume/cellCentred/fields/volumeField.hpp"
+#include "NeoFOAM/finiteVolume/cellCentred/boundary/volumeBoundaryFactory.hpp"
+
+namespace NeoFOAM::finiteVolume::cellCentred
+{
+
+enum class FieldCategory
+{
+    Iteration,
+    oldTime,
+    newTime,
+    cacheField
+};
+
+template<typename Type>
+struct FieldComponent
+{
+    std::shared_ptr<Type> field;
+    std::size_t timeIndex;
+    std::size_t iterationIndex;
+    FieldCategory category;
+};
+
+class FieldEntityManager
+{
+
+public:
+
+    FieldEntityManager();
+
+    template<typename T>
+    void insert(const int& key, T value)
+    {
+        fieldDB_.emplace(key, value);
+    }
+
+    template<typename T>
+    T& get(const int& key)
+    {
+        try
+        {
+            return std::any_cast<T&>(fieldDB_.at(key));
+        }
+        catch (const std::bad_any_cast& e)
+        {
+            logBadAnyCast<T>(e, key, fieldDB_);
+            throw e;
+        }
+    }
+
+    template<typename T>
+    const T& get(const int& key) const
+    {
+        try
+        {
+            return std::any_cast<const T&>(fieldDB_.at(key));
+        }
+        catch (const std::bad_any_cast& e)
+        {
+            logBadAnyCast<T>(e, key, fieldDB_);
+            throw e;
+        }
+    }
+
+    std::string fieldName;
+private:
+
+    std::unordered_map<int, std::any> fieldDB_;
+
+};  
+
+namespace operations
+{
+
+template <typename T>
+FieldEntityManager newFieldEntity(const T& field)
+{
+    FieldEntityManager fieldEntityManager;
+    fieldEntityManager.fieldName = field.name;
+    FieldComponent<T> fieldComponent{
+        .field = std::make_shared<T>(field),
+        .timeIndex = 0,
+        .iterationIndex = 0,
+        .category = FieldCategory::newTime
+    };
+    fieldEntityManager.insert(0, fieldComponent);
+    return fieldEntityManager;
+}
+
+} // namespace operations
+
+} // namespace NeoFOAM
---
+++ test/finiteVolume/cellCentred/fields/fieldDatabase.cpp
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: 2024 NeoFOAM authors
+
+#define CATCH_CONFIG_RUNNER // Define this before including catch.hpp to create
+                            // a custom main
+#include <catch2/catch_session.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators_all.hpp>
+
+#include "NeoFOAM/finiteVolume/cellCentred/fields/volumeField.hpp"
+#include "NeoFOAM/finiteVolume/cellCentred/boundary/volumeBoundaryFactory.hpp"
+#include "NeoFOAM/finiteVolume/cellCentred/fieldDatabase.hpp"
+#include "NeoFOAM/finiteVolume/cellCentred/fieldEntityManager.hpp"
+
+template<typename T>
+using I = std::initializer_list<T>;
+
+namespace fvcc = NeoFOAM::finiteVolume::cellCentred;
+
+fvcc::VolumeField<NeoFOAM::scalar> createVolumeField(const NeoFOAM::UnstructuredMesh& mesh)
+{
+    std::vector<fvcc::VolumeBoundary<NeoFOAM::scalar>> bcs {};
+    for (auto patchi : I<size_t> {0, 1, 2, 3})
+    {
+        NeoFOAM::Dictionary dict;
+        dict.insert("type", std::string("fixedValue"));
+        dict.insert("fixedValue", 2.0);
+        bcs.push_back(fvcc::VolumeBoundary<NeoFOAM::scalar>(mesh, dict, patchi));
+    }
+    fvcc::VolumeField<NeoFOAM::scalar> vf(mesh.exec(), "vf", mesh, bcs);
+    NeoFOAM::fill(vf.internalField(), 1.0);
+    return vf;
+}
+
+TEST_CASE("FieldEntityManager")
+{
+
+    NeoFOAM::Executor exec = GENERATE(
+        NeoFOAM::Executor(NeoFOAM::SerialExecutor {}),
+        NeoFOAM::Executor(NeoFOAM::CPUExecutor {}),
+        NeoFOAM::Executor(NeoFOAM::GPUExecutor {})
+    );
+
+    std::string execName = std::visit([](auto e) { return e.print(); }, exec);
+    NeoFOAM::UnstructuredMesh mesh = NeoFOAM::createSingleCellMesh(exec);
+
+
+    SECTION("FieldEntityManager: " + execName)
+    {
+        fvcc::FieldEntityManager fm = fvcc::operations::newFieldEntity(createVolumeField(mesh));
+
+        auto fieldPtr = fm.get<fvcc::FieldComponent<fvcc::VolumeField<NeoFOAM::scalar>>>(0).field;//->internalField().copyToHost();
+        auto hostInteralField = fieldPtr->internalField().copyToHost();
+        REQUIRE(hostInteralField.span()[0] == 1.0);
+        REQUIRE(fm.get<fvcc::FieldComponent<fvcc::VolumeField<NeoFOAM::scalar>>>(0).timeIndex == 0.0);
+        REQUIRE(fm.get<fvcc::FieldComponent<fvcc::VolumeField<NeoFOAM::scalar>>>(0).iterationIndex == 0.0);
+        REQUIRE(fm.get<fvcc::FieldComponent<fvcc::VolumeField<NeoFOAM::scalar>>>(0).category == fvcc::FieldCategory::newTime);
+    }
+}
+
+
+TEST_CASE("FieldDatabase")
+{
+
+    NeoFOAM::Executor exec = GENERATE(
+        NeoFOAM::Executor(NeoFOAM::SerialExecutor {}),
+        NeoFOAM::Executor(NeoFOAM::CPUExecutor {}),
+        NeoFOAM::Executor(NeoFOAM::GPUExecutor {})
+    );
+
+    std::string execName = std::visit([](auto e) { return e.print(); }, exec);
+    NeoFOAM::UnstructuredMesh mesh = NeoFOAM::createSingleCellMesh(exec);
+
+    SECTION("store Field: " + execName)
+    {
+        auto vf = createVolumeField(mesh);
+        NeoFOAM::fill(vf.internalField(), 1.0);
+
+        fvcc::FieldDatabase fieldDB {};
+
+        fieldDB.insert("test", 1.0);
+        REQUIRE(fieldDB.get<double>("test") == 1.0);
+
+        fieldDB.insert("vf", vf);
+        auto vf2 = fieldDB.get<fvcc::VolumeField<NeoFOAM::scalar>>("vf").internalField().copyToHost();
+        REQUIRE(vf2.span()[0] == 1.0);
+    }
+
+}
