ad97043c082a86932f93dff018f2b0e4af57b30c
Gregor Olenik <gregor.olenik@web.de>
2024-10-07 14:26:01 +0200

    renamed EqnSystem->Equation

--- test/dsl/operator.cpp
+++ test/dsl/operator.cpp
@@ -2,57 +2,7 @@
 // SPDX-FileCopyrightText: 2023-2024 NeoFOAM authors
 #define CATCH_CONFIG_RUNNER // Define this before including catch.hpp to create
                             // a custom main
-#include <catch2/catch_session.hpp>
-#include <catch2/catch_test_macros.hpp>
-#include <catch2/generators/catch_generators_all.hpp>
-#include <catch2/benchmark/catch_benchmark.hpp>
-
-#include "NeoFOAM/fields/field.hpp"
-#include "NeoFOAM/DSL/coeff.hpp"
-#include "NeoFOAM/DSL/operator.hpp"
-
-using Field = NeoFOAM::Field<NeoFOAM::scalar>;
-using Coeff = NeoFOAM::DSL::Coeff;
-using Operator = NeoFOAM::DSL::Operator;
-using OperatorMixin = NeoFOAM::DSL::OperatorMixin;
-using Executor = NeoFOAM::Executor;
-using VolumeField = fvcc::VolumeField<NeoFOAM::scalar>;
-using BoundaryFields = NeoFOAM::BoundaryFields<NeoFOAM::scalar>;
-namespace fvcc = NeoFOAM::finiteVolume::cellCentred;
-
-/* A dummy implementation of a Operator
- * following the Operator interface */
-class Dummy : public OperatorMixin
-{
-
-public:
-
-    Dummy(const Executor& exec, VolumeField& field) : OperatorMixin(exec), field_(field) {}
-
-    void explicitOperation(Field& source)
-    {
-        auto sourceSpan = source.span();
-        auto fieldSpan = field_.internalField().span();
-        auto coeff = getCoefficient();
-        NeoFOAM::parallelFor(
-            source.exec(),
-            source.range(),
-            KOKKOS_LAMBDA(const size_t i) { sourceSpan[i] += coeff[i] * fieldSpan[i]; }
-        );
-    }
-
-    std::string getName() const { return "Dummy"; }
-
-    const VolumeField& volumeField() const { return field_; }
-
-    VolumeField& volumeField() { return field_; }
-
-    Operator::Type getType() const { return Operator::Type::Explicit; }
-
-private:
-
-    VolumeField& field_;
-};
+#include "common.hpp"
 
 TEST_CASE("Operator")
 {
