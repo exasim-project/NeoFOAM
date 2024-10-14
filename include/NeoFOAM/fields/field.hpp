34fc503e410431dfcfd3ab03c688465f3b3d8912
Gregor Olenik <gregor.olenik@web.de>
2024-10-14 12:02:58 +0200

    improve error message for non matching executors

--- include/NeoFOAM/fields/field.hpp
+++ include/NeoFOAM/fields/field.hpp
@@ -413,7 +413,13 @@ private:
     void validateOtherField(const Field<ValueType>& rhs) const
     {
         NF_DEBUG_ASSERT(size() == rhs.size(), "Fields are not the same size.");
-        NF_DEBUG_ASSERT(exec() == rhs.exec(), "Executors are not the same.");
+
+        std::string execName = std::visit([](auto e) { return e.print(); }, exec());
+        std::string rhsExecName = std::visit([](auto e) { return e.print(); }, rhs.exec());
+        NF_DEBUG_ASSERT(
+            exec() == rhs.exec(),
+            "Executors: " + execName + " and " + rhsExecName + " are not the same"
+        );
     }
 };
 
