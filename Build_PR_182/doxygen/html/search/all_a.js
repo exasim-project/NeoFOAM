50f2bf7cd99cd05d34028b76ea593c9416461d08
HenningScheufler <HenningScheufler@users.noreply.github.com>
2024-12-07 10:26:03 +0000

    Deploying to gh-pages from @ exasim-project/NeoFOAM@f1b76373ba0a12852560d46b1cfcfe0298484c76 🚀

--- Build_PR_182/doxygen/html/search/all_a.js
+++ Build_PR_182/doxygen/html/search/all_a.js
@@ -4,7 +4,7 @@ var searchData=
   ['label_2ehpp_198',['label.hpp',['../label_8hpp.html',1,'']]],
   ['labelfield_199',['labelField',['../namespaceNeoFOAM.html#a476d354d6778b23d4bd3b15eec820f47',1,'NeoFOAM']]],
   ['level_200',['level',['../classNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1OldTimeDocument.html#a0f91cbfbf17e6b558f92f817a7f16cb1',1,'NeoFOAM::finiteVolume::cellCentred::OldTimeDocument::level()'],['../classNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1OldTimeDocument.html#a612ccde5c4ed4593a33afceaad68a5fd',1,'NeoFOAM::finiteVolume::cellCentred::OldTimeDocument::level() const']]],
-  ['linear_201',['Linear',['../classNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1Linear.html#af534d6b3f1567cee8faf492a7fe1a16e',1,'NeoFOAM::finiteVolume::cellCentred::Linear::Linear()'],['../classNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1Linear.html',1,'NeoFOAM::finiteVolume::cellCentred::Linear']]],
+  ['linear_201',['Linear',['../classNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1Linear.html#af534d6b3f1567cee8faf492a7fe1a16e',1,'NeoFOAM::finiteVolume::cellCentred::Linear::Linear(const Executor &amp;exec, const UnstructuredMesh &amp;mesh, Input input)'],['../classNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1Linear.html#a7b24cdc3bd137f5529d24ce35a974a32',1,'NeoFOAM::finiteVolume::cellCentred::Linear::Linear(const Executor &amp;exec, const UnstructuredMesh &amp;mesh)'],['../classNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1Linear.html',1,'NeoFOAM::finiteVolume::cellCentred::Linear']]],
   ['linear_2ehpp_202',['linear.hpp',['../linear_8hpp.html',1,'']]],
   ['localidx_203',['localIdx',['../namespaceNeoFOAM.html#a95525d89d661b88aaa4548bdf2766842',1,'NeoFOAM']]],
   ['logbadanycast_204',['logBadAnyCast',['../namespaceNeoFOAM.html#ad0a06ebc4aa82a65353cdc36eb19810e',1,'NeoFOAM']]],
--- Build_PR_182/doxygen/html/search/functions_a.js
+++ Build_PR_182/doxygen/html/search/functions_a.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['level_686',['level',['../classNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1OldTimeDocument.html#a0f91cbfbf17e6b558f92f817a7f16cb1',1,'NeoFOAM::finiteVolume::cellCentred::OldTimeDocument::level()'],['../classNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1OldTimeDocument.html#a612ccde5c4ed4593a33afceaad68a5fd',1,'NeoFOAM::finiteVolume::cellCentred::OldTimeDocument::level() const']]],
-  ['linear_687',['Linear',['../classNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1Linear.html#af534d6b3f1567cee8faf492a7fe1a16e',1,'NeoFOAM::finiteVolume::cellCentred::Linear']]],
+  ['linear_687',['Linear',['../classNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1Linear.html#af534d6b3f1567cee8faf492a7fe1a16e',1,'NeoFOAM::finiteVolume::cellCentred::Linear::Linear(const Executor &amp;exec, const UnstructuredMesh &amp;mesh, Input input)'],['../classNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1Linear.html#a7b24cdc3bd137f5529d24ce35a974a32',1,'NeoFOAM::finiteVolume::cellCentred::Linear::Linear(const Executor &amp;exec, const UnstructuredMesh &amp;mesh)']]],
   ['logbadanycast_688',['logBadAnyCast',['../namespaceNeoFOAM.html#ad0a06ebc4aa82a65353cdc36eb19810e',1,'NeoFOAM']]],
   ['logoutrange_689',['logOutRange',['../namespaceNeoFOAM.html#af6f9721ae18e83a58215ddfbba7413b1',1,'NeoFOAM']]],
   ['loop_690',['loop',['../classNeoFOAM_1_1Time.html#a97324c456ba7c0d944a4ee8bcf891d75',1,'NeoFOAM::Time']]]
