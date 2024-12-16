de866b2a0e0f6a1dc31a1e241fd9eb7643dd9865
bevanwsjones <bevanwsjones@users.noreply.github.com>
2024-12-16 19:31:44 +0000

    Deploying to gh-pages from @ exasim-project/NeoFOAM@aa7e453f05dbde40f7ba170f103101879205ae7e 🚀

--- Build_PR_81/doxygen/html/search/classes_c.js
+++ Build_PR_81/doxygen/html/search/classes_c.js
@@ -1,14 +1,17 @@
 var searchData=
 [
   ['serialexecutor_0',['SerialExecutor',['../classNeoFOAM_1_1SerialExecutor.html',1,'NeoFOAM']]],
-  ['stencildatabase_1',['StencilDataBase',['../classNeoFOAM_1_1StencilDataBase.html',1,'NeoFOAM']]],
-  ['sumkernel_2',['SumKernel',['../structNeoFOAM_1_1SumKernel.html',1,'NeoFOAM']]],
-  ['surfaceboundary_3',['SurfaceBoundary',['../classNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1SurfaceBoundary.html',1,'NeoFOAM::finiteVolume::cellCentred']]],
-  ['surfaceboundaryfactory_4',['SurfaceBoundaryFactory',['../classNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1SurfaceBoundaryFactory.html',1,'NeoFOAM::finiteVolume::cellCentred']]],
-  ['surfacefield_5',['SurfaceField',['../classNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1SurfaceField.html',1,'NeoFOAM::finiteVolume::cellCentred']]],
-  ['surfacefield_3c_20neofoam_3a_3ascalar_20_3e_6',['SurfaceField&lt; NeoFOAM::scalar &gt;',['../classNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1SurfaceField.html',1,'NeoFOAM::finiteVolume::cellCentred']]],
-  ['surfacefield_3c_20neofoam_3a_3avector_20_3e_7',['SurfaceField&lt; NeoFOAM::Vector &gt;',['../classNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1SurfaceField.html',1,'NeoFOAM::finiteVolume::cellCentred']]],
-  ['surfacefield_3c_20scalar_20_3e_8',['SurfaceField&lt; scalar &gt;',['../classNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1SurfaceField.html',1,'NeoFOAM::finiteVolume::cellCentred']]],
-  ['surfaceinterpolation_9',['SurfaceInterpolation',['../classNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1SurfaceInterpolation.html',1,'NeoFOAM::finiteVolume::cellCentred']]],
-  ['surfaceinterpolationfactory_10',['SurfaceInterpolationFactory',['../classNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1SurfaceInterpolationFactory.html',1,'NeoFOAM::finiteVolume::cellCentred']]]
+  ['skvector_1',['SKVector',['../classNeoFOAM_1_1sundials_1_1SKVector.html',1,'NeoFOAM::sundials']]],
+  ['skvectordefault_2',['SKVectorDefault',['../classNeoFOAM_1_1sundials_1_1SKVectorDefault.html',1,'NeoFOAM::sundials']]],
+  ['skvectorserial_3',['SKVectorSerial',['../classNeoFOAM_1_1sundials_1_1SKVectorSerial.html',1,'NeoFOAM::sundials']]],
+  ['stencildatabase_4',['StencilDataBase',['../classNeoFOAM_1_1StencilDataBase.html',1,'NeoFOAM']]],
+  ['sumkernel_5',['SumKernel',['../structNeoFOAM_1_1SumKernel.html',1,'NeoFOAM']]],
+  ['surfaceboundary_6',['SurfaceBoundary',['../classNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1SurfaceBoundary.html',1,'NeoFOAM::finiteVolume::cellCentred']]],
+  ['surfaceboundaryfactory_7',['SurfaceBoundaryFactory',['../classNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1SurfaceBoundaryFactory.html',1,'NeoFOAM::finiteVolume::cellCentred']]],
+  ['surfacefield_8',['SurfaceField',['../classNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1SurfaceField.html',1,'NeoFOAM::finiteVolume::cellCentred']]],
+  ['surfacefield_3c_20neofoam_3a_3ascalar_20_3e_9',['SurfaceField&lt; NeoFOAM::scalar &gt;',['../classNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1SurfaceField.html',1,'NeoFOAM::finiteVolume::cellCentred']]],
+  ['surfacefield_3c_20neofoam_3a_3avector_20_3e_10',['SurfaceField&lt; NeoFOAM::Vector &gt;',['../classNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1SurfaceField.html',1,'NeoFOAM::finiteVolume::cellCentred']]],
+  ['surfacefield_3c_20scalar_20_3e_11',['SurfaceField&lt; scalar &gt;',['../classNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1SurfaceField.html',1,'NeoFOAM::finiteVolume::cellCentred']]],
+  ['surfaceinterpolation_12',['SurfaceInterpolation',['../classNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1SurfaceInterpolation.html',1,'NeoFOAM::finiteVolume::cellCentred']]],
+  ['surfaceinterpolationfactory_13',['SurfaceInterpolationFactory',['../classNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1SurfaceInterpolationFactory.html',1,'NeoFOAM::finiteVolume::cellCentred']]]
 ];
--- Build_PR_81/doxygen/html/search/functions_b.js
+++ Build_PR_81/doxygen/html/search/functions_b.js
@@ -3,7 +3,8 @@ var searchData=
   ['mag_0',['mag',['../namespaceNeoFOAM.html#a30753699ca02c237b4081286380cabd6',1,'NeoFOAM']]],
   ['magfaceareas_1',['magFaceAreas',['../classNeoFOAM_1_1UnstructuredMesh.html#a75051ce1557fff2fcb282f25b90cf816',1,'NeoFOAM::UnstructuredMesh']]],
   ['magsf_2',['magsf',['../classNeoFOAM_1_1BoundaryMesh.html#a440a8c36013674d9873371d13543aa05',1,'NeoFOAM::BoundaryMesh::magSf() const'],['../classNeoFOAM_1_1BoundaryMesh.html#a3ee8d38788a65bc335e566f182e60600',1,'NeoFOAM::BoundaryMesh::magSf(const localIdx i) const']]],
-  ['map_3',['map',['../namespaceNeoFOAM.html#a33ee4264037f5c9c9d7e2b8db186814b',1,'NeoFOAM']]],
-  ['mesh_4',['mesh',['../classNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1GeometricFieldMixin.html#a18570fe2823eff871124806028b8fe33',1,'NeoFOAM::finiteVolume::cellCentred::GeometricFieldMixin']]],
-  ['mul_5',['mul',['../namespaceNeoFOAM.html#a335407c0e359f3a30480c61fb0aa491f',1,'NeoFOAM']]]
+  ['makeskvector_3',['makeSKVector',['../namespaceNeoFOAM_1_1sundials.html#ab980a8b3bde1a7ec731866c758bd57d5',1,'NeoFOAM::sundials']]],
+  ['map_4',['map',['../namespaceNeoFOAM.html#a33ee4264037f5c9c9d7e2b8db186814b',1,'NeoFOAM']]],
+  ['mesh_5',['mesh',['../classNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1GeometricFieldMixin.html#a18570fe2823eff871124806028b8fe33',1,'NeoFOAM::finiteVolume::cellCentred::GeometricFieldMixin']]],
+  ['mul_6',['mul',['../namespaceNeoFOAM.html#a335407c0e359f3a30480c61fb0aa491f',1,'NeoFOAM']]]
 ];
