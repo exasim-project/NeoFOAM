526988e88ebb1d10a8e9b61d4c8da3de561b8f8b
Roman Mishchuk <romamiykg@gmail.com>
2024-12-17 11:13:26 +0200

    Checking whether docTable contains classes, intead of its size

--- test/core/registerClass.cpp
+++ test/core/registerClass.cpp
@@ -7,31 +7,43 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/generators/catch_generators_adapters.hpp>
 
+#include <set>
+
 #include "testRegister.hpp"
 
 TEST_CASE("Register")
 {
     std::cout << "Table size: " << NeoFOAM::BaseClassDocumentation::docTable().size() << std::endl;
 
-    CHECK(NeoFOAM::BaseClassDocumentation::docTable().size() == 2);
+    std::set<std::string> customClasses = {BaseClass::name(), BaseClass2::name()};
+
+    int presentCustomClasses = 0;
     for (const auto& it : NeoFOAM::BaseClassDocumentation::docTable())
-    {
-        std::string baseClassName = it.first;
-        std::cout << "baseClassName " << baseClassName << std::endl;
-        auto entries = NeoFOAM::BaseClassDocumentation::entries(baseClassName);
-        for (const auto& derivedClass : entries)
+        presentCustomClasses += customClasses.contains(it.first);
+
+    bool customClassesArePresent = presentCustomClasses == customClasses.size();
+
+    CHECK(customClassesArePresent);
+    if (!customClassesArePresent)
+        for (const auto& it : NeoFOAM::BaseClassDocumentation::docTable())
         {
-            std::cout << "   - " << derivedClass << std::endl;
-            std::cout << "     doc: "
-                      << NeoFOAM::BaseClassDocumentation::doc(baseClassName, derivedClass)
-                      << std::endl;
-            std::cout << "     schema: "
-                      << NeoFOAM::BaseClassDocumentation::schema(baseClassName, derivedClass)
-                      << std::endl;
-            CHECK(!NeoFOAM::BaseClassDocumentation::doc(baseClassName, derivedClass).empty());
-            CHECK(!NeoFOAM::BaseClassDocumentation::schema(baseClassName, derivedClass).empty());
+            std::string baseClassName = it.first;
+            std::cout << "baseClassName " << baseClassName << std::endl;
+            auto entries = NeoFOAM::BaseClassDocumentation::entries(baseClassName);
+            for (const auto& derivedClass : entries)
+            {
+                std::cout << "   - " << derivedClass << std::endl;
+                std::cout << "     doc: "
+                          << NeoFOAM::BaseClassDocumentation::doc(baseClassName, derivedClass)
+                          << std::endl;
+                std::cout << "     schema: "
+                          << NeoFOAM::BaseClassDocumentation::schema(baseClassName, derivedClass)
+                          << std::endl;
+                CHECK(!NeoFOAM::BaseClassDocumentation::doc(baseClassName, derivedClass).empty());
+                CHECK(!NeoFOAM::BaseClassDocumentation::schema(baseClassName, derivedClass).empty()
+                );
+            }
         }
-    }
 
     CHECK(BaseClass::table().size() == 1);
     CHECK(BaseClass2::table().size() == 1);
