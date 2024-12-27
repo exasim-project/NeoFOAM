b436679c141727d00c44f031dc1e1443ab9addc7
Gregor Olenik <gregor.olenik@web.de>
2024-12-27 10:41:31 +0100

    remove some maybe_unused

--- include/NeoFOAM/core/parallelAlgorithms.hpp
+++ include/NeoFOAM/core/parallelAlgorithms.hpp
@@ -24,9 +24,7 @@ concept parallelForKernel = requires(Kernel t, size_t i) {
 };
 
 template<typename Executor, parallelForKernel Kernel>
-void parallelFor(
-    [[maybe_unused]] const Executor& exec, std::pair<size_t, size_t> range, Kernel kernel
-)
+void parallelFor(const Executor& exec, std::pair<size_t, size_t> range, Kernel kernel)
 {
     auto [start, end] = range;
     if constexpr (std::is_same<std::remove_reference_t<Executor>, SerialExecutor>::value)
@@ -63,7 +61,7 @@ concept parallelForFieldKernel = requires(Kernel t, ValueType val, size_t i) {
 };
 
 template<typename Executor, typename ValueType, parallelForFieldKernel<ValueType> Kernel>
-void parallelFor([[maybe_unused]] const Executor& exec, Field<ValueType>& field, Kernel kernel)
+void parallelFor(const Executor& exec, Field<ValueType>& field, Kernel kernel)
 {
     auto span = field.span();
     if constexpr (std::is_same<std::remove_reference_t<Executor>, SerialExecutor>::value)
@@ -92,9 +90,7 @@ void parallelFor(Field<ValueType>& field, Kernel kernel)
 
 
 template<typename Executor, typename Kernel, typename T>
-void parallelReduce(
-    [[maybe_unused]] const Executor& exec, std::pair<size_t, size_t> range, Kernel kernel, T& value
-)
+void parallelReduce(const Executor& exec, std::pair<size_t, size_t> range, Kernel kernel, T& value)
 {
     auto [start, end] = range;
     if constexpr (std::is_same<std::remove_reference_t<Executor>, SerialExecutor>::value)
@@ -123,9 +119,7 @@ void parallelReduce(
 
 
 template<typename Executor, typename ValueType, typename Kernel, typename T>
-void parallelReduce(
-    [[maybe_unused]] const Executor& exec, Field<ValueType>& field, Kernel kernel, T& value
-)
+void parallelReduce(const Executor& exec, Field<ValueType>& field, Kernel kernel, T& value)
 {
     if constexpr (std::is_same<std::remove_reference_t<Executor>, SerialExecutor>::value)
     {
