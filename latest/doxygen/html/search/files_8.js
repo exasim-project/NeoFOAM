dcf785e50b9f18df5c86e9823186fff80e82141b
greole <greole@users.noreply.github.com>
2025-01-11 18:12:05 +0000

    Deploying to gh-pages from @ exasim-project/NeoFOAM@ac13fb00efc32b9ee5ee9eb1010866c25ce96e76 🚀

--- latest/doxygen/html/search/files_8.js
+++ latest/doxygen/html/search/files_8.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['label_2ehpp_0',['label.hpp',['../label_8hpp.html',1,'']]],
   ['linear_2ehpp_1',['linear.hpp',['../linear_8hpp.html',1,'']]],
-  ['linearsystem_2ehpp_2',['LinearSystem.hpp',['../LinearSystem_8hpp.html',1,'']]]
+  ['linearalgebra_2ehpp_2',['linearAlgebra.hpp',['../linearAlgebra_8hpp.html',1,'']]],
+  ['linearsystem_2ehpp_3',['linearSystem.hpp',['../linearSystem_8hpp.html',1,'']]]
 ];
