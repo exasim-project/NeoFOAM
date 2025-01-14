5e6458b191a1f87fb24e4c40532755c7d416dc92
Gregor Olenik <gregor.olenik@web.de>
2025-01-14 22:16:53 +0100

    refactor catch parser + field.cpp

--- benchmarks/fields/field.cpp
+++ benchmarks/fields/field.cpp
@@ -37,9 +37,31 @@ int main(int argc, char* argv[])
     return result;
 }
 
-TEST_CASE("[bench] fields")
+TEST_CASE("Field<scalar>::addition", "[bench]")
 {
+    auto size = GENERATE(1 << 16, 1 << 17, 1 << 18, 1 << 19, 1 << 20);
+    NeoFOAM::Executor exec = GENERATE(
+        NeoFOAM::Executor(NeoFOAM::SerialExecutor {}),
+        NeoFOAM::Executor(NeoFOAM::CPUExecutor {}),
+        NeoFOAM::Executor(NeoFOAM::GPUExecutor {})
+    );
+    std::string execName = std::visit([](auto e) { return e.name(); }, exec);
 
+    DYNAMIC_SECTION("" << size)
+    {
+        NeoFOAM::Field<NeoFOAM::scalar> cpuA(exec, size);
+        NeoFOAM::fill(cpuA, 1.0);
+        NeoFOAM::Field<NeoFOAM::scalar> cpuB(exec, size);
+        NeoFOAM::fill(cpuB, 2.0);
+        NeoFOAM::Field<NeoFOAM::scalar> cpuC(exec, size);
+        NeoFOAM::fill(cpuC, 0.0);
+
+        BENCHMARK(std::string(execName)) { return (cpuC = cpuA + cpuB); };
+    }
+}
+
+TEST_CASE("Field<scalar>::multiplication", "[bench]")
+{
     auto size = GENERATE(1 << 16, 1 << 17, 1 << 18, 1 << 19, 1 << 20);
 
     NeoFOAM::Executor exec = GENERATE(
@@ -47,36 +69,17 @@ TEST_CASE("[bench] fields")
         NeoFOAM::Executor(NeoFOAM::CPUExecutor {}),
         NeoFOAM::Executor(NeoFOAM::GPUExecutor {})
     );
-
-    CAPTURE(size); // Capture the value of size
-
     std::string execName = std::visit([](auto e) { return e.name(); }, exec);
 
-    // capture the value of size as section name
     DYNAMIC_SECTION("" << size)
     {
-        {
-            NeoFOAM::Field<NeoFOAM::scalar> cpuA(exec, size);
-            NeoFOAM::fill(cpuA, 1.0);
-            NeoFOAM::Field<NeoFOAM::scalar> cpuB(exec, size);
-            NeoFOAM::fill(cpuB, 2.0);
-            NeoFOAM::Field<NeoFOAM::scalar> cpuC(exec, size);
-            NeoFOAM::fill(cpuC, 0.0);
+        NeoFOAM::Field<NeoFOAM::scalar> cpuA(exec, size);
+        NeoFOAM::fill(cpuA, 1.0);
+        NeoFOAM::Field<NeoFOAM::scalar> cpuB(exec, size);
+        NeoFOAM::fill(cpuB, 2.0);
+        NeoFOAM::Field<NeoFOAM::scalar> cpuC(exec, size);
+        NeoFOAM::fill(cpuC, 0.0);
 
-            BENCHMARK("Field<scalar>::addition on " + execName) { return (cpuC = cpuA + cpuB); };
-        }
-        {
-            NeoFOAM::Field<NeoFOAM::scalar> cpuA(exec, size);
-            NeoFOAM::fill(cpuA, 1.0);
-            NeoFOAM::Field<NeoFOAM::scalar> cpuB(exec, size);
-            NeoFOAM::fill(cpuB, 2.0);
-            NeoFOAM::Field<NeoFOAM::scalar> cpuC(exec, size);
-            NeoFOAM::fill(cpuC, 0.0);
-
-            BENCHMARK("Field<scalar>::multiplication on " + execName)
-            {
-                return (cpuC = cpuA * cpuB);
-            };
-        }
+        BENCHMARK(std::string(execName)) { return (cpuC = cpuA * cpuB); };
     }
 }
