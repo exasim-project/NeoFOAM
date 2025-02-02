82f6f168c49a824820954579315784b2ae9d589b
Henning Scheufler <henning.scheufler@web.de>
2025-02-02 10:01:40 +0100

    moved getGkoExecutor into source file

--- include/NeoFOAM/linearAlgebra/utilities.hpp
+++ include/NeoFOAM/linearAlgebra/utilities.hpp
@@ -14,14 +14,7 @@
 namespace NeoFOAM::la
 {
 
-std::shared_ptr<gko::Executor> getGkoExecutor(Executor exec)
-{
-    return std::visit(
-        [](auto concreteExec)
-        { return gko::ext::kokkos::create_executor(concreteExec.underlyingExec()); },
-        exec
-    );
-}
+std::shared_ptr<gko::Executor> getGkoExecutor(Executor exec);
 
 namespace detail
 {
---
+++ src/linearAlgebra/utilities.cpp
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: 2024-2025 NeoFOAM authors
+
+#pragma once
+
+#include "NeoFOAM/linearAlgebra/utilities.hpp"
+
+
+namespace NeoFOAM::la
+{
+
+std::shared_ptr<gko::Executor> getGkoExecutor(Executor exec)
+{
+    return std::visit(
+        [](auto concreteExec)
+        { return gko::ext::kokkos::create_executor(concreteExec.underlyingExec()); },
+        exec
+    );
+}
+
+}
