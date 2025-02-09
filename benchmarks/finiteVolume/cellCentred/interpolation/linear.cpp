f67fb15242cdd43e690c0ed68221ae93daffb80e
Gregor Olenik <gregor.olenik@web.de>
2025-02-09 16:13:01 +0100

    add linear benchmark

---
+++ benchmarks/finiteVolume/cellCentred/interpolation/linear.cpp
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: 2023 NeoFOAM authors
+
+#define CATCH_CONFIG_RUNNER // Define this before including catch.hpp to create
+                            // a custom main
+
+#include "NeoFOAM/NeoFOAM.hpp"
+#include "../../../catch_main.hpp"
+
+using NeoFOAM::finiteVolume::cellCentred::SurfaceInterpolation;
+using NeoFOAM::finiteVolume::cellCentred::VolumeField;
+using NeoFOAM::finiteVolume::cellCentred::SurfaceField;
+
+TEST_CASE("DivOperator::div", "[bench]")
+{
+    auto size = GENERATE(1 << 16, 1 << 17, 1 << 18, 1 << 19, 1 << 20);
+
+    NeoFOAM::Executor exec = GENERATE(
+        NeoFOAM::Executor(NeoFOAM::SerialExecutor {}),
+        NeoFOAM::Executor(NeoFOAM::CPUExecutor {}),
+        NeoFOAM::Executor(NeoFOAM::GPUExecutor {})
+    );
+
+    std::string execName = std::visit([](auto e) { return e.name(); }, exec);
+    NeoFOAM::UnstructuredMesh mesh = NeoFOAM::create1DUniformMesh(exec, size);
+    auto surfaceBCs = fvcc::createCalculatedBCs<fvcc::SurfaceBoundary<NeoFOAM::scalar>>(mesh);
+
+    auto in = VolumeField<TestType>(exec, "in", mesh, {});
+    auto out = SurfaceField<TestType>(exec, "out", mesh, surfaceBCs);
+
+    fill(in.internalField(), NeoFOAM::one<NeoFOAM::scalar>::value);
+
+    // capture the value of size as section name
+    DYNAMIC_SECTION("" << size)
+    {
+
+        BENCHMARK(std::string(execName)) {return (linear.interpolate(in, out); };
+    }
+}
