00cbe228728ccb26f619d5dae8056ec094854842
Gregor Olenik <gregor.olenik@web.de>
2025-02-09 14:43:14 +0100

    fix failing test, bcs were missing

--- test/finiteVolume/cellCentred/interpolation/linear.cpp
+++ test/finiteVolume/cellCentred/interpolation/linear.cpp
@@ -14,6 +14,9 @@ using NeoFOAM::finiteVolume::cellCentred::SurfaceInterpolation;
 using NeoFOAM::finiteVolume::cellCentred::VolumeField;
 using NeoFOAM::finiteVolume::cellCentred::SurfaceField;
 
+template<typename T>
+using I = std::initializer_list<T>;
+
 TEMPLATE_TEST_CASE("linear", "", NeoFOAM::scalar, NeoFOAM::Vector)
 {
     NeoFOAM::Executor exec = GENERATE(
@@ -26,13 +29,22 @@ TEMPLATE_TEST_CASE("linear", "", NeoFOAM::scalar, NeoFOAM::Vector)
     auto mesh = NeoFOAM::create1DUniformMesh(exec, 10);
     NeoFOAM::Input input = NeoFOAM::TokenList({std::string("linear")});
     auto linear = SurfaceInterpolation(exec, mesh, input);
+    std::vector<fvcc::SurfaceBoundary<TestType>> bcs {};
+    for (auto patchi : I<size_t> {0, 1})
+    {
+        NeoFOAM::Dictionary dict;
+        dict.insert("type", std::string("fixedValue"));
+        dict.insert("fixedValue", NeoFOAM::one<TestType>::value);
+        bcs.push_back(fvcc::SurfaceBoundary<TestType>(mesh, dict, patchi));
+    }
 
     auto in = VolumeField<TestType>(exec, "in", mesh, {});
-    auto out = SurfaceField<TestType>(exec, "out", mesh, {});
+    auto out = SurfaceField<TestType>(exec, "out", mesh, bcs);
 
     fill(in.internalField(), NeoFOAM::one<TestType>::value);
 
     linear.interpolate(in, out);
+    out.correctBoundaryConditions();
 
     for (int i = 0; i < out.internalField().size(); i++)
     {
