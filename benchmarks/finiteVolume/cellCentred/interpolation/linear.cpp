9be203315d54249ff89040ee209f33627e7c30c5
Gregor Olenik <gregor.olenik@web.de>
2025-02-11 13:57:41 +0100

    add templated benchmark case, extract type for json

--- benchmarks/finiteVolume/cellCentred/interpolation/linear.cpp
+++ benchmarks/finiteVolume/cellCentred/interpolation/linear.cpp
@@ -1,20 +1,20 @@
 // SPDX-License-Identifier: MIT
-// SPDX-FileCopyrightText: 2023 NeoFOAM authors
+// SPDX-FileCopyrightText: 2025 NeoFOAM authors
 
 #define CATCH_CONFIG_RUNNER // Define this before including catch.hpp to create
                             // a custom main
 
 #include "NeoFOAM/NeoFOAM.hpp"
 #include "../../../catch_main.hpp"
+#include <catch2/catch_template_test_macros.hpp>
 
 using NeoFOAM::finiteVolume::cellCentred::SurfaceInterpolation;
 using NeoFOAM::finiteVolume::cellCentred::VolumeField;
 using NeoFOAM::finiteVolume::cellCentred::SurfaceField;
 using NeoFOAM::Input;
 
-TEST_CASE("linear", "[bench]")
+TEMPLATE_TEST_CASE("linear", "", NeoFOAM::scalar, NeoFOAM::Vector)
 {
-    using TestType = NeoFOAM::scalar;
     auto size = GENERATE(1 << 16, 1 << 17, 1 << 18, 1 << 19, 1 << 20);
 
     NeoFOAM::Executor exec = GENERATE(
@@ -25,14 +25,14 @@ TEST_CASE("linear", "[bench]")
 
     std::string execName = std::visit([](auto e) { return e.name(); }, exec);
     NeoFOAM::UnstructuredMesh mesh = NeoFOAM::create1DUniformMesh(exec, size);
-    auto surfaceBCs = fvcc::createCalculatedBCs<fvcc::SurfaceBoundary<NeoFOAM::scalar>>(mesh);
+    auto surfaceBCs = fvcc::createCalculatedBCs<fvcc::SurfaceBoundary<TestType>>(mesh);
     Input input = NeoFOAM::TokenList({std::string("linear")});
     auto linear = SurfaceInterpolation(exec, mesh, input);
 
     auto in = VolumeField<TestType>(exec, "in", mesh, {});
     auto out = SurfaceField<TestType>(exec, "out", mesh, surfaceBCs);
 
-    fill(in.internalField(), NeoFOAM::one<NeoFOAM::scalar>::value);
+    fill(in.internalField(), NeoFOAM::one<TestType>::value);
 
     // capture the value of size as section name
     DYNAMIC_SECTION("" << size)
