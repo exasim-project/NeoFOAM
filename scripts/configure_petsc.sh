9f0d552e2bc42330202c8459ebf423c8240f6341
happyKitesurfer <michael.alletto@gmx.de>
2025-02-15 11:18:15 +0100

    updated script to download and configure petsc

--- scripts/configure_petsc.sh
+++ scripts/configure_petsc.sh
@@ -7,6 +7,8 @@
 ## example script how to download, configure, compile and install petsc
 ## see also https://petsc.org/release/install/
 
+CURRENTDIR="$(pwd)"
+
 # clone latest release
 git clone -b release https://gitlab.com/petsc/petsc.git petsc
 cd petsc
@@ -15,7 +17,9 @@ cd petsc
 ./configure --with-64-bit-indices=0 --with-precision=double --with-cuda=1 --with-cuda-dir=/usr/local/cuda-12.3 --with-mpi=1 --with-fc=0 --force -use-gpu-aware-mpi=0 --with-mpi=1 --with-32bits-pci-domain=1 --prefix=opt/petsc --with-cc=mpicc --with-cxx=mpicxx --with-debugging=no --download-kokkos --download-kokkos-kernels --with-kokkos-kernels=1 --with-kokkos=1
 
 # compile petsc
-make PETSC_DIR=/home/michael/testPetscConfgigureCompile/test_script/petsc PETSC_ARCH=arch-linux-c-opt all
+make PETSC_DIR=$CURRENTDIR/petsc PETSC_ARCH=arch-linux-c-opt all
 
 #install petsc
-make PETSC_DIR=/home/michael/testPetscConfgigureCompile/test_script/petsc PETSC_ARCH=arch-linux-c-opt install
+make PETSC_DIR=$CURRENTDIR/petsc PETSC_ARCH=arch-linux-c-opt install
+
+export PKG_CONFIG_PATH=$CURRENTDIR/opt/petsc/lib/pkgconfig
