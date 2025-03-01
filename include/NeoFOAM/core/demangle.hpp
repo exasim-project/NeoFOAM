d231f3c41a9778f0569bf0e51c080a742831355f
Henning Scheufler <henning.scheufler@web.de>
2025-03-01 17:35:32 +0100

    FIX: std::format in logBadAnyCast

--- include/NeoFOAM/core/demangle.hpp
+++ include/NeoFOAM/core/demangle.hpp
@@ -9,6 +9,7 @@
 #include <string>
 #include <any>
 #include <iostream>
+#include <format>
 
 namespace NeoFOAM
 {
@@ -18,11 +19,16 @@ std::string demangle(const char* mangledName);
 template<typename T, typename Container, typename Key>
 void logBadAnyCast(const std::bad_any_cast& e, const Key& key, const Container& data)
 {
-    std::cerr << "Caught a bad_any_cast exception: \n"
-              << std::format("Caught a bad_any_cast exception: key requested: {}, requested type: {}, actual type: {}\n" , 
-              key, 
-              demangle(typeid(T).name()),
-              demangle(data.at(key).type().name()))
+    std::cerr << std::format(
+        "Caught a bad_any_cast exception:\n",
+        "key: {}\n",
+        "requested type: {}\n",
+        "actual type: {}\n",
+        key,
+        demangle(typeid(T).name()),
+        demangle(data.at(key).type().name())
+    ) << e.what()
+              << std::endl;
 }
 
 }
