798ebef91cbb6375ce82b42385a760c016fda02d
happyKitesurfer <michael.alletto@gmx.de>
2025-03-04 00:03:39 +0100

    Kokkos_ENABLE_CUDA_CONSTEXPR=ON required to correct deep_copy from device to host

--- scripts/configure_petsc.sh
+++ scripts/configure_petsc.sh
@@ -14,7 +14,7 @@ git clone -b release https://gitlab.com/petsc/petsc.git petsc
 cd petsc
 
 # configure petsc
-./configure --with-64-bit-indices=0 --with-precision=double --with-cuda=1 --with-cuda-dir=/usr/local/cuda-12.3 --with-mpi=1 --with-fc=0 --force --with-mpi=1 --with-32bits-pci-domain=1 --prefix=opt/petsc --with-cc=mpicc --with-cxx=mpicxx --with-debugging=no --download-kokkos --download-kokkos-kernels --with-kokkos-kernels=1 --with-kokkos=1
+./configure --with-64-bit-indices=0 --with-precision=double --with-cuda=1 --with-cuda-dir=/usr/local/cuda-12.3 --with-mpi=1 --with-fc=0 --force --with-mpi=1 --with-32bits-pci-domain=1 --prefix=opt/petsc --with-cc=mpicc --with-cxx=mpicxx --with-debugging=no --download-kokkos --download-kokkos-kernels --with-kokkos-kernels=1 --with-kokkos=1 --download-kokkos-cmake-arguments=-DKokkos_ENABLE_CUDA_CONSTEXPR=ON
 
 # compile petsc
 make PETSC_DIR=$CURRENTDIR/petsc PETSC_ARCH=arch-linux-c-opt all
