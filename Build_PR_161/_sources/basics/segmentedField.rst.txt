e9b4586fe8ec172749441bbeb2425a90ab55f3ae
greole <greole@users.noreply.github.com>
2025-03-05 10:52:27 +0000

    Deploying to gh-pages from @ exasim-project/NeoFOAM@416ae9a59a7aefc77845ba943777dee6d90dccca 🚀

---
+++ Build_PR_161/_sources/basics/segmentedField.rst.txt
@@ -0,0 +1,55 @@
+.. _fvcc_segmentedFields:
+
+SegmentedField
+^^^^^^^^^^^^^^
+
+SegmentedField is a template class that represents a field divided into multiple segments and can represent vector of vector of a defined ValueType.
+It also allows the definition of an IndexType, so each segment of the vector can be addressed.
+It can be used to represent cell to cell stencil.
+
+.. code-block:: cpp
+
+    NeoFOAM::Field<NeoFOAM::label> values(exec, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9});
+    NeoFOAM::Field<NeoFOAM::localIdx> segments(exec, {0, 2, 4, 6, 8, 10});
+
+    NeoFOAM::SegmentedField<NeoFOAM::label, NeoFOAM::localIdx> segField(values, segments);
+    auto [valueSpan, segment] = segField.spans();
+    auto segView = segField.view();
+    NeoFOAM::Field<NeoFOAM::label> result(exec, 5);
+
+    NeoFOAM::fill(result, 0);
+    auto resultSpan = result.span();
+
+    parallelFor(
+        exec,
+        {0, segField.numSegments()},
+        KOKKOS_LAMBDA(const size_t segI) {
+            // check if it works with bounds
+            auto [bStart, bEnd] = segView.bounds(segI);
+            auto bVals = valueSpan.subspan(bStart, bEnd - bStart);
+            for (auto& val : bVals)
+            {
+                resultSpan[segI] += val;
+            }
+
+            // check if it works with range
+            auto [rStart, rLength] = segView.range(segI);
+            auto rVals = valueSpan.subspan(rStart, rLength);
+            for (auto& val : rVals)
+            {
+                resultSpan[segI] += val;
+            }
+
+            // check with subspan
+            auto vals = segView.span(segI);
+            for (auto& val : vals)
+            {
+                resultSpan[segI] += val;
+            }
+        }
+    );
+
+In this example, each of the five segments would have a size of two.
+This data allows the representation of stencils in a continuous memory layout, which can be beneficial for performance optimization in numerical simulations especially on GPUs.
+
+The spans method return the value and segment span and it is also possible to return a view that can also be called on a device
---
+++ Build_PR_161/doxygen/html/search/namespaces_0.js
@@ -0,0 +1,20 @@
+var searchData=
+[
+  ['neofoam_0',['NeoFOAM',['../namespaceNeoFOAM.html',1,'']]],
+  ['neofoam_3a_3adetail_1',['detail',['../namespaceNeoFOAM_1_1detail.html',1,'NeoFOAM']]],
+  ['neofoam_3a_3adsl_2',['dsl',['../namespaceNeoFOAM_1_1dsl.html',1,'NeoFOAM']]],
+  ['neofoam_3a_3adsl_3a_3adetail_3',['detail',['../namespaceNeoFOAM_1_1dsl_1_1detail.html',1,'NeoFOAM::dsl']]],
+  ['neofoam_3a_3adsl_3a_3aexp_4',['exp',['../namespaceNeoFOAM_1_1dsl_1_1exp.html',1,'NeoFOAM::dsl']]],
+  ['neofoam_3a_3adsl_3a_3aimp_5',['imp',['../namespaceNeoFOAM_1_1dsl_1_1imp.html',1,'NeoFOAM::dsl']]],
+  ['neofoam_3a_3adsl_3a_3atemporal_6',['temporal',['../namespaceNeoFOAM_1_1dsl_1_1temporal.html',1,'NeoFOAM::dsl']]],
+  ['neofoam_3a_3afinitevolume_7',['finiteVolume',['../namespaceNeoFOAM_1_1finiteVolume.html',1,'NeoFOAM']]],
+  ['neofoam_3a_3afinitevolume_3a_3acellcentred_8',['cellCentred',['../namespaceNeoFOAM_1_1finiteVolume_1_1cellCentred.html',1,'NeoFOAM::finiteVolume']]],
+  ['neofoam_3a_3afinitevolume_3a_3acellcentred_3a_3asurfaceboundary_9',['surfaceBoundary',['../namespaceNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1surfaceBoundary.html',1,'NeoFOAM::finiteVolume::cellCentred']]],
+  ['neofoam_3a_3afinitevolume_3a_3acellcentred_3a_3asurfaceboundary_3a_3adetail_10',['detail',['../namespaceNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1surfaceBoundary_1_1detail.html',1,'NeoFOAM::finiteVolume::cellCentred::surfaceBoundary']]],
+  ['neofoam_3a_3afinitevolume_3a_3acellcentred_3a_3avolumeboundary_11',['volumeBoundary',['../namespaceNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1volumeBoundary.html',1,'NeoFOAM::finiteVolume::cellCentred']]],
+  ['neofoam_3a_3afinitevolume_3a_3acellcentred_3a_3avolumeboundary_3a_3adetail_12',['detail',['../namespaceNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1volumeBoundary_1_1detail.html',1,'NeoFOAM::finiteVolume::cellCentred::volumeBoundary']]],
+  ['neofoam_3a_3ala_13',['la',['../namespaceNeoFOAM_1_1la.html',1,'NeoFOAM']]],
+  ['neofoam_3a_3asundials_14',['sundials',['../namespaceNeoFOAM_1_1sundials.html',1,'NeoFOAM']]],
+  ['neofoam_3a_3asundials_3a_3adetail_15',['detail',['../namespaceNeoFOAM_1_1sundials_1_1detail.html',1,'NeoFOAM::sundials']]],
+  ['neofoam_3a_3atimeintegration_16',['timeIntegration',['../namespaceNeoFOAM_1_1timeIntegration.html',1,'NeoFOAM']]]
+];
