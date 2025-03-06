71b7cefc31585a9f219bc7cf63662879464128cb
bevanwsjones <bevanwsjones@users.noreply.github.com>
2025-03-06 17:52:07 +0000

    Deploying to gh-pages from @ exasim-project/NeoFOAM@338cc9f6c4e79ecf64230175407ead018344a914 🚀

--- Build_PR_268/doxygen/html/search/variables_0.js
+++ Build_PR_268/doxygen/html/search/variables_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['base_0',['Base',['../classNeoFOAM_1_1RuntimeSelectionFactory_3_01Base_00_01Parameters_3_01Args_8_8_8_01_4_01_4.html#a14385b85ff354c53958b8c18d09dd257',1,'NeoFOAM::RuntimeSelectionFactory&lt; Base, Parameters&lt; Args... &gt; &gt;']]]
+  ['a_0',['A',['../structNeoFOAM_1_1la_1_1LinearSystemView.html#a4d409fdeb3b6c5ec3339177ce24e2c17',1,'NeoFOAM::la::LinearSystemView']]]
 ];
