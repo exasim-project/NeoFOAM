9a836662bb9a114e9eb306626401f410b1dabdb8
Gregor Olenik <gregor.olenik@web.de>
2025-03-11 11:41:42 +0100

    revert string creation

--- include/NeoFOAM/core/span.hpp
+++ include/NeoFOAM/core/span.hpp
@@ -36,7 +36,8 @@ public:
 #ifdef NF_DEBUG
         if (index >= this->size())
         {
-            std::string msg("Index is out of range. Index: " + std::to_string(index));
+            std::string msg;
+            msg += "Index is out of range. Index: " + std::to_string(index);
             if (abort)
             {
                 Kokkos::abort(msg.c_str());
