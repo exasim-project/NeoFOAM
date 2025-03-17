2a716c218c3f7ebd74c21af9bbb4b7e4e7a2e7c6
greole <greole@users.noreply.github.com>
2025-03-17 09:26:08 +0000

    Deploying to gh-pages from @ exasim-project/NeoFOAM@3ee565c37723653b540a68c42558cede4430b7f4 🚀

--- Build_PR_254/doxygen/html/search/variables_2.js
+++ Build_PR_254/doxygen/html/search/variables_2.js
@@ -1,7 +1,5 @@
 var searchData=
 [
-  ['db_5f_0',['db_',['../classNeoFOAM_1_1CollectionMixin.html#a6d93ee4df562eb900011d15f67462b13',1,'NeoFOAM::CollectionMixin']]],
-  ['derivedclass_1',['derivedClass',['../classNeoFOAM_1_1RuntimeSelectionFactory_3_01Base_00_01Parameters_3_01Args_8_8_8_01_4_01_4_1_1Register.html#ab938b12aec58fa3555e7dc906ecb3a63',1,'NeoFOAM::RuntimeSelectionFactory&lt; Base, Parameters&lt; Args... &gt; &gt;::Register']]],
-  ['doc_2',['doc',['../classNeoFOAM_1_1BaseClassData.html#a9f8e7568f339f93d75cd51d79d0d46f6',1,'NeoFOAM::BaseClassData::doc'],['../classNeoFOAM_1_1DerivedClassDocumentation.html#ae856a3541d5917dab6a7c6c2cf553d9b',1,'NeoFOAM::DerivedClassDocumentation::doc']]],
-  ['docs_5f_3',['docs_',['../classNeoFOAM_1_1CollectionMixin.html#a1522617c4fb1edcc03aced8a87b8e38a',1,'NeoFOAM::CollectionMixin']]]
+  ['coeffs_5f_0',['coeffs_',['../classNeoFOAM_1_1dsl_1_1OperatorMixin.html#a2dd8768f6c39dfa1635192aa98c4766d',1,'NeoFOAM::dsl::OperatorMixin']]],
+  ['control_5fdict_5fname_1',['CONTROL_DICT_NAME',['../classNeoFOAM_1_1Time.html#a993a30f96f2f961ce76bc92a922396a3',1,'NeoFOAM::Time']]]
 ];
