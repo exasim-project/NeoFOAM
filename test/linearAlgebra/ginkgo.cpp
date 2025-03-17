51e69782c78fd8cb5ac93165f015a6a392e62ac4
Marcel Koch <marcel.koch@kit.edu>
2025-03-17 09:58:35 +0100

    use operator== from ginkgo

--- test/linearAlgebra/ginkgo.cpp
+++ test/linearAlgebra/ginkgo.cpp
@@ -10,83 +10,6 @@
 
 #if NF_WITH_GINKGO
 
-bool operator==(const gko::config::pnode& a, const gko::config::pnode& b)
-{
-    using tag_t = gko::config::pnode::tag_t;
-
-    if (a.get_tag() != b.get_tag())
-    {
-        return false;
-    }
-
-    if (a.get_tag() == tag_t::array)
-    {
-        const auto& aArr = a.get_array();
-        const auto& bArr = b.get_array();
-        if (aArr.size() != bArr.size())
-        {
-            return false;
-        }
-        for (std::size_t i = 0; i < aArr.size(); ++i)
-        {
-            if (!(aArr[i] == bArr[i]))
-            {
-                return false;
-            }
-        }
-    }
-    if (a.get_tag() == tag_t::boolean)
-    {
-        return a.get_boolean() == b.get_boolean();
-    }
-    if (a.get_tag() == tag_t::integer)
-    {
-        return a.get_integer() == b.get_integer();
-    }
-    if (a.get_tag() == tag_t::map)
-    {
-        const auto& aMap = a.get_map();
-        const auto& bMap = b.get_map();
-        if (aMap.size() != bMap.size())
-        {
-            return false;
-        }
-        for (const auto& [key, value] : aMap)
-        {
-            if (!bMap.contains(key))
-            {
-                return false;
-            }
-            if (!(bMap.at(key) == value))
-            {
-                return false;
-            }
-        }
-    }
-    if (a.get_tag() == tag_t::real)
-    {
-        return a.get_real() == b.get_real();
-    }
-    if (a.get_tag() == tag_t::string)
-    {
-        return a.get_string() == b.get_string();
-    }
-
-    return true;
-}
-
-struct EqualsPnodeMatcher : Catch::Matchers::MatcherGenericBase
-{
-    EqualsPnodeMatcher(const gko::config::pnode& node) : node_ {node} {}
-
-    bool match(const gko::config::pnode& other) const { return node_ == other; }
-
-    std::string describe() const override { return "Equals: to node"; }
-
-private:
-
-    const gko::config::pnode& node_;
-};
 
 TEST_CASE("Dictionary Parsing - Ginkgo")
 {
@@ -97,7 +20,7 @@ TEST_CASE("Dictionary Parsing - Ginkgo")
         auto node = NeoFOAM::la::ginkgo::parse(dict);
 
         gko::config::pnode expected({{"key", gko::config::pnode {"value"}}});
-        CHECK_THAT(node, EqualsPnodeMatcher(expected));
+        CHECK(node == expected);
     }
     SECTION("Const Char *")
     {
@@ -106,7 +29,7 @@ TEST_CASE("Dictionary Parsing - Ginkgo")
         auto node = NeoFOAM::la::ginkgo::parse(dict);
 
         gko::config::pnode expected({{"key", gko::config::pnode {"value"}}});
-        CHECK_THAT(node, EqualsPnodeMatcher(expected));
+        CHECK(node == expected);
     }
     SECTION("Int")
     {
@@ -115,7 +38,7 @@ TEST_CASE("Dictionary Parsing - Ginkgo")
         auto node = NeoFOAM::la::ginkgo::parse(dict);
 
         gko::config::pnode expected({{"key", gko::config::pnode {10}}});
-        CHECK_THAT(node, EqualsPnodeMatcher(expected));
+        CHECK(node == expected);
     }
     SECTION("Double")
     {
@@ -124,7 +47,7 @@ TEST_CASE("Dictionary Parsing - Ginkgo")
         auto node = NeoFOAM::la::ginkgo::parse(dict);
 
         gko::config::pnode expected({{"key", gko::config::pnode {1.0}}});
-        CHECK_THAT(node, EqualsPnodeMatcher(expected));
+        CHECK(node == expected);
     }
     SECTION("Float")
     {
@@ -133,7 +56,7 @@ TEST_CASE("Dictionary Parsing - Ginkgo")
         auto node = NeoFOAM::la::ginkgo::parse(dict);
 
         gko::config::pnode expected({{"key", gko::config::pnode {1.0f}}});
-        CHECK_THAT(node, EqualsPnodeMatcher(expected));
+        CHECK(node == expected);
     }
     SECTION("Dict")
     {
@@ -145,7 +68,7 @@ TEST_CASE("Dictionary Parsing - Ginkgo")
         gko::config::pnode expected(
             {{"key", gko::config::pnode({{"key", gko::config::pnode {"value"}}})}}
         );
-        CHECK_THAT(node, EqualsPnodeMatcher(expected));
+        CHECK(node == expected);
     }
     SECTION("Throws")
     {
