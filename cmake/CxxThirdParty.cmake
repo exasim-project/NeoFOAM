4ba588063c65fdfe5e9fb7d6fa60b3a1aadde81f
gregorweiss <52161555+gregorweiss@users.noreply.github.com>
2025-03-21 10:01:29 +0100

    Thirdparty configuration for ADIOS2 (#280)
    * add adios2 in configuration and thirdparty
    
    * configure ADIOS2 for Kokkos support (Kokkos targets must be installed)
    
    * set Kokkos_DIR
    
    * add patch to bypass find_package(Kokkos) in ADIOS2 config
    
    * add licensing information in patch
    
    * Update cmake/CxxThirdParty.cmake
    
    Co-authored-by: Gregor Olenik <gregor.olenik@web.de>
    
    ---------
    
    Co-authored-by: Gregor Olenik <gregor.olenik@web.de>

--- cmake/CxxThirdParty.cmake
+++ cmake/CxxThirdParty.cmake
@@ -50,6 +50,45 @@ cpmaddpackage(
   https://github.com/nlohmann/json/releases/download/v3.11.3/include.zip
   SYSTEM)
 
+if(${NEOFOAM_WITH_ADIOS2})
+
+  set(ADIOS2_KOKKOS_PATCH git apply ${CMAKE_CURRENT_SOURCE_DIR}/cmake/patches/adios2_kokkos.patch)
+
+  set(ADIOS2_OPTIONS
+      "BUILD_TYPE ${CMAKE_BUILD_TYPE}"
+      "ADIOS2_USE_Kokkos ON"
+      "Kokkos_DIR ${Kokkos_BINARY_DIR}"
+      "ADIOS2_USE_Fortran OFF"
+      "ADIOS2_USE_Python OFF"
+      "ADIOS2_USE_MHS OFF"
+      "ADIOS2_USE_SST OFF"
+      "ADIOS2_BUILD_EXAMPLES OFF"
+      "BUILD_TESTING OFF"
+      "ADIOS2_USE_Profiling OFF")
+
+  if(WIN32)
+    list(APPEND ADIOS2_OPTIONS "BUILD_STATIC_LIBS ON")
+    list(APPEND ADIOS2_OPTIONS "BUILD_SHARED_LIBS OFF")
+  else()
+    list(APPEND ADIOS2_OPTIONS "BUILD_STATIC_LIBS OFF")
+    list(APPEND ADIOS2_OPTIONS "BUILD_SHARED_LIBS ON")
+  endif()
+
+  cpmaddpackage(
+    NAME
+    adios2
+    GITHUB_REPOSITORY
+    ornladios/ADIOS2
+    PATCH_COMMAND
+    ${ADIOS2_KOKKOS_PATCH}
+    VERSION
+    2.10.2
+    OPTIONS
+    ${ADIOS2_OPTIONS}
+    ${ADIOS2_CUDA_OPTIONS}
+    SYSTEM)
+endif()
+
 if(${NEOFOAM_WITH_SUNDIALS})
 
   set(SUNDIALS_OPTIONS
