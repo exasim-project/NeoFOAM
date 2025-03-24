8b13bab67e0747fa9e3da3110f6138e052d6ec45
Gregor Olenik <gregor.olenik@web.de>
2025-03-24 12:54:38 +0100

    move implementation to src

--- src/finiteVolume/cellCentred/operators/sourceTerm.cpp
+++ src/finiteVolume/cellCentred/operators/sourceTerm.cpp
@@ -7,6 +7,49 @@
 
 namespace NeoFOAM::finiteVolume::cellCentred
 {
+template<typename ValueType>
+SourceTerm<ValueType>::SourceTerm(
+    dsl::Operator::Type termType, VolumeField<scalar>& coefficients, VolumeField<ValueType>& field
+)
+    : dsl::OperatorMixin<VolumeField<ValueType>>(field.exec(), dsl::Coeff(1.0), field, termType),
+      coefficients_(coefficients), sparsityPattern_(SparsityPattern::readOrCreate(field.mesh())) {};
+
+template<typename ValueType>
+void SourceTerm<ValueType>::explicitOperation(Field<ValueType>& source) const
+{
+    auto operatorScaling = this->getCoefficient();
+    // const auto vol = coefficients_.mesh().cellVolumes().span();
+    auto [sourceSpan, fieldSpan, coeff] =
+        spans(source, this->field_.internalField(), coefficients_.internalField());
+    NeoFOAM::parallelFor(
+        source.exec(),
+        source.range(),
+        KOKKOS_LAMBDA(const size_t celli) {
+            sourceSpan[celli] += operatorScaling[celli] * coeff[celli] * fieldSpan[celli];
+        }
+    );
+}
+
+template<typename ValueType>
+void SourceTerm<ValueType>::implicitOperation(la::LinearSystem<ValueType, localIdx>& ls) const
+{
+    const auto operatorScaling = this->getCoefficient();
+    const auto vol = coefficients_.mesh().cellVolumes().span();
+    const auto [diagOffs, coeff] =
+        spans(sparsityPattern_->diagOffset(), coefficients_.internalField());
+    auto [A, b] = ls.view();
+
+    NeoFOAM::parallelFor(
+        ls.exec(),
+        {0, coeff.size()},
+        KOKKOS_LAMBDA(const size_t celli) {
+            std::size_t idx = A.rowOffset[celli] + diagOffs[celli];
+            A.value[idx] += operatorScaling[celli] * coeff[celli] * vol[celli] * one<ValueType>();
+        }
+    );
+}
+
+
 // instantiate the template class
 template class SourceTerm<scalar>;
 template class SourceTerm<Vector>;
