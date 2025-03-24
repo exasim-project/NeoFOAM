e3d0b1efc6ae87f7d07c134f0352f527865a9e27
greole <greole@users.noreply.github.com>
2025-03-24 07:40:34 +0000

    Deploying to gh-pages from @ exasim-project/NeoFOAM@4941fd37220eeb6a5baed6aa99c38141d2db393d 🚀

--- Build_PR_288/doxygen/html/search/classes_2.js
+++ Build_PR_288/doxygen/html/search/classes_2.js
@@ -10,7 +10,6 @@ var searchData=
   ['cpuexecutor_7',['CPUExecutor',['../classNeoFOAM_1_1CPUExecutor.html',1,'NeoFOAM']]],
   ['createfromexistingfield_8',['CreateFromExistingField',['../classNeoFOAM_1_1finiteVolume_1_1cellCentred_1_1CreateFromExistingField.html',1,'NeoFOAM::finiteVolume::cellCentred']]],
   ['csrmatrix_9',['CSRMatrix',['../classNeoFOAM_1_1la_1_1CSRMatrix.html',1,'NeoFOAM::la']]],
-  ['csrmatrix_3c_20neofoam_3a_3ascalar_2c_20neofoam_3a_3alocalidx_20_3e_10',['CSRMatrix&lt; NeoFOAM::scalar, NeoFOAM::localIdx &gt;',['../classNeoFOAM_1_1la_1_1CSRMatrix.html',1,'NeoFOAM::la']]],
-  ['csrmatrix_3c_20valuetype_2c_20localidx_20_3e_11',['CSRMatrix&lt; ValueType, localIdx &gt;',['../classNeoFOAM_1_1la_1_1CSRMatrix.html',1,'NeoFOAM::la']]],
-  ['csrmatrixview_12',['CSRMatrixView',['../structNeoFOAM_1_1la_1_1CSRMatrixView.html',1,'NeoFOAM::la']]]
+  ['csrmatrix_3c_20valuetype_2c_20localidx_20_3e_10',['CSRMatrix&lt; ValueType, localIdx &gt;',['../classNeoFOAM_1_1la_1_1CSRMatrix.html',1,'NeoFOAM::la']]],
+  ['csrmatrixview_11',['CSRMatrixView',['../structNeoFOAM_1_1la_1_1CSRMatrixView.html',1,'NeoFOAM::la']]]
 ];
