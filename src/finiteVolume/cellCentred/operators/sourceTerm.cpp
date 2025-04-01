a100e349a08d0ecd09f59a04e6f3184a525eb1fd
bevanwsjones <bevanwsjones@gmail.com>
2025-04-01 21:23:32 +0200

    - CSRMatrixView's rowOffset renamed to rowOffs.

--- src/finiteVolume/cellCentred/operators/sourceTerm.cpp
+++ src/finiteVolume/cellCentred/operators/sourceTerm.cpp
@@ -43,7 +43,7 @@ void SourceTerm<ValueType>::implicitOperation(la::LinearSystem<ValueType, localI
         ls.exec(),
         {0, coeff.size()},
         KOKKOS_LAMBDA(const size_t celli) {
-            std::size_t idx = A.rowOffset[celli] + diagOffs[celli];
+            std::size_t idx = A.rowOffs[celli] + diagOffs[celli];
             A.values[idx] += operatorScaling[celli] * coeff[celli] * vol[celli] * one<ValueType>();
         }
     );
