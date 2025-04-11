33fcc1c234563f378161867bb22de9b247c2e3eb
greole <greole@users.noreply.github.com>
2025-04-11 12:45:44 +0000

    Deploying to gh-pages from @ exasim-project/NeoFOAM@907632352053f4a2350a6a63b20624a60c89fb40 🚀

--- latest/doxygen/html/search/files_a.js
+++ latest/doxygen/html/search/files_a.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['parallelalgorithms_2ehpp_0',['parallelAlgorithms.hpp',['../parallelAlgorithms_8hpp.html',1,'']]]
+  ['parallelalgorithms_2ehpp_0',['parallelAlgorithms.hpp',['../parallelAlgorithms_8hpp.html',1,'']]],
+  ['petsc_2ehpp_1',['petsc.hpp',['../petsc_8hpp.html',1,'']]],
+  ['petscsolvercontext_2ehpp_2',['petscSolverContext.hpp',['../petscSolverContext_8hpp.html',1,'']]]
 ];
--- latest/doxygen/html/search/files_c.js
+++ latest/doxygen/html/search/files_c.js
@@ -3,7 +3,7 @@ var searchData=
   ['scalar_2ehpp_0',['scalar.hpp',['../scalar_8hpp.html',1,'']]],
   ['segmentedfield_2ehpp_1',['segmentedField.hpp',['../segmentedField_8hpp.html',1,'']]],
   ['serialexecutor_2ehpp_2',['serialExecutor.hpp',['../serialExecutor_8hpp.html',1,'']]],
-  ['solver_2ehpp_3',['solver.hpp',['../solver_8hpp.html',1,'']]],
+  ['solver_2ehpp_3',['solver.hpp',['../dsl_2solver_8hpp.html',1,'(Global Namespace)'],['../linearAlgebra_2solver_8hpp.html',1,'(Global Namespace)']]],
   ['sourceterm_2ehpp_4',['sourceTerm.hpp',['../sourceTerm_8hpp.html',1,'']]],
   ['span_2ehpp_5',['span.hpp',['../span_8hpp.html',1,'']]],
   ['sparsitypattern_2ehpp_6',['sparsityPattern.hpp',['../sparsityPattern_8hpp.html',1,'']]],
