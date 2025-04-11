bfba51222c17b335cf05935e8e63d1f865cb7594
greole <greole@users.noreply.github.com>
2025-04-11 11:39:41 +0000

    Deploying to gh-pages from @ exasim-project/NeoFOAM@eb3dbfc950f159cf3f8fe62ea3198afa13117bff 🚀

--- Build_PR_295/doxygen/html/search/files_a.js
+++ Build_PR_295/doxygen/html/search/files_a.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['parallelalgorithms_2ehpp_0',['parallelAlgorithms.hpp',['../parallelAlgorithms_8hpp.html',1,'']]],
-  ['pressurevelocitycoupling_2ehpp_1',['pressureVelocityCoupling.hpp',['../pressureVelocityCoupling_8hpp.html',1,'']]]
+  ['petsc_2ehpp_1',['petsc.hpp',['../petsc_8hpp.html',1,'']]],
+  ['petscsolvercontext_2ehpp_2',['petscSolverContext.hpp',['../petscSolverContext_8hpp.html',1,'']]]
 ];
