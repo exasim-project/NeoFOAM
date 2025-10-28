8013a5cc0038e11618190785a2e8d7194fc7785b
Chih-Ta Wang <chihta.wang@tum.de>
2025-10-28 15:54:08 +0100

    Update the rules to distinguish trigger sources (FoamAdapter or NeoN)

--- ci/github/trigger_pipeline.sh
+++ ci/github/trigger_pipeline.sh
@@ -59,6 +59,7 @@ echo "Triggering new CI pipeline on branch $BRANCH in project: $GROUP/$PROJECT"
 # Prepare curl form data for variables
 FORM_DATA="--form ref=$BRANCH --form token=$TRIGGER_TOKEN"
 FORM_DATA="$FORM_DATA --form variables[NEON_BRANCH]=$NEON_BRANCH"
+FORM_DATA="$FORM_DATA --form variables[TRIGGER_SOURCE]=$PROJECT"
 
 for var in $VARIABLES; do
   FORM_DATA="$FORM_DATA --form $var"
