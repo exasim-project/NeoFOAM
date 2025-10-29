5fc4cba6f21b8bb58182c07da4fda4f999692125
Chih-Ta Wang <chihta.wang@tum.de>
2025-10-29 11:37:42 +0100

    Make it clear which NeoN branch will be used for building FoamAdapter

--- ci/github/trigger_pipeline.sh
+++ ci/github/trigger_pipeline.sh
@@ -54,7 +54,8 @@ fi
 # -----------------------------------------------------------------------------
 # Trigger FoamAdapter pipeline
 # -----------------------------------------------------------------------------
-echo "Triggering new CI pipeline on branch $BRANCH in project: $GROUP/$PROJECT"
+echo "Triggering a new CI pipeline on branch $BRANCH in project: $GROUP/$PROJECT \
+with NeoN branch: $NEON_BRANCH"
 
 # Prepare curl form data for variables
 FORM_DATA="--form ref=$BRANCH --form token=$TRIGGER_TOKEN"
