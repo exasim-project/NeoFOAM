6b654bd91e7c9c2c1802b571ad98eba68ce8d228
chihtaw <chihtaw@users.noreply.github.com>
2025-11-20 15:11:33 +0000

    Deploying to gh-pages from @ exasim-project/NeoFOAM@2b6d2e1f32db298c74a8691e61c45eed93b8a8a8 🚀

---
+++ Build_PR_169/doxygen/html/search/functions_d.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['setdeltat_0',['setDeltaT',['../namespaceNeoFOAM.html#a39bc76acefb5a1dd3074b0feba5864eb',1,'NeoFOAM']]],
+  ['setreference_1',['setreference',['../structNeoFOAM_1_1PDESolver_1_1SetReference.html#a8871e71d55b49e4309d2acc9207825dd',1,'NeoFOAM::PDESolver::SetReference::SetReference()'],['../classNeoFOAM_1_1PDESolver.html#aa632d298feb601ef802ba3b85b2dc8ed',1,'NeoFOAM::PDESolver::setReference(NeoN::localIdx pRefCell, NeoN::scalar pRefValue)']]],
+  ['solve_2',['solve',['../classNeoFOAM_1_1PDESolver.html#a0464b266cbae77cbfa7f4ffec79e6acb',1,'NeoFOAM::PDESolver::solve()'],['../classNeoFOAM_1_1PDESolver.html#a7f4464b4305040de5ace88df987c05df',1,'NeoFOAM::PDESolver::solve(dsl::SpatialOperator&lt; NeoN::Vec3 &gt; &amp;&amp;rhs)']]],
+  ['sparsitypattern_3',['sparsityPattern',['../classNeoFOAM_1_1PDESolver.html#a8e5fb73b6f8356252df9d57badb62f66',1,'NeoFOAM::PDESolver']]]
+];
