a6fd189f8af1ae4d3a56f02feff93fe798c8bf03
Gregor Olenik <gregor.olenik@web.de>
2025-11-21 09:39:53 +0100

    safely retrieve numerical values

--- src/compatibility/fvSolution.cpp
+++ src/compatibility/fvSolution.cpp
@@ -105,6 +105,15 @@ void updatePreconditioner(NeoN::Dictionary& solverDict)
 
 void updateCriteria(NeoN::Dictionary& solverDict)
 {
+    // parse given dictionary, get numeric value of key in a safe way
+    auto safeRetrieveScalarAndRemove = [](NeoN::Dictionary& d, std::string key)
+    {
+        NeoN::scalar ret =
+            (d.isType<int>(key)) ? NeoN::scalar(d.get<int>(key)) : d.get<NeoN::scalar>(key);
+        d.remove(key);
+        return ret;
+    };
+
     // Ensure the criteria dictionary exists
     if (!solverDict.contains("criteria"))
     {
@@ -120,9 +129,8 @@ void updateCriteria(NeoN::Dictionary& solverDict)
     // Set default values for relative residual norm and iteration count
     if (solverDict.contains("relTol"))
     {
-        NeoN::Dictionary& criteriaDict = solverDict.subDict("criteria");
-        criteriaDict.insert("relative_residual_norm", solverDict.get<NeoN::scalar>("relTol"));
-        solverDict.remove("relTol");
+        NeoN::Dictionary& d = solverDict.subDict("criteria");
+        d.insert("relative_residual_norm", safeRetrieveScalarAndRemove(solverDict, "relTol"));
     }
     if (solverDict.contains("maxIter"))
     {
@@ -132,9 +140,8 @@ void updateCriteria(NeoN::Dictionary& solverDict)
     }
     if (solverDict.contains("tolerance"))
     {
-        NeoN::Dictionary& criteriaDict = solverDict.subDict("criteria");
-        criteriaDict.insert("absolute_residual_norm", solverDict.get<NeoN::scalar>("tolerance"));
-        solverDict.remove("tolerance");
+        NeoN::Dictionary& d = solverDict.subDict("criteria");
+        d.insert("absolute_residual_norm", safeRetrieveScalarAndRemove(solverDict, "tolerance"));
     }
 
     NeoN::Dictionary& criteriaDict = solverDict.subDict("criteria");
