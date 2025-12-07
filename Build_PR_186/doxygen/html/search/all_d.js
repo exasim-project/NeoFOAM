cdd3ac32cf41eded7353ab5f59308d0ed7295756
greole <greole@users.noreply.github.com>
2025-12-07 14:19:02 +0000

    Deploying to gh-pages from @ exasim-project/NeoFOAM@614ab949a86f2f2b832f0025abe4ddb377e4f83f 🚀

--- Build_PR_186/doxygen/html/search/all_d.js
+++ Build_PR_186/doxygen/html/search/all_d.js
@@ -7,6 +7,6 @@ var searchData=
   ['solve_4',['solve',['../classNeoFOAM_1_1PDESolver.html#a0464b266cbae77cbfa7f4ffec79e6acb',1,'NeoFOAM::PDESolver::solve()'],['../classNeoFOAM_1_1PDESolver.html#a7f4464b4305040de5ace88df987c05df',1,'NeoFOAM::PDESolver::solve(dsl::SpatialOperator&lt; NeoN::Vec3 &gt; &amp;&amp;rhs)']]],
   ['sparsitypattern_5',['sparsityPattern',['../classNeoFOAM_1_1PDESolver.html#a8e5fb73b6f8356252df9d57badb62f66',1,'NeoFOAM::PDESolver']]],
   ['subcycleindex_6',['subCycleIndex',['../classNeoFOAM_1_1CreateFromFoamField.html#a0b659c2eb72285147dd9d0622203c9c7',1,'NeoFOAM::CreateFromFoamField']]],
-  ['sync_7',['sync',['../namespaceNeoFOAM.html#a643f330ea29bb0ce69e50ecc792cc872',1,'NeoFOAM']]],
+  ['sync_7',['sync',['../namespaceNeoFOAM.html#a94998f25dc15bb578a9049969fd654f5',1,'NeoFOAM']]],
   ['syncruntimes_8',['syncRunTimes',['../namespaceNeoFOAM.html#aff116721405a6ad86fee9675a3cc3156',1,'NeoFOAM']]]
 ];
