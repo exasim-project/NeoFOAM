19b7aa12c7e62efb8736c0046208d6c7cf5b7a24
Henning Scheufler <henning.scheufler@web.de>
2025-12-13 15:11:08 +0100

    fix tomlib issue

--- src/foamadapter/io/input_validation.py
+++ src/foamadapter/io/input_validation.py
@@ -6,7 +6,6 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import Any, Callable, Tuple, Type
 
-import tomllib
 import yaml
 from pydantic import BaseModel, ValidationError
 
