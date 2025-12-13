1c4c40dacf174fbc717d25de21e2f524a1e0d1ab
Henning Scheufler <henning.scheufler@web.de>
2025-12-13 09:33:30 +0100

    FIX: unused parameter in member_function

--- test/framework/integration/decorator/test_decorator_dataclass_injection.py
+++ test/framework/integration/decorator/test_decorator_dataclass_injection.py
@@ -16,9 +16,10 @@ class CombinedData:
 class SomeClass:
     @operation
     def member_function(self, a: int, combined: CombinedData) -> FieldUpdates:
+        a += 1
         combined.b += 2
         combined.c += 3.0
-        return FieldUpdates({"b": combined.b, "c": combined.c})
+        return FieldUpdates({"a": a, "b": combined.b, "c": combined.c})
 
 
 def test_context_adapter_step():
@@ -29,5 +30,6 @@ def test_context_adapter_step():
     ctx = Context(fields={"a": 0, "b": 0, "c": 0.0}, models={})
     adapted_func(ctx)
 
+    assert ctx.fields["a"] == 1
     assert ctx.fields["b"] == 2
     assert ctx.fields["c"] == 3.0
