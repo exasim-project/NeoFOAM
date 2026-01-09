6762d532b67d8d323579b9eaa67a47d13dd9f2c3
HendriceH <hendrik.hetmann@upstream-cfd.com>
2026-01-09 21:33:51 +0100

    Added turbulence equation and required functions

--- examples/neoIcoFoam/neoIcoFoam.cpp
+++ examples/neoIcoFoam/neoIcoFoam.cpp
@@ -76,10 +76,14 @@ int main(int argc, char* argv[])
                 }
             );
 
-        auto nuBCs = fvcc::createCalculatedBCs<fvcc::SurfaceBoundary<NeoN::scalar>>(rt.nfMesh);
-        fvcc::SurfaceField<NeoN::scalar> nu(rt.exec, "nu", rt.nfMesh, nuBCs);
+//        auto nuBCs = fvcc::createCalculatedBCs<fvcc::SurfaceBoundary<NeoN::scalar>>(rt.nfMesh);
+        auto volCalcBCs = fvcc::createCalculatedBCs<fvcc::VolumeBoundary<NeoN::scalar>>(rt.nfMesh);
+        fvcc::VolumeField<NeoN::scalar> nu(rt.exec, "nu", rt.nfMesh, volCalcBCs);
         NeoN::fill(nu.internalVector(), viscosity.value());
         NeoN::fill(nu.boundaryData().value(), viscosity.value());
+        fvcc::VolumeField<NeoN::scalar> one(rt.exec, "one", rt.nfMesh, volCalcBCs);
+        NeoN::fill(one.internalVector(), scalar(1));
+        NeoN::fill(one.boundaryData().value(), scalar(1));
 
         NeoN::Logging::info("Creating phi");
         auto& phi = vectorCollection.registerVector<fvcc::SurfaceField<NeoN::scalar>>(
@@ -91,10 +95,57 @@ int main(int argc, char* argv[])
             }
         );
 
-	NeoN::turbulenceModels::DES::SpalartAllmarasBase saBase(rt.exec, mesh);
-        NeoN::turbulenceModels::DES::maxDeltaxyz deltaModel(mesh);
+	NeoN::turbulenceModels::DES::SpalartAllmarasBase saBase(rt.exec, rt.nfMesh);
+	auto gradOp = nnfvcc::GaussGreenGrad(rt.exec, rt.nfMesh);
+	auto gradUx = gradOp.grad(U.x());
+	auto gradUy = gradOp.grad(U.y());
+	auto gradUz = gradOp.grad(U.z());
+	NeoN::Logging::info("after grad(U)");
+	fvcc::VolumeField<NeoN::scalar> wallDist(rt.exec, "wallDist", rt.nfMesh, volCalcBCs);
+	fvcc::VolumeField<NeoN::scalar> chi(rt.exec, "chi", rt.nfMesh, volCalcBCs);
+	fvcc::VolumeField<NeoN::scalar> fv1(rt.exec, "fv1", rt.nfMesh, volCalcBCs);
+	fvcc::VolumeField<NeoN::scalar> fv2(rt.exec, "fv2", rt.nfMesh, volCalcBCs);
+	fvcc::VolumeField<NeoN::scalar> ft2(rt.exec, "ft2", rt.nfMesh, volCalcBCs);
+	fvcc::VolumeField<NeoN::scalar> stilda(rt.exec, "stilda", rt.nfMesh, volCalcBCs);
+	fvcc::VolumeField<NeoN::scalar> fw(rt.exec, "fw", rt.nfMesh, volCalcBCs);
+	fvcc::VolumeField<NeoN::scalar> nut(rt.exec, "nut", rt.nfMesh, volCalcBCs);
+	fvcc::VolumeField<NeoN::scalar> dTilda(rt.exec, "dTilda", rt.nfMesh, volCalcBCs);
+	fvcc::VolumeField<NeoN::scalar> strainRate(rt.exec, "strainRate", rt.nfMesh, volCalcBCs);
+	fvcc::VolumeField<NeoN::scalar> dNuTildaEff(rt.exec, "dNuTildaEff", rt.nfMesh, volCalcBCs);
+	NeoN::turbulenceModels::DES::maxDeltaxyz deltaModel(rt.nfMesh);
         NeoN::turbulenceModels::DES::SpalartAllmarasDDES ddes(rt.exec);
-        NeoN::turbulenceModels::ReynoldsStress reynolds(exec, mesh);
+	NeoN::Logging::info("wallDist");
+	saBase.wallDistance(wallDist);
+	NeoN::Logging::info("chi");
+	saBase.chi(chi, nuTilda, nu);
+        saBase.fv1(fv1, chi);
+        saBase.fv2(fv2, chi, fv1);
+        saBase.ft2(ft2, chi);
+	NeoN::Logging::info("strainRate");
+	saBase.strainRate(strainRate,gradUx, gradUy, gradUz); // from velocity gradients
+	auto delta = deltaModel.delta();
+        ddes.dTilda(dTilda, wallDist, nuTilda, nu, strainRate, delta);
+        saBase.stilda(stilda, strainRate, nuTilda, dTilda, fv2);
+        saBase.fw(fw, stilda, dTilda, nuTilda);
+        saBase.dNuTildaEff(dNuTildaEff, nuTilda, nu);
+	saBase.nut(nut, nuTilda, nu);
+	nnfvcc::SurfaceField<NeoN::scalar> nuEff =
+                    fvcc::SurfaceInterpolation<NeoN::scalar>(
+                        rt.exec,
+                        rt.nfMesh,
+                        NeoN::TokenList({std::string("linear")})
+                    )
+                        .interpolate(nu+nut);
+                nuEff.name = "nuEff";
+	const auto& coeffs = saBase.coeffs();
+        const auto Cb1 = coeffs.Cb1;
+        const auto Cb2 = coeffs.Cb2;
+        const auto kappa = coeffs.kappa;
+        const auto sigmaNut = coeffs.sigmaNut;
+	const auto Cw1 = saBase.cw1();
+        const auto Cw2 = coeffs.Cw2;
+        const auto Cw3 = coeffs.Cw3;
+        //NeoN::turbulenceModels::ReynoldsStress reynolds(rt.exec, rt.nfMesh);
 
         // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
@@ -106,16 +157,15 @@ int main(int argc, char* argv[])
 
             fvcc::rotateOldTimes(U);
             fvcc::rotateOldTimes(phi);
+            fvcc::rotateOldTimes(nuTilda);
 
             auto [maxCoNum, meanCoNum] = fvcc::computeCoNum(phi, rt.dt);
             NeoN::Logging::info("Courant Number mean: {} max: {}", meanCoNum, maxCoNum);
             nf::syncRunTimes(runTime, rt, maxCoNum);
 
-	    auto nut = saBase.nut(nuTilda, nu);
-
             // Momentum predictor
             nf::PDESolver<NeoN::Vec3> UEqn(
-                dsl::imp::ddt(U) + dsl::imp::div(phi, U) - dsl::imp::laplacian(nu+nut, U),
+                dsl::imp::ddt(U) + dsl::imp::div(phi, U) - dsl::imp::laplacian(nuEff, U),
                 U,
                 rt
             );
@@ -189,9 +239,21 @@ int main(int argc, char* argv[])
             }
 
 	    deltaModel.update();
-            auto wallDist = saBase.wallDistance();
-            auto strain = saBase.strainRate(gradUx, gradUy, gradUz); // from velocity gradients
-            auto dTilda = ddes.dTilda(wallDist, nuTilda, nu, strain, deltaModel.delta());
+	    auto delta = deltaModel.delta();
+            saBase.strainRate(strainRate,gradUx, gradUy, gradUz); // from velocity gradients
+            ddes.dTilda(dTilda, wallDist, nuTilda, nu, strainRate, delta);
+	    ddes.correct(dTilda, nut, saBase, wallDist, nuTilda, nu, strainRate, delta);
+	    auto production = Cb1 * stilda * nuTilda * (one - ft2);
+	    auto spCoeff = (Cw1 * fw - (Cb1 / (kappa * kappa)) * ft2) * nuTilda / (dTilda * dTilda);
+	    auto nuTildaEff = NeoN::scalar(1/sigmaNut)*nuEff;
+	    nf::PDESolver<NeoN::scalar> nuTildaEqn(
+                        dsl::imp::ddt(nuTilda) + dsl::imp::div(phi, nuTilda) - NeoN::dsl::imp::laplacian(nuTildaEff, nuTilda)
+			+ dsl::imp::source(spCoeff, nuTilda)
+			- dsl::exp::source(production, nuTilda),
+                        nuTilda,
+                        rt
+                    );
+	    nuTildaEqn.solve();
 
             runTime.write();
             if (runTime.outputTime())
