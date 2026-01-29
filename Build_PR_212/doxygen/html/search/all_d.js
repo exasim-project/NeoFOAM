07ade77770a3b0f02aabf32de00d5c8c3ad4b4cf
greole <greole@users.noreply.github.com>
2026-01-29 09:00:35 +0000

    Deploying to gh-pages from @ exasim-project/NeoFOAM@c3eff02530c9a25cc95ad626a88afe4e06d9e587 🚀

--- Build_PR_212/doxygen/html/search/all_d.js
+++ Build_PR_212/doxygen/html/search/all_d.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['scalar_0',['scalar',['../pressureVelocityCoupling_8hpp.html#acc912e7a5a44b028589fda3fd76ba7ea',1,'pressureVelocityCoupling.hpp']]],
   ['setdeltat_1',['setDeltaT',['../namespaceNeoFOAM.html#a39bc76acefb5a1dd3074b0feba5864eb',1,'NeoFOAM']]],
-  ['setreference_2',['setreference',['../structNeoFOAM_1_1PDESolver_1_1SetReference.html',1,'NeoFOAM::PDESolver&lt; ValueType, IndexType &gt;::SetReference&lt; FunctorValueType &gt;'],['../structNeoFOAM_1_1PDESolver_1_1SetReference.html#a8871e71d55b49e4309d2acc9207825dd',1,'NeoFOAM::PDESolver::SetReference::SetReference()'],['../classNeoFOAM_1_1PDESolver.html#aa632d298feb601ef802ba3b85b2dc8ed',1,'NeoFOAM::PDESolver::setReference()']]],
+  ['setreference_2',['setreference',['../structNeoFOAM_1_1PDESolver_1_1SetReference.html',1,'NeoFOAM::PDESolver&lt; ValueType, IndexType &gt;::SetReference&lt; FunctorValueType &gt;'],['../classNeoFOAM_1_1PDESolver.html#aa632d298feb601ef802ba3b85b2dc8ed',1,'NeoFOAM::PDESolver::setReference(NeoN::localIdx pRefCell, NeoN::scalar pRefValue)'],['../structNeoFOAM_1_1PDESolver_1_1SetReference.html#a8871e71d55b49e4309d2acc9207825dd',1,'NeoFOAM::PDESolver::SetReference::SetReference()']]],
   ['setup_2ehpp_3',['setup.hpp',['../setup_8hpp.html',1,'']]],
   ['solve_4',['solve',['../classNeoFOAM_1_1PDESolver.html#a0464b266cbae77cbfa7f4ffec79e6acb',1,'NeoFOAM::PDESolver::solve()'],['../classNeoFOAM_1_1PDESolver.html#a7f4464b4305040de5ace88df987c05df',1,'NeoFOAM::PDESolver::solve(dsl::SpatialOperator&lt; NeoN::Vec3 &gt; &amp;&amp;rhs)']]],
   ['sparsitypattern_5',['sparsityPattern',['../classNeoFOAM_1_1PDESolver.html#a8e5fb73b6f8356252df9d57badb62f66',1,'NeoFOAM::PDESolver']]],
