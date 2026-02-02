ad05002c9ba4a1623a11065c0e270d2eaba15550
Henning Scheufler <henning.scheufler@web.de>
2026-02-02 18:09:59 +0100

    added license headers

--- test/io/test_input_validation.py
+++ test/io/test_input_validation.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-FileCopyrightText: 2025 NeoFOAM authors
+
 import os
 import pytest
 
--- test/io/test_input_validation_dataclasses.py
+++ test/io/test_input_validation_dataclasses.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-FileCopyrightText: 2025 NeoFOAM authors
+
 from pydantic import BaseModel
 from foamadapter.io.input_validation import ModelInputDefinition, ValidationErrors
 
