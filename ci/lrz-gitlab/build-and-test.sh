4e3da06833749e1f482e1e1c0d7cadfc61503bd3
dheerajraghunathan <dheeraj.raghunathan@tum.de>
2026-02-04 11:24:35 +0100

    Minor Fix

--- ci/lrz-gitlab/build-and-test.sh
+++ ci/lrz-gitlab/build-and-test.sh
@@ -105,5 +105,5 @@ ctest --preset $PRESET -R neofoam --output-on-failure
 # Step 4: Validate neoIcoFoam
 # -----------------------------
 pushd tutorials/cavity >/dev/null
-python3 cleanRunPlot.py
+python3 cleanRunValidate.py
 popd >/dev/null
