8ea5b87c007cbe073f7bcf85a406ea96b440b17d
HendriceH <HendriceH@users.noreply.github.com>
2026-02-06 12:52:43 +0000

    Deploying to gh-pages from @ exasim-project/NeoFOAM@01c73f68cd4092f32b559c5b6a0cc3c99cd59348 🚀

---
+++ Build_PR_233/doxygen/html/search/variables_8.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['prefcell_5f_0',['pRefCell_',['../structNeoFOAM_1_1PDESolver_1_1SetReference.html#a312bf19b6c97dcb123a125e2be3c9bc4',1,'NeoFOAM::PDESolver::SetReference']]],
+  ['prefvalue_5f_1',['pRefValue_',['../structNeoFOAM_1_1PDESolver_1_1SetReference.html#a3c95dcddaa8f9c91c6a660df2f75312b',1,'NeoFOAM::PDESolver::SetReference']]]
+];
