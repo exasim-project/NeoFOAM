912490ba6231282ae6cefb2433ea7f95551e70df
chihtaw <wangchihta@gmail.com>
2026-02-06 10:41:00 +0100

    Configure NeoFOAM for profiling in CI

--- ci/lrz-gitlab/build-and-test.sh
+++ ci/lrz-gitlab/build-and-test.sh
@@ -70,7 +70,8 @@ elif [[ "$GPU_VENDOR" == "amd" ]]; then
         -DCMAKE_CXX_COMPILER=hipcc \
         -DCMAKE_HIP_ARCHITECTURES=gfx90a \
         -DKokkos_ARCH_AMD_GFX90A=ON \
-        -DNeoN_WITH_THREADS=OFF
+        -DNeoN_WITH_THREADS=OFF \
+        -DNEOFOAM_BUILD_BENCHMARKS=ON
 elif [[ "$GPU_VENDOR" == "intel" ]]; then
     cmake --preset $PRESET \
         -DNEOFOAM_NEON_DIR=../NeoN \
