aae45871494b14140cd41fb68f7cd5cb65bef5ff
HenningScheufler <HenningScheufler@users.noreply.github.com>
2026-02-17 17:22:12 +0000

    Deploying to gh-pages from @ exasim-project/NeoFOAM@06098985db6ef41a40a760bcbe8ac2e0d15419a1 🚀

---
+++ Build_PR_242/doxygen/html/search/variables_5.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['issurfacefield_0',['isSurfaceField',['../namespaceNeoFOAM_1_1detail.html#ab279b37af86eb68b39242b6bf49e208d',1,'NeoFOAM::detail']]],
+  ['isvolumefield_1',['isVolumeField',['../namespaceNeoFOAM_1_1detail.html#a5376cc046ea43727dbdeedf0f7b2614c',1,'NeoFOAM::detail']]],
+  ['iterationindex_2',['iterationIndex',['../classNeoFOAM_1_1CreateFromFoamField.html#ae2f5cd13138970d78a2462ae06a4b68f',1,'NeoFOAM::CreateFromFoamField']]]
+];
