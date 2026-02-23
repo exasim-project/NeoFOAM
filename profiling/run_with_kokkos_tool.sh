c2168f6104917da35172a16d179bd327ebb9b38f
chihtaw <wangchihta@gmail.com>
2026-02-23 21:36:28 +0100

    Fix the tool names of kokkos tools for profiling

--- profiling/run_with_kokkos_tool.sh
+++ profiling/run_with_kokkos_tool.sh
@@ -1,36 +1,36 @@
 #!/usr/bin/env bash
-
 set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 NEOFOAM_SRC_DIR="$( cd "$SCRIPT_DIR/.." && pwd )"
 BUILD_DIR="${NEOFOAM_SRC_DIR}/build"
 
-TOOL_NAME=$1
-shift
-
-if [ -z "$TOOL_NAME" ]; then
+# Ensure at least one argument
+if [ $# -lt 1 ]; then
     echo "Usage:"
     echo "  $0 <tool-name>"
     echo ""
     echo "Available tools:"
-    echo "  simple kernel timer"
-    echo "  space time stack"
-    echo "  memory usage"
+    echo "  simple-kernel-timer"
+    echo "  space-time-stack"
+    echo "  memory-usage"
     exit 1
 fi
+
+TOOL_NAME=$1
+shift
 
 TOOLS_DIR="${BUILD_DIR}/profiling/kokkos_tools_build/profiling"
 
 case "$TOOL_NAME" in
-  simple_kernel_timer)
+  simple-kernel-timer)
     export KOKKOS_TOOLS_LIBS=${TOOLS_DIR}/simple-kernel-timer/libkp_kernel_timer.so
     export PATH=${TOOLS_DIR}/simple-kernel-timer:$PATH
     ;;
-  space_time_stack)
+  space-time-stack)
     export KOKKOS_TOOLS_LIBS=${TOOLS_DIR}/space-time-stack/libkp_space_time_stack.so
     ;;
-  memory_usage)
+  memory-usage)
     export KOKKOS_TOOLS_LIBS=${TOOLS_DIR}/memory-usage/libkp_memory_usage.so
     ;;
   *)
@@ -41,5 +41,4 @@ esac
 
 echo "Using tool: $TOOL_NAME"
 echo "KOKKOS_TOOLS_LIBS=$KOKKOS_TOOLS_LIBS"
-echo "-----------------------------------"
 
