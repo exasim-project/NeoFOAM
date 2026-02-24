b4eee4fb551d79e8745293d9d22b4eb82e9aae82
chihtaw <wangchihta@gmail.com>
2026-02-24 16:47:31 +0100

    Improve comments for code instrumentation

--- examples/neoIcoFoam/neoIcoFoam.cpp
+++ examples/neoIcoFoam/neoIcoFoam.cpp
@@ -95,7 +95,7 @@ int main(int argc, char* argv[])
                 // TODO use a free function here
                 Kokkos::Profiling::pushRegion("Momentum predictor (UEqn.solve)");
                 UEqn.solve(-1.0 * dsl::exp::grad(p));
-                Kokkos::Profiling::popRegion(); // Momentum predictor - UEqn.solve
+                Kokkos::Profiling::popRegion(); // Momentum predictor (UEqn.solve)
             }
             else
             {
@@ -103,7 +103,7 @@ int main(int argc, char* argv[])
                 // explicitly trigger assembly here.
                 Kokkos::Profiling::pushRegion("No momentum predictor (UEqn.assemble)");
                 UEqn.assemble();
-                Kokkos::Profiling::popRegion(); // No momentum predictor - UEqn.assemble
+                Kokkos::Profiling::popRegion(); // No momentum predictor (UEqn.assemble)
             }
 
             // --- PISO loop
