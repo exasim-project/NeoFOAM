4887217b97f0c46ba32c6244f14bc4e6385ce182
greole <greole@users.noreply.github.com>
2026-03-25 18:57:58 +0000

    Deploying to gh-pages from @ exasim-project/NeoFOAM@26d75bf1c984de559735f6d48b5da110f65cc286 🚀

--- Build_PR_263/doxygen/html/search/functions_b.js
+++ Build_PR_263/doxygen/html/search/functions_b.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['pdesolver_0',['pdesolver',['../classNeoFOAM_1_1PDESolver.html#ace256b5f26d8077c69f0e2990fc88dac',1,'NeoFOAM::PDESolver::PDESolver(dsl::Expression&lt; ValueType &gt; expr, VolumeField &amp;psi, RunTime &amp;runTime)'],['../classNeoFOAM_1_1PDESolver.html#aadc56e8e1c8d0adb8d792fbcfcb844e1',1,'NeoFOAM::PDESolver::PDESolver(const PDESolver &amp;expr)']]]
+  ['pdesolver_0',['pdesolver',['../classNeoFOAM_1_1PDESolver.html#a6bed6447ef9e2cf72bd7a4809de72a48',1,'NeoFOAM::PDESolver::PDESolver(dsl::Expression&lt; ValueType &gt; expr, VolumeField &amp;psi, RunTime &amp;runTime, NeoN::CommunicationPattern commPattern)'],['../classNeoFOAM_1_1PDESolver.html#aadc56e8e1c8d0adb8d792fbcfcb844e1',1,'NeoFOAM::PDESolver::PDESolver(const PDESolver &amp;expr)']]]
 ];
