a76433af5e7a15906ccbed26e8e7abe639c9d787
greole <greole@users.noreply.github.com>
2026-03-25 11:08:16 +0000

    Deploying to gh-pages from @ exasim-project/NeoFOAM@865cbb4d553036ae2ae9070aaf084f272b75ef13 🚀

---
+++ Build_PR_263/doxygen/html/search/functions_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['applyoperator_0',['applyOperator',['../namespaceNeoFOAM.html#ac5f785f34f3c207b2c484ad3a70c0694',1,'NeoFOAM']]],
+  ['assemble_1',['assemble',['../classNeoFOAM_1_1PDESolver.html#a8aef9ec9ddbd7e1dcb0f283cb6ad2dba',1,'NeoFOAM::PDESolver::assemble()'],['../classNeoFOAM_1_1PDESolver.html#a94ed687e58cbf43e4cd8154b2344182b',1,'NeoFOAM::PDESolver::assemble(dsl::SpatialOperator&lt; NeoN::Vec3 &gt; &amp;&amp;rhs)']]]
+];
