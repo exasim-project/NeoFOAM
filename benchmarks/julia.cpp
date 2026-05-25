d2b992af3a8f9e7d4632e3cc072fb37688214633
Lukas Petermann <lukas.petermann13@gmail.com>
2026-05-25 20:25:43 +0200

    add julia interface benchmark

--- benchmarks/julia.cpp
+++ benchmarks/julia.cpp
@@ -19,6 +19,8 @@ JULIA_DEFINE_FAST_TLS; // only define this once, in an executable (not in a
 namespace fvcc = NeoN::finiteVolume::cellCentred;
 namespace nf = NeoFOAM;
 namespace dsl = NeoN::dsl;
+namespace fvc = Foam::fvc;
+namespace fvm = Foam::fvm;
 
 #include "fvc.H"
 #include "fvm.H"
@@ -27,22 +29,63 @@ namespace dsl = NeoN::dsl;
 extern Foam::Time* timePtr;    // A single time object
 extern Foam::argList* argsPtr; // Some forks want argList access at createMesh.H
 extern Foam::fvMesh* meshPtr;  // A single mesh object
-
 TEST_CASE("julia")
 {
+    jl_init();
+
     Foam::Time& runTime = *timePtr;
     std::unique_ptr<Foam::fvMesh> meshPtr = NeoFOAM::createMesh(runTime);
     Foam::fvMesh& mesh = *meshPtr;
 
     auto ofU = nf::randomVectorField(runTime, mesh, "U");
     auto ofP = nf::randomScalarField(runTime, mesh, "p");
-    auto ofPhi = nf::randDimField<Foam::surfaceScalarField>(mesh, {0, 3, -1, 0, 0}, "phi");
-    auto ofGamma = nf::randDimField<Foam::surfaceScalarField>(mesh, {0, 2, -1, 0, 0}, "Gamma");
-
+    
+    auto exec = NeoN::CPUExecutor {};
+    auto rt = nf::createAdapterRunTime(runTime, exec);
+    auto& schemesDict = rt.fvSchemesDict;
+    schemesDict = nf::mapFvSchemes(schemesDict);
+    auto& nfMesh = rt.mesh;
+    auto& fieldCollection = fvcc::VectorCollection::instance(rt.db, "fieldCollection");
+    auto& vectorCollection = nnfvcc::VectorCollection::instance(rt.db, "VectorCollection");
+    
+    Foam::surfaceScalarField ofPhi(
+        Foam::IOobject(
+            "phi",
+            runTime.timeName(),
+            mesh,
+            Foam::IOobject::NO_READ,
+            Foam::IOobject::NO_WRITE
+        ),
+        fvc::flux(ofU)
+    );
+    
+    Foam::surfaceScalarField ofNu(
+        Foam::IOobject(
+            "nu",
+            runTime.timeName(),
+            mesh,
+            Foam::IOobject::NO_READ,
+            Foam::IOobject::NO_WRITE
+        ),
+        mesh,
+        Foam::dimensionedScalar("nu", Foam::dimensionSet(0, 2, -1, 0, 0), 0.01)
+    );
+    
+    auto faceFlux = NeoFOAM::constructFrom(rt.exec, rt.nfMesh, ofPhi);
+    auto gamma = NeoFOAM::constructFrom(rt.exec, rt.nfMesh, ofNu);
+    
+    auto& phi = NeoFOAM::constructAndRegister(vectorCollection, rt, ofU);
+    auto& nfOldPhi = fvcc::oldTime(phi);
+    NeoN::fill(nfOldPhi.internalVector(), NeoN::Vec3(0.0, 0.0, 0.0));
+    
+    nf::PDESolver<NeoN::Vec3> UEQN(
+        dsl::imp::div(faceFlux, phi) - dsl::imp::laplacian(gamma, phi),
+        phi,
+        rt
+    );
+    std::cout << "benchmarkg?\n";
     SECTION("NeoN With Julia")
     {
-        jl_init();
-
         std::string path = fmt::format(
             fmt::runtime(
                 "include(\"{}\")"
@@ -50,46 +93,22 @@ TEST_CASE("julia")
             JULIA_MODULE_INIT
         );
         jl_eval_string(path.c_str());
-
-        std::cout << "path: " << path << std::endl;
-        auto exec = NeoN::Executor(NeoN::SerialExecutor {});
-        auto rt = nf::createAdapterRunTime(runTime, exec);
-        auto& schemesDict = rt.fvSchemesDict;
-        schemesDict = nf::mapFvSchemes(schemesDict);
-        auto& nfMesh = rt.mesh;
-        auto& fieldCollection = fvcc::VectorCollection::instance(rt.db, "fieldCollection");
-        auto& nfU = constructAndRegister(fieldCollection, rt, ofU);
-        auto [nfP, nfPhi, nfGamma] =
-            NeoFOAM::constFromMany(rt.exec, rt.nfMesh, ofP, ofPhi, ofGamma);
-        
-            SECTION(std::string("[NEON] SpatialOperators only"))
+        UEQN.warmupFaceBased();
+        BENCHMARK("SpatialOperators")
         {
-            nf::PDESolver<NeoN::Vec3> eqn(
-                dsl::imp::div(nfPhi, nfU) - dsl::imp::laplacian(nfGamma, nfU),
-                nfU,
-                rt
-            );
-            BENCHMARK("CPUExecutor")
-            {
-                eqn.assemble();
-                NeoN::fence(exec);
-                return;
-            };
-        }
-        SECTION(std::string("[JULIA] SpatialOperators, no explicit warmup"))
-        {
-            nf::PDESolver<NeoN::Vec3> juliaUEqn(
-                dsl::imp::div(nfPhi, nfU) - dsl::imp::laplacian(nfGamma, nfU),
-                nfU,
-                rt
-            );
-            BENCHMARK("CPUExecutor")
-            {
-                juliaUEqn.juliaFaceBased(nfPhi, nfU, nfGamma);
-                return;
-            };
-        }
-        jl_atexit_hook(0);
+            UEQN.juliaFaceBased(faceFlux, phi, gamma);
+            return;
+        };
     }
+    // SECTION(std::string("NeoN"))
+    // {
+        //     BENCHMARK("SpatialOperators")
+        //     {
+            //         UEQN.assemble();
+            //         NeoN::fence(exec);
+            //         return;
+            //     };
+            // }
+    jl_atexit_hook(0);
 }
 #endif()
