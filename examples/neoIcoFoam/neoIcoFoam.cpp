5ab17684ea827350cb6301d47cb64fa23d7be3e4
HendriceH <hendrik.hetmann@upstream-cfd.com>
2026-05-28 08:24:15 +0200

    Added continuity error calculation

--- examples/neoIcoFoam/neoIcoFoam.cpp
+++ examples/neoIcoFoam/neoIcoFoam.cpp
@@ -59,6 +59,7 @@ int main(int argc, char* argv[])
 
         NeoN::Logging::info("Creating phi");
         auto& phi = nf::constructAndRegister(vectorCollection, rt, ofPhi, false);
+        NeoN::scalar cumulativeContErr = 0.0;
 
         // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
@@ -143,8 +144,7 @@ int main(int argc, char* argv[])
                         nf::updateFaceVelocity(phiHbyA, pEqn, phi);
                     }
                 }
-                // TODO: missing
-                // #include "continuityErrs.H"
+                nf::reportContinuityError(phi, rt, cumulativeContErr);
 
                 nf::updateVelocity(hByA, crAU, p, U);
                 U.correctBoundaryConditions();
