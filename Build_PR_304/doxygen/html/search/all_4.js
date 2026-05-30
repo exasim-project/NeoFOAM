b962c3ca0f193880c4dcd2f66c5c8169544e2317
greole <greole@users.noreply.github.com>
2026-05-30 19:14:33 +0000

    Deploying to gh-pages from @ exasim-project/NeoFOAM@d84c267f313ab16196f2b599946823bd209edbab 🚀

--- Build_PR_304/doxygen/html/search/all_4.js
+++ Build_PR_304/doxygen/html/search/all_4.js
@@ -3,7 +3,7 @@ var searchData=
   ['fdcoef_0',['fdCoef',['../structNeoFOAM_1_1SpalartAllmarasDDES_1_1Coefficients.html#ab77a6fc8473fe02ce74d4ca5674b4a42',1,'NeoFOAM::SpalartAllmarasDDES::Coefficients']]],
   ['fieldtraits_2ehpp_1',['fieldTraits.hpp',['../fieldTraits_8hpp.html',1,'']]],
   ['flatbcfield_2',['flatBCField',['../namespaceNeoFOAM.html#a01c08e9d79fe780ebcdbc7f82a70b1e1',1,'NeoFOAM']]],
-  ['flux_3',['flux',['../namespaceNeoFOAM.html#a0d9937816ddfe9d997066890d86705a3',1,'NeoFOAM::flux(const nnfvcc::VolumeField&lt; Vec3 &gt; &amp;volField)'],['../namespaceNeoFOAM.html#ab609cded887cb5a650200fa3d53bdc2a',1,'NeoFOAM::flux(const PDESolver&lt; scalar &gt; &amp;expr)']]],
+  ['flux_3',['flux',['../namespaceNeoFOAM.html#a0d9937816ddfe9d997066890d86705a3',1,'NeoFOAM']]],
   ['foamfield_4',['foamField',['../classNeoFOAM_1_1CreateFromFoamField.html#a62ab45d4ea5e6815e38d82ecfda3f169',1,'NeoFOAM::CreateFromFoamField']]],
   ['fromfoamfield_5',['fromFoamField',['../namespaceNeoFOAM.html#a1dfe753898e56c3e2e7b5c6f00bd2a4f',1,'NeoFOAM']]],
   ['fvschemes_2ehpp_6',['fvSchemes.hpp',['../fvSchemes_8hpp.html',1,'']]],
