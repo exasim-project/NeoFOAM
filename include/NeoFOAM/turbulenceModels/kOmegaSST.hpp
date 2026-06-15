a9adac7ddae349245a6bb0aafad0ad54037b8992
Gregor Olenik <gregor.olenik@web.de>
2026-06-15 20:32:18 +0200

    wip kOmegaSST

--- include/NeoFOAM/turbulenceModels/kOmegaSST.hpp
+++ include/NeoFOAM/turbulenceModels/kOmegaSST.hpp
@@ -198,6 +198,7 @@ private:
     // Owner-cell wall distance copied to boundary faces so wall BCs get a non-zero y.
     nnfvcc::VolumeField<scalar> nearWallDistTmp_;
 
+
     // Cached face-interpolated nu (computed once in constructor)
     nnfvcc::SurfaceField<scalar> surfNuTmp_;
 
