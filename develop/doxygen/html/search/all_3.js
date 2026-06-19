9db015620a3a93c4febf827a260503a2757d19f5
HendriceH <HendriceH@users.noreply.github.com>
2026-06-19 14:06:55 +0000

    Deploying to gh-pages from @ exasim-project/NeoFOAM@1973ee37caf63d2784394af447f2e700dca4d5f1 🚀

--- develop/doxygen/html/search/all_3.js
+++ develop/doxygen/html/search/all_3.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['exec_0',['exec',['../classNeoFOAM_1_1CreateFromFoamField.html#a67c825247791986116ce46a30ecd66a3',1,'NeoFOAM::CreateFromFoamField::exec'],['../structNeoFOAM_1_1RunTime.html#a8dcd260a0853c8590add966729198cb1',1,'NeoFOAM::RunTime::exec'],['../classNeoFOAM_1_1MeshAdapter.html#a959398dd5266c98c2f2cbd4fb350dfc4',1,'NeoFOAM::MeshAdapter::exec()'],['../classNeoFOAM_1_1PDESolver.html#aabc63169ccc777c668a09248650af9e4',1,'NeoFOAM::PDESolver::exec()']]],
+  ['exec_0',['exec',['../classNeoFOAM_1_1CreateFromFoamField.html#a67c825247791986116ce46a30ecd66a3',1,'NeoFOAM::CreateFromFoamField::exec'],['../structNeoFOAM_1_1RunTime.html#a8dcd260a0853c8590add966729198cb1',1,'NeoFOAM::RunTime::exec'],['../classNeoFOAM_1_1MeshAdapter.html#a959398dd5266c98c2f2cbd4fb350dfc4',1,'NeoFOAM::MeshAdapter::exec()'],['../classNeoFOAM_1_1PDESolver.html#acb2d73ff3e20e7309f84400b13b9b53f',1,'NeoFOAM::PDESolver::exec()']]],
   ['exec_5f_1',['exec_',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1ViscousStressOperatorFactory.html#aa6428987a60690d8ad374ddc92b0ae6c',1,'NeoN::finiteVolume::cellCentred::ViscousStressOperatorFactory']]],
   ['explicitop_2',['explicitop',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1ViscousStressOperatorFactory.html#a3acf56a7565d63193a041ba29d900d46',1,'NeoN::finiteVolume::cellCentred::ViscousStressOperatorFactory::explicitOp()'],['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1GaussViscousStress.html#a458ed7f1a373c74997a6edf6435f5a44',1,'NeoN::finiteVolume::cellCentred::GaussViscousStress::explicitOp()']]],
   ['explicitoperation_3',['explicitOperation',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1ViscousStressOperator.html#a4b6c8f8214c76215023a0cf69cf56d83',1,'NeoN::finiteVolume::cellCentred::ViscousStressOperator']]],
-  ['expression_4',['expression',['../classNeoFOAM_1_1PDESolver.html#a84bccc8b1ecb7bea2e60d4d9e4904879',1,'NeoFOAM::PDESolver']]]
+  ['expression_4',['expression',['../classNeoFOAM_1_1PDESolver.html#a0be0db3a3113fe9cc11f034221c29538',1,'NeoFOAM::PDESolver']]]
 ];
--- develop/doxygen/html/search/functions_0.js
+++ develop/doxygen/html/search/functions_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['applyoperator_0',['applyOperator',['../namespaceNeoFOAM.html#a1a20de9aeea93a39b256d91014ce9039',1,'NeoFOAM']]],
-  ['assemble_1',['assemble',['../classNeoFOAM_1_1PDESolver.html#a8aef9ec9ddbd7e1dcb0f283cb6ad2dba',1,'NeoFOAM::PDESolver::assemble()'],['../classNeoFOAM_1_1PDESolver.html#a94ed687e58cbf43e4cd8154b2344182b',1,'NeoFOAM::PDESolver::assemble(dsl::SpatialOperator&lt; NeoN::Vec3 &gt; &amp;&amp;rhs)']]]
+  ['assemble_1',['assemble',['../classNeoFOAM_1_1PDESolver.html#a428ead04185bd650a91cd4ff3c573a24',1,'NeoFOAM::PDESolver::assemble()'],['../classNeoFOAM_1_1PDESolver.html#ad47450a4caf56d16843a3e70481eee0b',1,'NeoFOAM::PDESolver::assemble(dsl::SpatialOperator&lt; NeoN::Vec3 &gt; &amp;&amp;rhs)']]]
 ];
