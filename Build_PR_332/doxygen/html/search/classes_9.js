a7c2fa8d1aaa2a078432bd01a1d14a8693e2a3f2
HendriceH <HendriceH@users.noreply.github.com>
2026-06-20 14:34:10 +0000

    Deploying to gh-pages from @ exasim-project/NeoFOAM@220436ff282974937d2c728011cbfb0d55ee6ca9 🚀

---
+++ Build_PR_332/doxygen/html/search/classes_9.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['viscousstressoperator_0',['ViscousStressOperator',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1ViscousStressOperator.html',1,'NeoN::finiteVolume::cellCentred']]],
+  ['viscousstressoperatorfactory_1',['ViscousStressOperatorFactory',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1ViscousStressOperatorFactory.html',1,'NeoN::finiteVolume::cellCentred']]]
+];
