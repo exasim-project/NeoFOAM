25ac791a32c0a8b03e5d1610c4667735b9e3355a
HendriceH <HendriceH@users.noreply.github.com>
2026-06-20 12:39:47 +0000

    Deploying to gh-pages from @ exasim-project/NeoFOAM@473515809615358aac94514e87aa39f15913a154 🚀

--- Build_PR_329/doxygen/html/search/all_d.js
+++ Build_PR_329/doxygen/html/search/all_d.js
@@ -2,13 +2,14 @@ var searchData=
 [
   ['read_0',['read',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1ViscousStressOperator.html#ac82afed643409ea721268ebaf58e38cb',1,'NeoN::finiteVolume::cellCentred::ViscousStressOperator']]],
   ['readers_2ehpp_1',['readers.hpp',['../readers_8hpp.html',1,'']]],
-  ['readopenfoammesh_2',['readOpenFOAMMesh',['../namespaceNeoFOAM.html#afb828727f0f7b23418e94e1c464d9dae',1,'NeoFOAM']]],
-  ['readorcreate_3',['readOrCreate',['../namespaceNeoFOAM.html#ae3448b9356abfd01a14f812c7acffc7c',1,'NeoFOAM']]],
-  ['readsurfaceboundaryconditions_4',['readSurfaceBoundaryConditions',['../namespaceNeoFOAM.html#adb365cf8e725db6dbfc0a758c392e2b1',1,'NeoFOAM']]],
-  ['readvolboundaryconditions_5',['readVolBoundaryConditions',['../namespaceNeoFOAM.html#a6cb4d89d1e44667e5a66f5119bad9206',1,'NeoFOAM']]],
-  ['relaxownedls_6',['relaxOwnedLs',['../classNeoFOAM_1_1PDESolver.html#a5bf63a2b300465377c937bda5d52f5f5',1,'NeoFOAM::PDESolver']]],
-  ['reportcontinuityerror_7',['reportContinuityError',['../namespaceNeoFOAM.html#a216c99c971b9acb00fbd8ff36d9f2ee9',1,'NeoFOAM']]],
-  ['residualmap_8',['ResidualMap',['../namespaceNeoFOAM.html#ad7da788dc1fc4af69c98dea808d84073',1,'NeoFOAM']]],
-  ['runtime_9',['RunTime',['../structNeoFOAM_1_1RunTime.html',1,'NeoFOAM']]],
-  ['runtime_2ehpp_10',['runTime.hpp',['../runTime_8hpp.html',1,'']]]
+  ['readimplicittransformbc_2',['readImplicitTransformBC',['../namespaceNeoFOAM.html#a1c190519eafff0cdc2ab2d37fc3e5243',1,'NeoFOAM']]],
+  ['readopenfoammesh_3',['readOpenFOAMMesh',['../namespaceNeoFOAM.html#afb828727f0f7b23418e94e1c464d9dae',1,'NeoFOAM']]],
+  ['readorcreate_4',['readOrCreate',['../namespaceNeoFOAM.html#ae3448b9356abfd01a14f812c7acffc7c',1,'NeoFOAM']]],
+  ['readsurfaceboundaryconditions_5',['readSurfaceBoundaryConditions',['../namespaceNeoFOAM.html#adb365cf8e725db6dbfc0a758c392e2b1',1,'NeoFOAM']]],
+  ['readvolboundaryconditions_6',['readVolBoundaryConditions',['../namespaceNeoFOAM.html#a6cb4d89d1e44667e5a66f5119bad9206',1,'NeoFOAM']]],
+  ['relaxownedls_7',['relaxOwnedLs',['../classNeoFOAM_1_1PDESolver.html#a5bf63a2b300465377c937bda5d52f5f5',1,'NeoFOAM::PDESolver']]],
+  ['reportcontinuityerror_8',['reportContinuityError',['../namespaceNeoFOAM.html#a216c99c971b9acb00fbd8ff36d9f2ee9',1,'NeoFOAM']]],
+  ['residualmap_9',['ResidualMap',['../namespaceNeoFOAM.html#ad7da788dc1fc4af69c98dea808d84073',1,'NeoFOAM']]],
+  ['runtime_10',['RunTime',['../structNeoFOAM_1_1RunTime.html',1,'NeoFOAM']]],
+  ['runtime_2ehpp_11',['runTime.hpp',['../runTime_8hpp.html',1,'']]]
 ];
--- Build_PR_329/doxygen/html/search/functions_f.js
+++ Build_PR_329/doxygen/html/search/functions_f.js
@@ -4,5 +4,6 @@ var searchData=
   ['updatefacevelocity_1',['updateFaceVelocity',['../namespaceNeoFOAM.html#afc1d72b867bd234d1acdac0bf9f77bc5',1,'NeoFOAM']]],
   ['updatepreconditioner_2',['updatePreconditioner',['../namespaceNeoFOAM.html#a8c1ca67d102991ab4a3c1bc3a2a8fb03',1,'NeoFOAM']]],
   ['updatesolver_3',['updateSolver',['../namespaceNeoFOAM.html#ab68574263e9c80758820191001d6d256',1,'NeoFOAM']]],
-  ['updatevelocity_4',['updateVelocity',['../namespaceNeoFOAM.html#af74166d4a917f022e9371fa7a13cbc6a',1,'NeoFOAM']]]
+  ['updatevelocity_4',['updateVelocity',['../namespaceNeoFOAM.html#af74166d4a917f022e9371fa7a13cbc6a',1,'NeoFOAM']]],
+  ['useimplicittransformbc_5',['useImplicitTransformBC',['../namespaceNeoFOAM.html#a7abcd1d6e565197e8e96d0b16e3c1c63',1,'NeoFOAM']]]
 ];
