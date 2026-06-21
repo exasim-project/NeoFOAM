72ea4a15bea3d0a939a16084c8f2fd5457ca9cf8
greole <greole@users.noreply.github.com>
2026-06-21 13:53:36 +0000

    Deploying to gh-pages from @ exasim-project/NeoFOAM@5a333b605d315f11ca6de95f29fa37966e17f535 🚀

---
+++ Build_PR_328/doxygen/html/search/all_2.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['db_0',['db',['../structNeoFOAM_1_1RunTime.html#a9b5ae52074cd0e4e2a164a8320ba38b9',1,'NeoFOAM::RunTime']]],
+  ['ddtscheme_1',['ddtScheme',['../classNeoFOAM_1_1PDESolver.html#a577b2534bdc16547869ed719bd03d879',1,'NeoFOAM::PDESolver']]],
+  ['devrhoreff_2',['devRhoReff',['../classNeoFOAM_1_1SpalartAllmarasDDES.html#ac253c51b5267125370b87ac879bb96b9',1,'NeoFOAM::SpalartAllmarasDDES']]],
+  ['devstress_2ehpp_3',['devStress.hpp',['../devStress_8hpp.html',1,'']]],
+  ['divdevreff_4',['divDevReff',['../namespaceNeoN_1_1finiteVolume_1_1cellCentred.html#aac397c2e2aaea7b2e6cb89130315c39f',1,'NeoN::finiteVolume::cellCentred']]],
+  ['doc_5',['doc',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1volumeBoundary_1_1NutUSpaldingWallFunction.html#a0cd75798a3280f44f93258265d310d5e',1,'NeoN::finiteVolume::cellCentred::volumeBoundary::NutUSpaldingWallFunction::doc()'],['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1GaussViscousStress.html#a5e41477a0b62f4a6c203eb5970f3933b',1,'NeoN::finiteVolume::cellCentred::GaussViscousStress::doc()']]],
+  ['dt_6',['dt',['../structNeoFOAM_1_1RunTime.html#a6a3e375c6054e387635ba04bdfe76c6d',1,'NeoFOAM::RunTime']]]
+];
---
+++ Build_PR_328/doxygen/html/search/functions_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['applyoperator_0',['applyOperator',['../namespaceNeoFOAM.html#a1a20de9aeea93a39b256d91014ce9039',1,'NeoFOAM']]],
+  ['assemble_1',['assemble',['../classNeoFOAM_1_1PDESolver.html#a428ead04185bd650a91cd4ff3c573a24',1,'NeoFOAM::PDESolver::assemble()'],['../classNeoFOAM_1_1PDESolver.html#ad47450a4caf56d16843a3e70481eee0b',1,'NeoFOAM::PDESolver::assemble(dsl::SpatialOperator&lt; NeoN::Vec3 &gt; &amp;&amp;rhs)']]]
+];
