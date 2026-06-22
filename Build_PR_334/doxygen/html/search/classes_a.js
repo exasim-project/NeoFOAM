8e62033d5f4903345a20ba7fb806edd378d877dd
greole <greole@users.noreply.github.com>
2026-06-22 09:12:19 +0000

    Deploying to gh-pages from @ exasim-project/NeoFOAM@04397dc013c694e31f06650bf7f71ecf7b8eebc2 🚀

---
+++ Build_PR_334/doxygen/html/search/classes_a.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['spalartallmarasddes_0',['SpalartAllmarasDDES',['../classNeoFOAM_1_1SpalartAllmarasDDES.html',1,'NeoFOAM']]]
+];
---
+++ Build_PR_334/doxygen/html/search/variables_1.js
@@ -0,0 +1,15 @@
+var searchData=
+[
+  ['cb1_0',['Cb1',['../structNeoFOAM_1_1SpalartAllmarasDDES_1_1Coefficients.html#a7e8610a0c3139e28379f7164a23359ef',1,'NeoFOAM::SpalartAllmarasDDES::Coefficients']]],
+  ['cb2_1',['Cb2',['../structNeoFOAM_1_1SpalartAllmarasDDES_1_1Coefficients.html#ad183ce431e5168e2681c30e8bbf6fc56',1,'NeoFOAM::SpalartAllmarasDDES::Coefficients']]],
+  ['cdes_2',['Cdes',['../structNeoFOAM_1_1SpalartAllmarasDDES_1_1Coefficients.html#a9229b3e34d9dc52aeae92f83fbb62822',1,'NeoFOAM::SpalartAllmarasDDES::Coefficients']]],
+  ['charwidth_3',['charWidth',['../classNeoFOAM_1_1FunctionObjectIO.html#aef5897869b1dd2f418089688e85a2ad0',1,'NeoFOAM::FunctionObjectIO']]],
+  ['cofr_5f_4',['cofR_',['../classNeoFOAM_1_1Forces.html#ad26f03f0165380c32e24764c4d46f841',1,'NeoFOAM::Forces']]],
+  ['controldict_5',['controlDict',['../structNeoFOAM_1_1RunTime.html#a4c146e57d9c3841741e71c6b6bc1b432',1,'NeoFOAM::RunTime']]],
+  ['cs_6',['Cs',['../structNeoFOAM_1_1SpalartAllmarasDDES_1_1Coefficients.html#a92d65f36aa1ca85a30e4b33c1637ef0c',1,'NeoFOAM::SpalartAllmarasDDES::Coefficients']]],
+  ['ct3_7',['Ct3',['../structNeoFOAM_1_1SpalartAllmarasDDES_1_1Coefficients.html#aa94dba7fa96e65c077fb6e321cb321db',1,'NeoFOAM::SpalartAllmarasDDES::Coefficients']]],
+  ['ct4_8',['Ct4',['../structNeoFOAM_1_1SpalartAllmarasDDES_1_1Coefficients.html#aaf50a989267f20cd1c054b21fa47b084',1,'NeoFOAM::SpalartAllmarasDDES::Coefficients']]],
+  ['cv1_9',['Cv1',['../structNeoFOAM_1_1SpalartAllmarasDDES_1_1Coefficients.html#a3e6244b9c2f0101dbf042819d6f34ac3',1,'NeoFOAM::SpalartAllmarasDDES::Coefficients']]],
+  ['cw2_10',['Cw2',['../structNeoFOAM_1_1SpalartAllmarasDDES_1_1Coefficients.html#a32019687db57a01df8b7b8b442c04055',1,'NeoFOAM::SpalartAllmarasDDES::Coefficients']]],
+  ['cw3_11',['Cw3',['../structNeoFOAM_1_1SpalartAllmarasDDES_1_1Coefficients.html#ac882ae43c05ade94c40b7238bec4c7ae',1,'NeoFOAM::SpalartAllmarasDDES::Coefficients']]]
+];
