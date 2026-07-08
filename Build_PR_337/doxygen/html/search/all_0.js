e6aabcec7aee4c61b11f819950b27d15e4f23657
greole <greole@users.noreply.github.com>
2026-07-08 08:37:11 +0000

    Deploying to gh-pages from @ exasim-project/NeoFOAM@98301522120ffaddf66c1ce9da5abffd9ea8e076 🚀

--- Build_PR_337/doxygen/html/search/all_0.js
+++ Build_PR_337/doxygen/html/search/all_0.js
@@ -6,7 +6,9 @@ var searchData=
   ['alphak2_3',['alphaK2',['../structNeoFOAM_1_1KOmegaSST_1_1Coefficients.html#aca01f7c6d3579cfe15badd94525ea923',1,'NeoFOAM::KOmegaSST::Coefficients']]],
   ['alphaomega1_4',['alphaOmega1',['../structNeoFOAM_1_1KOmegaSST_1_1Coefficients.html#ab5f7fc45a3778316b3f7f581a57595bc',1,'NeoFOAM::KOmegaSST::Coefficients']]],
   ['alphaomega2_5',['alphaOmega2',['../structNeoFOAM_1_1KOmegaSST_1_1Coefficients.html#a943434a37d0a4e1473892ec638a1789e',1,'NeoFOAM::KOmegaSST::Coefficients']]],
-  ['applyoperator_6',['applyOperator',['../namespaceNeoFOAM.html#a1a20de9aeea93a39b256d91014ce9039',1,'NeoFOAM']]],
-  ['assemble_7',['assemble',['../classNeoFOAM_1_1PDE.html#a56dc3d06d1616cf7726cee7c009fa756',1,'NeoFOAM::PDE::assemble()'],['../classNeoFOAM_1_1PDE.html#aed5b3fe4bae15d1c14be3b651f750f9c',1,'NeoFOAM::PDE::assemble(VolumeField &amp;psi, RunTime &amp;rt)'],['../classNeoFOAM_1_1PDE.html#a609089ce3754f0e97385b3824ed0acca',1,'NeoFOAM::PDE::assemble(dsl::SpatialOperator&lt; NeoN::Vec3 &gt; &amp;&amp;rhs)'],['../classNeoFOAM_1_1Solver.html#a7a7ea1bc3bdbe2616189eaaca665b22f',1,'NeoFOAM::Solver::assemble()']]],
-  ['assembleandrelax_8',['assembleandrelax',['../classNeoFOAM_1_1PDE.html#a8fd8c810990be74009144741fe40dced',1,'NeoFOAM::PDE::assembleAndRelax()'],['../classNeoFOAM_1_1Solver.html#ac1b779d09de38cf1d2beff2f4ea2436b',1,'NeoFOAM::Solver::assembleAndRelax()']]]
+  ['applyfixedvalueconstraints_6',['applyFixedValueConstraints',['../namespaceNeoFOAM_1_1detail.html#a599b28f960f5c2fb5023957ebb933d30',1,'NeoFOAM::detail']]],
+  ['applyoperator_7',['applyOperator',['../namespaceNeoFOAM.html#a1a20de9aeea93a39b256d91014ce9039',1,'NeoFOAM']]],
+  ['applysetreference_8',['applySetReference',['../namespaceNeoFOAM_1_1detail.html#af9be99c1b0711afcd45c4f0b435e3718',1,'NeoFOAM::detail']]],
+  ['assemble_9',['assemble',['../classNeoFOAM_1_1PDE.html#a56dc3d06d1616cf7726cee7c009fa756',1,'NeoFOAM::PDE::assemble()'],['../classNeoFOAM_1_1PDE.html#aed5b3fe4bae15d1c14be3b651f750f9c',1,'NeoFOAM::PDE::assemble(VolumeField &amp;psi, RunTime &amp;rt)'],['../classNeoFOAM_1_1PDE.html#a609089ce3754f0e97385b3824ed0acca',1,'NeoFOAM::PDE::assemble(dsl::SpatialOperator&lt; NeoN::Vec3 &gt; &amp;&amp;rhs)'],['../classNeoFOAM_1_1Solver.html#a7a7ea1bc3bdbe2616189eaaca665b22f',1,'NeoFOAM::Solver::assemble()']]],
+  ['assembleandrelax_10',['assembleandrelax',['../classNeoFOAM_1_1PDE.html#a8fd8c810990be74009144741fe40dced',1,'NeoFOAM::PDE::assembleAndRelax()'],['../classNeoFOAM_1_1Solver.html#ac1b779d09de38cf1d2beff2f4ea2436b',1,'NeoFOAM::Solver::assembleAndRelax()']]]
 ];
--- Build_PR_337/doxygen/html/search/typedefs_5.js
+++ Build_PR_337/doxygen/html/search/typedefs_5.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['scalar_0',['scalar',['../pressureVelocityCoupling_8hpp.html#acc912e7a5a44b028589fda3fd76ba7ea',1,'scalar:&#160;pressureVelocityCoupling.hpp'],['../kEpsilon_8hpp.html#acc912e7a5a44b028589fda3fd76ba7ea',1,'scalar:&#160;kEpsilon.hpp'],['../kOmegaSST_8hpp.html#acc912e7a5a44b028589fda3fd76ba7ea',1,'scalar:&#160;kOmegaSST.hpp'],['../spalartAllmarasDDES_8hpp.html#acc912e7a5a44b028589fda3fd76ba7ea',1,'scalar:&#160;spalartAllmarasDDES.hpp']]]
+  ['scalar_0',['scalar',['../pressureVelocityCoupling_8hpp.html#acc912e7a5a44b028589fda3fd76ba7ea',1,'scalar:&#160;pressureVelocityCoupling.hpp'],['../kEpsilon_8hpp.html#acc912e7a5a44b028589fda3fd76ba7ea',1,'scalar:&#160;kEpsilon.hpp'],['../kOmegaSST_8hpp.html#acc912e7a5a44b028589fda3fd76ba7ea',1,'scalar:&#160;kOmegaSST.hpp'],['../spalartAllmarasDDES_8hpp.html#acc912e7a5a44b028589fda3fd76ba7ea',1,'scalar:&#160;spalartAllmarasDDES.hpp']]],
+  ['scalarlinearsystem_1',['ScalarLinearSystem',['../namespaceNeoFOAM_1_1detail.html#a8affbab6ab257914abb3164c473fc9f6',1,'NeoFOAM::detail']]]
 ];
