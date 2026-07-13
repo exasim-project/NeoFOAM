81b90c313d7c927050dfe7cbdf4129b6440f38f1
chihtaw <chihtaw@users.noreply.github.com>
2026-07-13 15:10:42 +0000

    Deploying to gh-pages from @ exasim-project/NeoFOAM@2c61fa015ca238e2897178ae5e21ef2ebb31d693 🚀

--- Build_PR_363/doxygen/html/search/files_6.js
+++ Build_PR_363/doxygen/html/search/files_6.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['pde_2ehpp_0',['pde.hpp',['../pde_8hpp.html',1,'']]],
-  ['permutatoin_2ehpp_1',['permutatoin.hpp',['../permutatoin_8hpp.html',1,'']]],
+  ['permutation_2ehpp_1',['permutation.hpp',['../permutation_8hpp.html',1,'']]],
   ['pimplecontrol_2ehpp_2',['pimpleControl.hpp',['../pimpleControl_8hpp.html',1,'']]],
   ['pressurevelocitycoupling_2ehpp_3',['pressureVelocityCoupling.hpp',['../pressureVelocityCoupling_8hpp.html',1,'']]]
 ];
