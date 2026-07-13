dd43d244756b960043f71d1fb14f869370975286
Gregor Olenik <gregor.olenik@web.de>
2026-07-13 18:17:14 +0200

    fix: use assembleAndRelax() when momentumPredictor is disabled

--- examples/neoSimpleFoam/neoSimpleFoam.cpp
+++ examples/neoSimpleFoam/neoSimpleFoam.cpp
@@ -93,7 +93,7 @@ int main(int argc, char* argv[])
             }
             else
             {
-                UEqn.assemble();
+                UEqn.assembleAndRelax();
             }
 
             // SIMPLE / SIMPLEC pressure-velocity coupling (single pass, no inner PISO loop)
