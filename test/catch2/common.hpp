4244a60ad9bf4e7f17dd025d7fa4f066782711cd
Gregor Olenik <gregor.olenik@web.de>
2026-07-22 10:25:31 +0200

    fixup

--- test/catch2/common.hpp
+++ test/catch2/common.hpp
@@ -33,6 +33,9 @@ namespace nnfvcc = NeoN::finiteVolume::cellCentred;
 struct ApproxScalar
 {
     Foam::scalar margin;
+
+    ApproxScalar(Foam::scalar margin) : margin(margin) {}
+
     bool operator()(double rhs, double lhs) const
     {
         return Catch::Approx(rhs).margin(margin) == lhs;
