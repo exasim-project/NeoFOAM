777a0784a97df554aa9d966678883a3f40bbc96b
greole <greole@users.noreply.github.com>
2026-09-08 18:35:13 +0000

    Deploying to gh-pages from @ exasim-project/NeoFOAM@3c6228722e854804bfb9381af6c6e305658cbaf5 🚀

---
+++ Build_PR_399/doxygen/html/search/searchdata.js
@@ -0,0 +1,33 @@
+var indexSectionsWithContent =
+{
+  0: "abcdefgiklmnoprstuvw~",
+  1: "bcdefgiklmnoprstv",
+  2: "n",
+  3: "bcdefklmnoprstvw",
+  4: "abcdefgiklmnoprstuvw~",
+  5: "abcdefgikmnoprstuvw",
+  6: "cfmprsv"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "files",
+  4: "functions",
+  5: "variables",
+  6: "typedefs"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Namespaces",
+  3: "Files",
+  4: "Functions",
+  5: "Variables",
+  6: "Typedefs"
+};
+
---
+++ Build_PR_399/doxygen/html/search/variables_e.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['sigmaeps_0',['sigmaEps',['../structNeoFOAM_1_1KEpsilon_1_1Coefficients.html#a003a74bbe808056b1f858c4d5f65f691',1,'NeoFOAM::KEpsilon::Coefficients']]],
+  ['sigmak_1',['sigmaK',['../structNeoFOAM_1_1KEpsilon_1_1Coefficients.html#a1a7dd7c47d090f88d24cc8c6083f4f08',1,'NeoFOAM::KEpsilon::Coefficients']]],
+  ['sigmanut_2',['sigmaNut',['../structNeoFOAM_1_1SpalartAllmarasDDES_1_1Coefficients.html#a9dc275051b5337f7ed4339990154d567',1,'NeoFOAM::SpalartAllmarasDDES::Coefficients']]],
+  ['subcycleindex_3',['subCycleIndex',['../classNeoFOAM_1_1CreateFromFoamField.html#a0b659c2eb72285147dd9d0622203c9c7',1,'NeoFOAM::CreateFromFoamField']]],
+  ['sumfacearea_4',['sumFaceArea',['../structNeoFOAM_1_1BoundCache.html#a787dbec3f543e8eb7d047f57821d9e01',1,'NeoFOAM::BoundCache']]],
+  ['sumlocal_5',['sumLocal',['../structNeoFOAM_1_1ContinuityErrors.html#aff5b9f9bc60466ce0ae2479324be7f0a',1,'NeoFOAM::ContinuityErrors']]]
+];
