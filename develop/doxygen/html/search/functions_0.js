a31e47d5d4481ad15fdd76303af53ec920a48452
greole <greole@users.noreply.github.com>
2026-09-14 18:45:18 +0000

    Deploying to gh-pages from @ exasim-project/NeoFOAM@b0811117fb261e7993969c054dd4425e6bc30e7a 🚀

--- develop/doxygen/html/search/functions_0.js
+++ develop/doxygen/html/search/functions_0.js
@@ -5,5 +5,6 @@ var searchData=
   ['applyoperator_2',['applyOperator',['../namespaceNeoFOAM.html#a1a20de9aeea93a39b256d91014ce9039',1,'NeoFOAM']]],
   ['applysetreference_3',['applySetReference',['../namespaceNeoFOAM_1_1detail.html#af9be99c1b0711afcd45c4f0b435e3718',1,'NeoFOAM::detail']]],
   ['assemble_4',['assemble',['../classNeoFOAM_1_1PDE.html#a56dc3d06d1616cf7726cee7c009fa756',1,'NeoFOAM::PDE::assemble()'],['../classNeoFOAM_1_1PDE.html#aed5b3fe4bae15d1c14be3b651f750f9c',1,'NeoFOAM::PDE::assemble(VolumeField &amp;psi, RunTime &amp;rt)'],['../classNeoFOAM_1_1PDE.html#a609089ce3754f0e97385b3824ed0acca',1,'NeoFOAM::PDE::assemble(dsl::SpatialOperator&lt; NeoN::Vec3 &gt; &amp;&amp;rhs)'],['../classNeoFOAM_1_1Solver.html#a7a7ea1bc3bdbe2616189eaaca665b22f',1,'NeoFOAM::Solver::assemble()']]],
-  ['assembleandrelax_5',['assembleandrelax',['../classNeoFOAM_1_1PDE.html#a8fd8c810990be74009144741fe40dced',1,'NeoFOAM::PDE::assembleAndRelax()'],['../classNeoFOAM_1_1Solver.html#ac1b779d09de38cf1d2beff2f4ea2436b',1,'NeoFOAM::Solver::assembleAndRelax()']]]
+  ['assembleandrelax_5',['assembleandrelax',['../classNeoFOAM_1_1PDE.html#a8fd8c810990be74009144741fe40dced',1,'NeoFOAM::PDE::assembleAndRelax()'],['../classNeoFOAM_1_1Solver.html#ac1b779d09de38cf1d2beff2f4ea2436b',1,'NeoFOAM::Solver::assembleAndRelax()']]],
+  ['assignfixedvalues_6',['assignFixedValues',['../namespaceNeoFOAM_1_1detail.html#a09133effcf933f66d4987ba2f1e3a396',1,'NeoFOAM::detail']]]
 ];
